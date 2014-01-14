2fc9030048230aea64eaa40a276c8d0855122e57
Bobkhin <ducdhm@gmail.com>
2014-01-14 23:51:09 +0700

    Update manager business unit

--- src/main/webapp/static/js/fuse/fuse.js
+++
@@ -1,5 +0,0 @@
-/**
- * FuseLMS JS library based on BobJS (http://github.com/bobkhin/bobjs)
- * @type {Bob|*}
- */
-var Fuse = Bob;
---
+++ src/main/webapp/templates/themes/admin2/assets/js/fuse.js
@@ -0,0 +1,61 @@
+(function (Bob, $, win, doc, undefined) {
+	Bob.onDOMReady(function () {
+		var $body = $(doc.body);
+
+		$('.modal.modal-upload').exist(function () {
+			this.each(function () {
+				this.setAttribute('data-width', 800);
+			});
+		});
+
+		$('.modal.modal-edit').exist(function () {
+			this.each(function () {
+				this.setAttribute('data-edit', 400);
+			});
+		});
+
+		$('.table.table-data').exist(function () {
+			this.each(function () {
+				var $tableData = $(this),
+					col_length = $tableData.find('thead').find('th').length;
+
+				var aoColumnsSetting = [];
+				for (var i = 0; i < col_length; i++) {
+					aoColumnsSetting.push({
+						"bSortable": i < col_length - 1
+					});
+				}
+
+				$tableData.dataTable({
+					"aoColumnDefs": [{
+						"aTargets": [0]
+					}],
+					"oLanguage": {
+						"sLengthMenu": "Show _MENU_ Rows",
+						"sSearch": "",
+						"oPaginate": {
+							"sPrevious": "",
+							"sNext": ""
+						}
+					},
+					"aaSorting": [
+						[1, 'asc']
+					],
+					"aoColumns":aoColumnsSetting,
+					"aLengthMenu": [
+						[5, 10, 15, 20, -1],
+						[5, 10, 15, 20, "All"]
+					],
+					"iDisplayLength": 10
+				});
+
+				var $wrapper = $tableData.parent();
+
+				$wrapper.find('.dataTables_filter input').addClass("form-control input-sm").attr("placeholder", "Search");
+				$wrapper.find('.dataTables_length select').addClass("m-wrap small");
+				$wrapper.find('.dataTables_length select').select2();
+			});
+		});
+	});
+
+})(Bob, jQuery, window, document);
