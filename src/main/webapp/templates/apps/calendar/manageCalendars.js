61deede5b934a6384f0dde0b8ce9743e699a7c7c
Bobkhin <ducdhm@gmail.com>
2014-02-06 23:04:41 +0700

    manageCalendar (part 1)

--- src/main/webapp/templates/apps/calendar/manageCalendars.js
+++ src/main/webapp/templates/apps/calendar/manageCalendars.js
@@ -1,27 +1,29 @@
-
-var selectedForum;
-
 function initManageCalendars() {
-    $(".AddCalendar").click(function() {
-        showModal($("#addCalendarModal"));
-    });
-    $("#addCalendarModal form").forms({
+	var modal = $('#modal-add-calendar');
+
+	$('.btn-add-calendar').on('click', function (e) {
+		e.preventDefault();
+
+		modal.modal('show');
+	});
+
+	modal.find('form').forms({
         callback: function() {
             window.location.reload();
         }
     });
-    jQuery.timeago.settings.allowFuture = true;
-    $("abbr.timeago").timeago();
 
+    jQuery.timeago.settings.allowFuture = true;
+    $('abbr.timeago').timeago();
     initFullCalendar();
     initDeletes();
 }
 
 function initManageCalendar() {
-    $(".AddEvent").click(function() {
-        showModal($("#addEventModal"));
+    $('.AddEvent').click(function() {
+        showModal($('#addEventModal'));
     });
-    $("#addEventModal form").forms({
+    $('#addEventModal form').forms({
         callback: function() {
             window.location.reload();
         }
@@ -31,19 +33,20 @@ function initManageCalendar() {
 }
 
 function initManageEvent() {
-    log("initManageEvent");
-    initHtmlEditors($(".htmleditor"));
-    $(".manageEventForm").forms();
+    log('initManageEvent');
+    initHtmlEditors($('.htmleditor'));
+    $('.manageEventForm').forms();
 }
 
 function initDeletes() {
-    $(".Delete").click(function(e){
+    $('.btn-delete').click(function(e){
         e.preventDefault();
-        var target = $(e.target);
-        var link = target.closest("a");
-        var href = link.attr("href");
-        var name = getFileName(href);
-        confirmDelete( href, name, function() {
+
+        var btn = $(this);
+        var href = btn.attr('href');
+        var name = btn.closest('tr').find('th:first').text();
+
+        confirmDelete(href, name, function() {
             window.location.reload();
         } );
     });
@@ -60,14 +63,14 @@ function initFullCalendar() {
         allDayDefault: false,
         events: window.location.pathname,
         eventDrop: function(event, deltaDays, minuteDelta) {
-            log("eventDrop", event.end);
+            log('eventDrop', event.end);
             adjustStartDate(event, deltaDays, minuteDelta);
 //            addStartDate(event, deltaDays, minuteDelta);
 //            addEndDate(event, deltaDays, minuteDelta);
 //            updateEvent(event);
         },
         eventResize: function(event, deltaDays, minuteDelta) {
-            log("eventResize", event.end);
+            log('eventResize', event.end);
             adjustEndDate(event, deltaDays, minuteDelta);
 //            addStartDate(event, 0, 0);
 //            addEndDate(event, deltaDays, minuteDelta);
