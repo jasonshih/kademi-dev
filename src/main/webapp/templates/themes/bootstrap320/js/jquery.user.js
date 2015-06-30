234603b3510b3feea04bc89965b0d27f7bc30317
brad <brad@brad-little-laptop>
2015-07-01 11:55:28 +1200

    https://github.com/Kademi/kademi-dev/issues/482 - integrate with latest milton for keep me logged in support

--- src/main/webapp/templates/themes/bootstrap320/js/jquery.user.js
+++ src/main/webapp/templates/themes/bootstrap320/js/jquery.user.js
@@ -1,20 +1,20 @@
 /**
  *
  *  jquery.login.js
- *  
+ *
  *  Depends on user.js and common.js
- *  
+ *
  *  The target should be a div containing
  *  - a form
  *  - <p> with id validationMessage
  *  - input type text for the username
  *  - input type password for the password
- *  
+ *
  *  Additionally, will prompt the user to register or login for any links of class requiresUser
  *
  * Config:
  * urlSuffix: is appended to the current page url to make the url to POST the login request to. Default /.ajax
- * afterLoginUrl: the page to redirect to after login. Default index.html.  3 possibilities 
+ * afterLoginUrl: the page to redirect to after login. Default index.html.  3 possibilities
  *      null = do a location.reload()
  *      "none" - literal value "none" means no redirect
  *      "something" or "" = a relative path, will be avaluated relative to the user's url (returned in cookie)
@@ -26,7 +26,7 @@
  *  userNameProperty: property name to use in sending request to server
  *  passwordProperty
  *  loginCallback: called after successful login
- * 
+ *
  */
 
 (function($) {
@@ -62,11 +62,11 @@
             e.preventDefault();
 
             $("input", container).removeClass("errorField");
-            
+
             $(config.valiationMessageSelector, this).hide(100);
             resetValidation(container);
             try {
-                var userName = $("input[name=email]", container).val();                
+                var userName = $("input[name=email]", container).val();
                 var password = $("input[type=password]", container).val();
                 if (userName === null || userName.length === 0) {
                     $("input[type=text]", container).closest(".control-group")
@@ -81,7 +81,7 @@
             }
             return false;
         });
-        flog("init requiresUser links");        
+        flog("init requiresUser links");
         // use a body class to ensure is only inited once
         $("body").not("body.requiresUserDone").addClass("requiresUserDone").on("click", "a.requiresUser, button.requiresUser", function(e) {
             var target = $(e.target);
@@ -96,7 +96,7 @@
                     //target.click();
 
                 });
-            } else{ 
+            } else{
                 flog("all good, carry on...");
             }
         });
@@ -121,8 +121,13 @@ function doLogin(userName, password, config, container) {
         passwordProperty = "_loginPassword";
     }
 
+    var chk = container.find("input[name=keepLoggedIn]");
+    var keepLoggedIn = chk.prop("checked");
+
     data[userNameProperty] = userName;
     data[passwordProperty] = password;
+    data["keepLoggedIn"] = keepLoggedIn;
+    flog("login", data);
     $.ajax({
         type: 'POST',
         url: config.urlSuffix,
@@ -153,7 +158,7 @@ function doLogin(userName, password, config, container) {
                         flog("Not doing redirect because afterLoginUrl=='none'");
                     } else if( config.afterLoginUrl === "reload") {
                         window.location.reload(true);
-                        
+
                     } else {
                         // if config has a relative path, then evaluate it relative to the user's own url in response
                         flog("redirect to2: " + userUrl + config.afterLoginUrl);
@@ -209,9 +214,9 @@ function initUser() {
         userName = userUrl.substr(0, userUrl.length - 1); // drop trailing slash
         var pos = userUrl.indexOf("users");
         userName = userName.substring(pos + 6);
-        
+
         flog("current userName", userName);
-        
+
         $("#currentuser").attr("href", userUrl);
         $(".requiresuser").show();
         $(".sansuser").hide();
@@ -310,14 +315,14 @@ function showRegisterOrLoginModal(callbackOnLoggedIn) {
                 $(".loginCont").user({
                     afterLoginUrl: "none",
                     loginCallback: function() {
-                        closeModals();                        
+                        closeModals();
                         flog("logged in ok, process callback", callbackOnLoggedIn);
                         $('body').trigger('userLoggedIn', [userUrl, userName]);
                         callbackOnLoggedIn();
                     }
                 });
                 initRegisterForms("none", function() {
-                    closeModals();                     
+                    closeModals();
                     flog("registered and logged in ok, process callback");
                     $('body').trigger('userLoggedIn', [userUrl, userName]);
                     callbackOnLoggedIn();
