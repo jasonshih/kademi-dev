f79466375a25f8fb90d43f083a0d1c5e69661ec4
Wesley <tuzzmaniandevil@gmail.com>
2015-09-02 09:34:50 +1200

    https://github.com/Kademi/kademi-dev/issues/677

--- src/main/webapp/templates/apps/blogs/manageBlogs.js
+++ src/main/webapp/templates/apps/blogs/manageBlogs.js
@@ -1,41 +1,44 @@
 function initManageBlogs() {
-	initCRUDBlog();
-	initCRUDArticle();
+    initCRUDBlog();
+    initCRUDArticle();
 }
 
 function initCRUDBlog() {
-	var blogContainer = $('#blogs-container');
-	var modalAddBlog = $('#modal-blog');
+    var blogContainer = $('#blogs-container');
+    var modalAddBlog = $('#modal-blog');
 
     modalAddBlog.find('form').forms({
-        callback: function(resp) {
+        callback: function (resp) {
             flog('created  blog', resp);
             modalAddBlog.modal('hide');
             blogContainer.reloadFragment();
         }
     });
 
-    blogContainer.on('click', '.btn-rename-group', function(e) {
+    blogContainer.on('click', '.btn-rename-group', function (e) {
         e.preventDefault();
+        var btn = $(this);
+        var name = btn.data('name');
         var href = $(e.target).closest('a').attr('href');
 
-        promptRename(href, function() {
+
+        promptRename(href, name, function () {
             blogContainer.reloadFragment();
         });
     });
 
-    blogContainer.on('click', '.btn-delete-group', function(e) {
+    blogContainer.on('click', '.btn-delete-group', function (e) {
         e.preventDefault();
         var href = $(e.target).closest('a').attr('href');
 
-        confirmDelete(href, getFileName(href), function() {
+        confirmDelete(href, getFileName(href), function () {
             blogContainer.reloadFragment();
         });
     });
 
     var copyBlogModal = $('#modal-copy-blog');
-    blogContainer.on('click', '.btn-copy-blog', function(e) {
-    	e.preventDefault();
+    blogContainer.on('click', '.btn-copy-blog', function (e) {
+        e.preventDefault();
 
         var href = $(this).attr('href');
 
@@ -44,19 +47,37 @@ function initCRUDBlog() {
     });
 
     copyBlogModal.find('form').forms({
-        callback: function() {
+        callback: function () {
             Msg.info('Copied blog');
             blogContainer.reloadFragment();
-        	copyBlogModal.modal('hide');
+            copyBlogModal.modal('hide');
         }
     });
 }
 
+function promptRename(sourceHref, name, callback) {
+    log("promptRename", sourceHref);
+    var currentName = getFileName(sourceHref);
+    var newName = prompt("Please enter a new name for " + name, name);
+    if (newName) {
+        newName = newName.trim();
+        if (newName.length > 0 && currentName != newName) {
+            var currentFolder = getFolderPath(sourceHref);
+            var dest = currentFolder;
+            if (!dest.endsWith("/")) {
+                dest += "/";
+            }
+            dest += newName;
+            move(sourceHref, dest, callback);
+        }
+    }
+}
+
 function initCRUDArticle() {
     var newArticleModal = $('#modal-new-article');
-	var blogContainer = $('#blogs-container');
+    var blogContainer = $('#blogs-container');
 
-    blogContainer.on('click', '.btn-new-article', function(e) {
+    blogContainer.on('click', '.btn-new-article', function (e) {
         e.preventDefault();
 
         var href = $(this).attr('href');
@@ -67,7 +88,7 @@ function initCRUDArticle() {
     });
 
     newArticleModal.find('form').forms({
-        callback: function(resp) {
+        callback: function (resp) {
             flog('done', resp);
             newArticleModal.modal('hide');
             Msg.info('Created article, now redirecting to the edit page');
@@ -75,13 +96,13 @@ function initCRUDArticle() {
         }
     });
 
-    blogContainer.on('click', 'a.btn-delete-article', function(e) {
+    blogContainer.on('click', 'a.btn-delete-article', function (e) {
         e.preventDefault();
 
-        var link = $(this);        
+        var link = $(this);
         var href = link.attr('href');
 
-        confirmDelete(href, href, function() {
+        confirmDelete(href, href, function () {
             link.closest('tr').remove();
         });
     });
