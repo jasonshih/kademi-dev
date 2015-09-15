13750fa878e77542779b16a118574d246d44d65f
DucDHM <duc@kademi.co>
2015-09-15 17:44:29 +0700

    Make Website image ratio is 4/3. Remove the redandunt scroll element in Steam panel in dashboard

--- src/main/webapp/templates/apps/admin/manageWebsite.js
+++ src/main/webapp/templates/apps/admin/manageWebsite.js
@@ -10,7 +10,7 @@ function initManageWebsiteImage() {
     $('#btn-change-ava').upcropImage({
         buttonContinueText: 'Save',
         url: window.location.pathname, // this is actually the default value anyway
-        ratio: 1/1,
+        ratio: 4/3,
         onCropComplete: function (resp) {
             flog("onCropComplete:", resp, resp.nextHref);
             $("#websiteImage").reloadFragment();
