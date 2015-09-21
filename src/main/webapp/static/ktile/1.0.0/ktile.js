c03b00b32ddc5efc59ad0677ece464dafc99822f
DucDHM <duc@kademi.co>
2015-09-21 11:04:01 +0700

    New dashboard style with Tile concept #734

--- src/main/webapp/static/ktile/1.0.0/ktile.js
+++ src/main/webapp/static/ktile/1.0.0/ktile.js
@@ -3,12 +3,16 @@ $(function () {
     ktileWrappers.each(function () {
         var wrapper = $(this);
         var ktile = wrapper.find('.ktile');
+        var isLg = ktile.is('[class*=ktile-lg]');
+        var isMd = ktile.is('[class*=ktile-md]');
         var isLgH = ktile.hasClass('ktile-lg-h');
 
         if (isLgH) {
             wrapper.addClass('col-xl-4 col-lg-6 col-md-6 col-sm-6');
         } else {
-            wrapper.addClass('col-xl-2 col-lg-3 col-md-3 col-sm-3');
+            if (isLg || isMd) {
+                wrapper.addClass('col-xl-2 col-lg-3 col-md-3 col-sm-3');
+            }
         }
     });
 });
