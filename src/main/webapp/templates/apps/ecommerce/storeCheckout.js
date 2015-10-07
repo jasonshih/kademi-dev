cff27b488649d88cabd3f06a76bfcb9486c8ad8b
Wesley <tuzzmaniandevil@gmail.com>
2015-10-08 12:56:18 +1300

    https://github.com/Kademi/kademi-dev/issues/803

--- src/main/webapp/templates/apps/ecommerce/storeCheckout.js
+++ src/main/webapp/templates/apps/ecommerce/storeCheckout.js
@@ -1,3 +1,11 @@
+function initEcommerceCheckout() {
+    flog('initEcommerceCheckout');
+    initItemQuantity();
+    initRemoveItem();
+    initPaymentOptionSelect();
+    initLoginForm();
+}
+
 function initItemQuantity() {
     flog('initItemQuantity');
 
@@ -19,11 +27,11 @@ function initItemQuantity() {
             quantity--;
         }
 
-        if (quantity < 0) {
-            quantity = 0;
+        if (quantity < 1) {
+            quantity = 1;
         }
 
-        txtQuantity.val(quantity);
+        txtQuantity.val(quantity).change();
     };
 
     body.on('click', '.btn-decrease-quantity', function (e) {
@@ -41,10 +49,118 @@ function initItemQuantity() {
 
         changeQuantity(btn, true);
     });
+
+    body.on('change', '.txt-quantity', function (e) {
+        e.preventDefault();
+        var inpt = $(this);
+        var val = inpt.val();
+        var row = inpt.closest('.item-row');
+        var itemHref = row.find('.itemHref');
+        var href = itemHref.val();
+
+        doQuantityUpdate(href, val);
+    });
 }
 
-function initEcommerceCheckout() {
-    flog('initEcommerceCheckout');
+function doQuantityUpdate(href, quantity) {
+    flog("doQuantityUpdate", href);
+    $.ajax({
+        type: 'POST',
+        url: "/storeCheckout",
+        data: {
+            changeItemHrefQuantity: href,
+            quantity: quantity
+        },
+        datatype: "json",
+        success: function (data) {
+            Msg.info("Updated item in your shopping cart");
+            $("#itemsTable").reloadFragment();
+        },
+        error: function (resp) {
+            Msg.error("An error occured adding the product to your shopping cart. Please check your internet connection and try again");
+        }
+    });
+}
 
-    initItemQuantity();
+function initRemoveItem() {
+    flog('initRemoveItem');
+
+    $(document.body).on('click', '.btn-remove-item', function (e) {
+        e.preventDefault();
+
+        var btn = $(this);
+        var row = btn.closest('.item-row');
+        var itemHref = row.find('.itemHref');
+        var href = itemHref.val();
+
+        doRemoveFromCart(href);
+    });
+}
+
+function doRemoveFromCart(href) {
+    flog('doRemoveFromCart', href);
+    $.ajax({
+        type: 'POST',
+        url: "/storeCheckout",
+        data: {
+            removeItemHref: href
+        },
+        datatype: "json",
+        success: function (data) {
+            Msg.info("Removed item from your shopping cart");
+            $("#itemsTable").reloadFragment();
+        },
+        error: function (resp) {
+            Msg.error("An error occured adding the product to your shopping cart. Please check your internet connection and try again");
+        }
+    });
+}
+
+function initPaymentOptionSelect() {
+    flog('initPaymentOptionSelect');
+
+    // First check to see if the current form has any required fields
+    checkPaymentRequiredFields();
+
+    // Next we setup event handlers
+    $('body').on('click', '.payment-option', function (e) {
+        e.preventDefault();
+
+        var btn = $(this);
+        var pid = btn.data('pid');
+
+        var paymentForms = $('.payment-form');
+        paymentForms.hide();
+
+        var selectedForm = $('#pf-' + pid);
+        selectedForm.show();
+
+        checkPaymentRequiredFields();
+    });
+}
+
+function checkPaymentRequiredFields() {
+    flog('checkPaymentRequiredFields');
+    var paymentForms = $('.payment-form');
+    paymentForms.each(function (i, item) {
+        var f = $(item);
+        var isShown = f.is(':visible');
+        var requiredField = f.find('.required-if-shown');
+        if (isShown) {
+            f.find('input, select, textarea, button').disable(false);
+            requiredField.addClass('required');
+            requiredField.attr('required', 'required');
+        } else {
+            f.find('input, select, textarea, button').disable(true);
+            requiredField.removeClass('required');
+            requiredField.removeAttr('required');
+        }
+    });
+}
+
+function initLoginForm() {
+    jQuery(".login-form").user({
+        afterLoginUrl: window.location.pathname,
+        valiationMessageSelector: "p.login.message"
+    }); // setup login and logout
 }
