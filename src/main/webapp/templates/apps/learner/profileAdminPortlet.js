3eb2dcbd4afd15ad4335b1f2c5376362d36db518
Wesley <tuzzmaniandevil@gmail.com>
2015-10-28 11:37:20 +1300

    https://github.com/Kademi/kademi-dev/issues/898

--- src/main/webapp/templates/apps/learner/profileAdminPortlet.js
+++ src/main/webapp/templates/apps/learner/profileAdminPortlet.js
@@ -1,61 +1,61 @@
 function initAdminProfile() {
-	$('.btn-complete-module').click(function (e) {
-		e.preventDefault();
+    $('.btn-complete-module').click(function (e) {
+        e.preventDefault();
 
-		var modal = $('#modal-complete-module').modal('show');
-	});
+        var modal = $('#modal-complete-module').modal('show');
+    });
 
-	$('.cbb-program').change(function (e) {
-		var select = $(e.target);
-		var form = select.closest('form');
-		var moduleList = form.find('.modules-list');
-		moduleList.html('');
-		log('clicked', select.find('option:checked'));
-		var href = select.find('option:checked').attr('value');
-		form.attr('action', href);
-		if (href != null && href.length > 0) {
-			$.getJSON(href + '_DAV/PROPFIND?fields=href,milton:title,name,milton:moduleType&depth=2&where=milton:moduleType', function (data) {
-				log('resp', data.length);
+    $('.cbb-program').change(function (e) {
+        var select = $(e.target);
+        var form = select.closest('form');
+        var moduleList = form.find('.modules-list');
+        moduleList.html('');
+        log('clicked', select.find('option:checked'));
+        var href = select.find('option:checked').attr('value');
+        form.attr('action', href);
+        if (href != null && href.length > 0) {
+            $.getJSON(href + '_DAV/PROPFIND?fields=href,milton:title,name,milton:moduleType&depth=2&where=milton:moduleType', function (data) {
+                log('resp', data.length);
 
-				var moduleLi = '';
+                var moduleLi = '';
 
-				for (i = 0; i < data.length; i++) {
-					var item = data[i];
-					var id = 'option' + i;
-					log('add', item);
-					if (!item.name.startsWith('.')) {
+                for (i = 0; i < data.length; i++) {
+                    var item = data[i];
+                    var id = 'option' + i;
+                    log('add', item);
+                    if (!item.name.startsWith('.')) {
                         var title = item.title;
-                        if( title === undefined || title.length === 0) {
+                        if (title === undefined || title.length === 0) {
                             title = item.href;
                         }
-						moduleLi +=
-							'<li class="col-md-6 module">' +
-								'<div>' +
-									'<label for="' + id + '">' +
-										'<input id="' + id + '" type="checkbox" name="forceCompleteModule" value="' + item.href + '"/>' +
-										title +
-									'</label>' +
-									'<a class="btn btn-xs btn-default" target="_blank" title="Open" href="' + item.href + '"><i class="fa fa-caret-square-o-right"></i></a>' +
-								'</div>' +
-							'</li>';
-					}
-				}
+                        moduleLi +=
+                                '<li class="col-md-6 module">' +
+                                '<div>' +
+                                '<label for="' + id + '">' +
+                                '<input id="' + id + '" type="checkbox" name="forceCompleteModule" value="' + item.href + '"/>' +
+                                title +
+                                '</label>' +
+                                '<a class="btn btn-xs btn-default" target="_blank" title="Open" href="' + item.href + '"><i class="fa fa-caret-square-o-right"></i></a>' +
+                                '</div>' +
+                                '</li>';
+                    }
+                }
 
-				moduleList.append(moduleLi);
+                moduleList.append(moduleLi);
 
-				moduleList.parent().removeClass('hide');
-			});
-		}
-	});
+                moduleList.parent().removeClass('hide');
+            });
+        }
+    });
 
-	$(document.body).on('click', '.btn-delete-download', function (e) {
-		e.preventDefault();
-		flog(href);
-		var target = $(e.target);
-		var href = target.closest('a').attr('href');
-		var name = getFileName(href);
-		confirmDelete(href, name, function () {
-			target.closest('div').remove();
-		});
-	});
+    $(document.body).on('click', '.btn-delete-download', function (e) {
+        e.preventDefault();
+        flog(href);
+        var target = $(e.target);
+        var href = target.closest('a').attr('href');
+        var name = getFileName(href);
+        confirmDelete(href, name, function () {
+            $('#moduleprogress-body').reloadFragment();
+        });
+    });
 }
