1dba174f81d7a49b84b72cef201785d0804993fb
DucDHM <ducdhm@gmail.com>
2015-12-22 11:06:53 +0700

    Update bs335

--- src/main/webapp/templates/themes/bootstrap335/apps/signup/register.js
+++ src/main/webapp/templates/themes/bootstrap335/apps/signup/register.js
@@ -1,65 +1,66 @@
 function initRegister(afterRegisterHref) {
-    log("init labels")
-    var form = $("#registerForm");
+    log('init labels')
+    var form = $('#registerForm');
     var lastTabIndex = 0;
-    jQuery("#registerForm label.collapse").each(function(i, n) {
-        var label = jQuery(n);
+    $('#registerForm label.collapse').each(function (i, n) {
+        var label = $(n);
         var title = label.text();
-        var input = form.find("#" + label.attr("for"));
-        input.attr("title", title);
-        input.attr("tabindex", i + 1);
-        log("set tab index", input, i + 1);
+        var input = form.find('#' + label.attr('for'));
+        input.attr('title', title);
+        input.attr('tabindex', i + 1);
+        log('set tab index', input, i + 1);
         label.text(i + 1);
         lastTabIndex = i + 2;
     });
-    form.find("button[type=submit]").attr("tabindex", lastTabIndex);
+    form.find('button[type=submit]').attr('tabindex', lastTabIndex);
     initRegisterForms(afterRegisterHref);
 }
 
-
 function initRegisterForms(afterRegisterHref, callback) {
-    log("initRegisterForms - bootstrap320", jQuery("#registerForm"));
-    $("#registerForm").forms({
-        validationFailedMessage: "Please enter your details below.",
-        callback: function(resp, form) {
-            if (resp.messages && resp.messages[0] == "pending") {
+    var registerForm = $('#registerForm');
+    log('initRegisterForms - bootstrap335', registerForm);
+    
+    registerForm.forms({
+        validationFailedMessage: 'Please enter your details below.',
+        callback: function (resp, form) {
+            if (resp.messages && resp.messages[0] == 'pending') {
                 showPendingMessage();
             } else {
-                log("created account ok, logging in...", resp, form);
-                var userName = form.find("input[name=email]").val();
+                log('created account ok, logging in...', resp, form);
+                var userName = form.find('input[name=email]').val();
                 userName = userName.toLowerCase();
-                var password = form.find("input[name=password]").val();
+                var password = form.find('input[name=password]').val();
                 doLogin(userName, password, {
                     afterLoginUrl: afterRegisterHref,
-                    urlSuffix: "/.dologin",
+                    urlSuffix: '/.dologin',
                     loginCallback: callback
-                }, this);
+                }, registerForm);
             }
         }
     });
 
-    $.getScript("/static/typeahead/0.10.5/typeahead.bundle.js", function() {
-        $.getScript("/static/handlebars/1.2.0/handlebars.js", function() {
+    $.getScript('/static/typeahead/0.10.5/typeahead.bundle.js', function () {
+        $.getScript('/static/handlebars/1.2.0/handlebars.js', function () {
             try {
-                var searchUrl = $("#registerForm").attr("action");
+                var searchUrl = $('#registerForm').attr('action');
 
                 var orgs = new Bloodhound({
                     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
                     queryTokenizer: Bloodhound.tokenizers.whitespace,
                     remote: {
                         url: searchUrl + '?jsonQuery=%QUERY&th',
-                        replace: function() {
-                            return $("#registerForm").attr("action") + '?jsonQuery=' + encodeURIComponent($("#orgName").val()) + '&th';
+                        replace: function () {
+                            return $('#registerForm').attr('action') + '?jsonQuery=' + encodeURIComponent($('#orgName').val()) + '&th';
                         }
                     }
                 });
 
                 orgs.initialize();
 
-                $("#orgName").typeahead(null, {
+                $('#orgName').typeahead(null, {
                     minLength: 1,
-                    displayKey: "title",
-                    name: "orgs",
+                    displayKey: 'title',
+                    name: 'orgs',
                     source: orgs.ttAdapter(),
                     templates: {
                         empty: [
@@ -71,19 +72,18 @@ function initRegisterForms(afterRegisterHref, callback) {
                     }
                 });
                 $('.tt-hint').removeClass('required');
-                $("#orgName").on("typeahead:selected", function(e, datum) {
-                    log("Selected", e, datum);
-                    $("#orgId").val(datum.orgId);
+                $('#orgName').on('typeahead:selected', function (e, datum) {
+                    log('Selected', e, datum);
+                    $('#orgId').val(datum.orgId);
                 });
-                flog("init typeahead3");
+                flog('init typeahead3');
             } catch (e) {
-                flog("exception: " + e);
+                flog('exception: ' + e);
             }
         });
     });
 
-
     function showPendingMessage() {
-        showModal($("#pending"));
+        showModal($('#pending'));
     }
 }
