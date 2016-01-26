81eb499ab6a6747f5c7d835f64132ae8e3a8fe94
brad <brad@brad-little-laptop>
2016-01-26 15:37:23 +1300

    https://github.com/Kademi/kademi-dev/issues/1241

--- src/main/webapp/templates/apps/leadman/analytics.js
+++ src/main/webapp/templates/apps/leadman/analytics.js
@@ -1,21 +1,23 @@
 
 function initLeadManAnalytics() {
-    flog("initLeadManAnalytics");
+    //flog("initLeadManAnalytics");
     var width = 1000;
     var height = 500;
-    var levelHeight = 100;
+    var stageHeight = 200;
+    var levelHeight = stageHeight / 2;
 
     var svg = d3.select("#funnel")
             .attr("width", 2 * width)
             .attr("height", 2 * height);
 
-    //var data_url = "https://raw.githubusercontent.com/Kademi/kademi-dev/master/src/main/webapp/static/scratch/funnels/data.json";
+    //var data_url = "data.json";
+    //var data_url = "https://raw.githubusercontent.com/goddice/kademi-funnel-vis/master/data.json";
     var data_url = window.location.pathname + "?asJson";
     d3.json(data_url, function (resp) {
-        var json = resp.data;
-        flog("initLeadManAnalytics: got data", svg, json);
+        var json = resp;
+        //flog("initLeadManAnalytics: got data", svg, json);
         var size = json.stages.length;
-        var totalHeight = size * levelHeight;
+        var totalHeight = size * levelHeight * 2;
 
         var adjustTopWidth = 0;
         var maxCount = 0;
@@ -67,7 +69,7 @@ function initLeadManAnalytics() {
             svg.append("text")
                     .style("fill", "black")
                     .attr("x", 0)
-                    .attr("y", (t + 0.5) * levelHeight)
+                    .attr("y", (t + 0.5) * levelHeight * 2)
                     .text(json.stages[t].name);
         }
 
@@ -75,11 +77,11 @@ function initLeadManAnalytics() {
         name_set.forEach(function (value) {
             svg.append("text")
                     .style("fill", "black")
-                    .attr("x", adjustTopWidth + 200)
-                    .attr("y", (counter + 1) * 50)
+                    .attr("x", adjustTopWidth + 400)
+                    .attr("y", (counter + 1) * 50 + 4)
                     .text(value);
             svg.append("ellipse")
-                    .attr("cx", adjustTopWidth + 300)
+                    .attr("cx", adjustTopWidth + 370)
                     .attr("cy", (counter + 1) * 50)
                     .attr("rx", 10)
                     .attr("ry", 10)
@@ -99,7 +101,7 @@ function initLeadManAnalytics() {
                     .datum(trap.p)
                     .attr("fill", "white")
                     .attr("stroke", "gray")
-                    .attr("stroke-width", 1)
+                    .attr("stroke-width", 1);
 
             polygon.append("path")
                     .call(function (path) {
@@ -108,6 +110,146 @@ function initLeadManAnalytics() {
                         });
                     });
         }
+
+        function reorganizeSource(source)
+        {
+            var chart_data = {"names": [], "dates": [], "leads": [], "max_leadsum": 0};
+            var len = source.length;
+
+            for (var i=0; i<len; i++)
+            {
+                chart_data.names.push(source[i].name);
+            }
+
+            var date_set = new Set();
+            for (var i=0; i<len; i++)
+            {
+                for (var j=0; j<source[i].bydate.length; j++)
+                {
+                    date_set.add(source[i].bydate[j].data);
+                }
+            }
+
+            chart_data.dates = Array.from(date_set).sort();
+
+            var empty_list = [];
+            for (var i=0; i<len; i++)
+            {
+                empty_list.push(0);
+            }
+
+            date_set.forEach(function (value) {
+               chart_data.leads.push(empty_list.slice());
+            });
+
+            for (var i=0; i<len; i++)
+            {
+                for (var j=0; j<source[i].bydate.length; j++)
+                {
+                    var idx = chart_data.dates.indexOf(source[i].bydate[j].data);
+                    chart_data.leads[idx][i] = source[i].bydate[j].leads;
+                }
+            }
+
+            var max_lead = 0;
+            for (var i=0; i<chart_data.leads.length; i++)
+            {
+                var tmp_max = 0;
+                for (var j=0; j<chart_data.leads[i].length; j++)
+                {
+                    tmp_max += chart_data.leads[i][j];
+                }
+                max_lead = Math.max(max_lead, tmp_max);
+            }
+
+            chart_data.max_leadsum = max_lead;
+
+
+            return chart_data;
+        }
+
+        var chart_data_arr = [];
+        var max_leadsum = 0;
+        for (var t = 0; t < size; t++)
+        {
+            var chart_data = reorganizeSource(json.stages[t].sources);
+            chart_data_arr.push(chart_data);
+            max_leadsum = Math.max(max_leadsum, chart_data.max_leadsum);
+        }
+        for (var t = 0; t < size; t++)
+        {
+            var chart_data = chart_data_arr[t];
+            for (var i=0; i<chart_data.leads.length; i++)
+            {
+                for (var j=0; j<chart_data.leads[i].length; j++)
+                {
+                    chart_data.leads[i][j] /= max_leadsum;
+                }
+            }
+        }
+
+        for (var t = 0; t < size; t++)
+        {
+            var date_str_len = 66;
+            var chart_data = chart_data_arr[t];
+            var chart_width = trapBox.right((t + 1) * totalHeight / size) - trapBox.left((t + 1) * totalHeight / size);
+            var chart_height = levelHeight * 0.4;
+            var chart_div = chart_width / chart_data.dates.length;
+            var stride = Math.ceil(date_str_len / chart_div);
+            for (var i=0; i<chart_data.leads.length; i++)
+            {
+                var base_x = trapBox.left((t + 1) * totalHeight / size) + chart_div * i;
+                var base_y = (t + 1) * totalHeight / size - 5;
+
+                if (i === chart_data.leads.length - 1)
+                {
+                    if (stride < 2)
+                    {
+                        if (i % stride === 0)
+                        {
+                            svg.append("text")
+                                .style("fill", "black")
+                                .attr("x", base_x + 5)
+                                .attr("y", base_y)
+                                .attr("font-size", "10px")
+                                .attr("font-family", "sans-serif")
+                                .text(chart_data.dates[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    if (i % stride === 0)
+                    {
+                        svg.append("text")
+                            .style("fill", "black")
+                            .attr("x", base_x)
+                            .attr("y", base_y)
+                            .attr("font-size", "10px")
+                            .attr("font-family", "sans-serif")
+                            .text(chart_data.dates[i]);
+                    }
+                }
+
+                base_y -= 15;
+
+                for (var j=0; j<chart_data.leads[i].length; j++)
+                {
+                    var l = chart_data.leads[i][j] * chart_height;
+                    svg.append("rect")
+                        .attr("x", base_x)
+                        .attr("y", base_y - l)
+                        .attr("width", chart_div - 5)
+                        .attr("height", l)
+                        .attr("fill", stringToColorCode(chart_data.names[j]));
+                    base_y = base_y - l;
+                }
+            }
+        }
+
+
+        //svg.append("text").attr("x", 1100).attr("y", 200).attr("font-size", "13px").attr("font-family", "sans-serif").text("99/99/9999");
+        //svg.append("rect").attr("x", 1100).attr("y", 200).attr("width", 66).attr("height", 20);
 
         var force = d3.layout.force()
                 .size([width, height]);
@@ -170,8 +312,8 @@ function initLeadManAnalytics() {
                                 var rad = d.radius;
                                 var trap = new Trapezoidal([[trapBox.left(idx * totalHeight / size), idx * totalHeight / size],
                                     [trapBox.right(idx * totalHeight / size), idx * totalHeight / size],
-                                    [trapBox.right((idx + 1) * totalHeight / size), (idx + 1) * totalHeight / size],
-                                    [trapBox.left((idx + 1) * totalHeight / size), (idx + 1) * totalHeight / size]]);
+                                    [trapBox.right((idx + 0.70) * totalHeight / size), (idx + 0.70) * totalHeight / size],
+                                    [trapBox.left((idx + 0.70) * totalHeight / size), (idx + 0.70) * totalHeight / size]]);
                                 return d.y = Math.max(trap.top() + rad + 8, Math.min(trap.button() - rad - 8, d.y));
                             })
                             .attr("cx", function (d) {
@@ -179,8 +321,8 @@ function initLeadManAnalytics() {
                                 var rad = d.radius;
                                 var trap = new Trapezoidal([[trapBox.left(idx * totalHeight / size), idx * totalHeight / size],
                                     [trapBox.right(idx * totalHeight / size), idx * totalHeight / size],
-                                    [trapBox.right((idx + 1) * totalHeight / size), (idx + 1) * totalHeight / size],
-                                    [trapBox.left((idx + 1) * totalHeight / size), (idx + 1) * totalHeight / size]]);
+                                    [trapBox.right((idx + 0.70) * totalHeight / size), (idx + 0.70) * totalHeight / size],
+                                    [trapBox.left((idx + 0.70) * totalHeight / size), (idx + 0.70) * totalHeight / size]]);
                                 return d.x = Math.max(trap.left(d.y) + rad + 11, Math.min(trap.right(d.y) - rad - 11, d.x));
                             });
                 })
@@ -223,7 +365,8 @@ Trapezoidal.prototype.center = function ()
     pos.push((this.p[0][0] + this.p[1][0] + this.p[2][0] + this.p[3][0]) / 2);
     pos.push((this.p[0][1] + this.p[1][1] + this.p[2][1] + this.p[3][1]) / 2);
     return pos;
-}
+};
+
 Trapezoidal.prototype.hasPoint = function (point)
 {
     if (point[1] < this.top())
