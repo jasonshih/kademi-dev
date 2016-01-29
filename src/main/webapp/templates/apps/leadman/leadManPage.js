b9116776cbf5d221af85e32db4b28d34c590fc12
tuzzm <tuzzm@DESKTOP-2QJ51PD>
2016-01-29 16:34:00 +1300

    https://github.com/Kademi/kademi-dev/issues/1216

--- src/main/webapp/templates/apps/leadman/leadManPage.js
+++ src/main/webapp/templates/apps/leadman/leadManPage.js
@@ -46,10 +46,85 @@
                 var t = template(data.hits);
                 $('#leadBody').empty();
                 $('#leadBody').html(t);
+
+                $('#LeadTotal').html(data.hits.total);
+                $('#LeadSumValue').html(data.aggregations.dealAmountTotal.value || 0);
+                $('#leadAvgValue').html(data.aggregations.dealAmountAvg.value || 0);
+
+                updateSourcesPie(data.aggregations.sources.buckets);
+                updateStagesPie(data.aggregations.stages.buckets);
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 flog('error', jqXHR, textStatus, errorThrown);
             }
+        });
+    }
+
+    function updateSourcesPie(buckets) {
+        $('#stagesPie svg').empty();
+        //Donut chart
+        nv.addGraph(function () {
+            var chart = nv.models.pieChart()
+                    .x(function (d) {
+                        return d.key;
+                    })
+                    .y(function (d) {
+                        return d.doc_count;
+                    })
+                    .showLabels(false)
+                    .showLegend(false)
+                    .margin({
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0
+                    })
+                    .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+                    .labelType("value")   //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+                    .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+                    .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+                    ;
+
+            d3.select("#stagesPie svg")
+                    .datum(buckets)
+                    .transition().duration(350)
+                    .call(chart);
+
+            return chart;
+        });
+    }
+
+    function updateStagesPie(buckets) {
+        $('#sourcesPie svg').empty();
+        //Donut chart
+        nv.addGraph(function () {
+            var chart = nv.models.pieChart()
+                    .x(function (d) {
+                        return d.key;
+                    })
+                    .y(function (d) {
+                        return d.doc_count;
+                    })
+                    .showLabels(false)
+                    .showLegend(false)
+                    .margin({
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0
+                    })
+                    .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+                    .labelType("value")   //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+                    .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+                    .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+                    ;
+
+            d3.select("#sourcesPie svg")
+                    .datum(buckets)
+                    .transition().duration(350)
+                    .call(chart);
+
+            return chart;
         });
     }
 
@@ -68,7 +143,7 @@
 
         Handlebars.registerHelper('formatTime', function (object) {
             var d = new moment(object);
-            
+
             return d.format('MMMM Do YYYY, h:mm:ss a');
         });
 
