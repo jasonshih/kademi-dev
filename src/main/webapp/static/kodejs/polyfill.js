e7da41d86d305e48da287a5be4bb50bb6a706bc9
tuzzm <tuzzm@DESKTOP-2QJ51PD>
2016-02-02 18:43:19 +1300

    https://github.com/Kademi/kademi-dev/issues/1288

--- src/main/webapp/static/kodejs/polyfill.js
+++ src/main/webapp/static/kodejs/polyfill.js
@@ -33,3 +33,22 @@ load('static:/kodejs/kademi.js');
     }
     exports.FormData = Java.type('io.milton.cloud.server.repoapps.http.FormData');
 })(this);
+
+
+/*
+ * setTimeout Polyfill
+ */
+(function (exports) {
+    if (!exports.setTimeout) {
+        exports.setTimeout = function () {
+            var args = [].slice.call(arguments, 2, arguments.length);
+            timer.setTimeout(arguments[0], arguments[1], args);
+        };
+    }
+
+    if (!exports.clearTimeout) {
+        exports.clearTimeout = function (id) {
+            timer.clearTimeout(id);
+        };
+    }
+})(this);
