24fa836bd0ca5a3da5589aaaf327085b46c43a09
dylan <dylan@dylan-G75VW>
2016-02-29 15:12:31 +1300

    https://github.com/Kademi/kademi-dev/issues/1381

--- src/main/webapp/templates/apps/leadman/leadTasks.js
+++ src/main/webapp/templates/apps/leadman/leadTasks.js
@@ -2,8 +2,21 @@ $(function () {
     $('input[name=taskType]').on('change', function (e) {
         var t = $(this);
         var uri = new URI(window.location.search);
-        uri.removeSearch('type')
+        uri.removeSearch('type');
         uri = uri.addSearch('type', t.val());
+
+        window.location.search = uri.search();
+    });
+
+    $('body').on('click', '.filter-assignedto', function (e) {
+        e.preventDefault();
+
+        var btn = $(this);
+        var name = btn.attr('href');
+
+        var uri = new URI(window.location.search);
+        uri.removeSearch('assignedTo');
+        uri = uri.addSearch('assignedTo', name);
 
         window.location.search = uri.search();
     });
