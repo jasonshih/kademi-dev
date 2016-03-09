aa52d44413156e47d467e1de131aa2415844d502
DucDHM <ducdhm@gmail.com>
2016-03-09 17:07:18 +0700

    https://github.com/Kademi/client-kademi-deloitte/issues/33

--- src/main/webapp/templates/apps/search/search.js
+++ src/main/webapp/templates/apps/search/search.js
@@ -2,14 +2,26 @@ function initContentSearch() {
     flog("initContentSearch");
 
     var txtKeyword = $("input[name=q]");
-    txtKeyword.keyup(function() {
-        var query = $(this).val();
-        typewatch(function() {
-            // Load fragment
-            flog("do search", $('#search-results'));
-            $('#search-results').load('contentSearch?q=' + encodeURIComponent(query.trim()) + ' #search-results > *');
-        }, 500);
+    var searchResults = $('#search-results');
+    txtKeyword.on({
+        keyup: function () {
+            var query = $(this).val();
+            typewatch(function () {
+                // Load fragment
+                flog("do search on keyup event", searchResults);
+                searchResults.load('contentSearch?q=' + encodeURIComponent(query.trim()) + ' #search-results > *');
+            }, 500);
+        },
+        input: function () {
+            var query = $(this).val();
+            if (query == '') {
+                flog("do search on change event", searchResults);
+                searchResults.load('contentSearch?q=' + encodeURIComponent(query.trim()) + ' #search-results > *');
+            }
+        }
     });
+
+    txtKeyword.keyup();
 
     txtKeyword.closest('form').on('submit', function (e) {
         var keyword = txtKeyword.val().trim();
@@ -21,60 +33,64 @@ function initContentSearch() {
 }
 
 function initReindexForms() {
-	$(".manage-search form").forms({
-		confirmMessage: "Processing, Please Wait...",
-        callback: function(resp) {
+    $(".manage-search form").forms({
+        confirmMessage: "Processing, Please Wait...",
+        callback: function (resp) {
             flog("The contents of current repository have been re-indexed", resp);
             // Should be disabled the Re-index button while processing, just enable after re-index thread was started
             $(".btn-reindex").attr({"disabled": "disabled"});
 
             // Load state of re-indexing process
-            setTimeout(function(){reIndexState()}, 1500);
+            setTimeout(function () {
+                reIndexState()
+            }, 1500);
         }
     });
 }
 
 function reIndexState() {
-	$.ajax({
-		url: location.href + "../manageSearchStatus",
-		success: function(res) {
-			flog("Response", res);
-			var resp = $.parseJSON(res);
-			var data = resp.data, status = resp.status, message = resp.messages, state = message[0];
-			if (state != 'STOPPED') {
-				flog("Re-indexing state: " + data + " resources (htmls, blogs, products...) have been processed");
-				$(".pageMessage").css({"display":"block"});
-				$(".pageMessage").empty();
-				$(".pageMessage").html("<b>Re-Indexing Status: </b><i class=\"badge badge-info\">" + data + "</i> files have been processed");
-			}
+    $.ajax({
+        url: location.href + "../manageSearchStatus",
+        success: function (res) {
+            flog("Response", res);
+            var resp = $.parseJSON(res);
+            var data = resp.data, status = resp.status, message = resp.messages, state = message[0];
+            if (state != 'STOPPED') {
+                flog("Re-indexing state: " + data + " resources (htmls, blogs, products...) have been processed");
+                $(".pageMessage").css({"display": "block"});
+                $(".pageMessage").empty();
+                $(".pageMessage").html("<b>Re-Indexing Status: </b><i class=\"badge badge-info\">" + data + "</i> files have been processed");
+            }
 
-			var lblReindex = $(".lbl-reindex-action");
-			if (state == 'PENDING' || state == 'PROCESSING') {
-				if (state == 'PROCESSING') {
-					$(".btn-reindex").removeAttr("disabled");
-		            lblReindex.empty();
-		            lblReindex.html("Stop re-indexing process");
-				}
-				// Continually to check running jobs status every 500 milliseconds
-				setTimeout(function(){reIndexState()}, 500);
-			} else if (state == 'STOPPED' || state == 'COMPLETED' || !status) {
-				if (state == 'STOPPED') {
-					flog("RE-INDEXING PROCESS HAVE BEEN STOPPED BY SOMEONE!!!");
-					$(".btn-reindex").removeAttr("disabled");
-				}
+            var lblReindex = $(".lbl-reindex-action");
+            if (state == 'PENDING' || state == 'PROCESSING') {
+                if (state == 'PROCESSING') {
+                    $(".btn-reindex").removeAttr("disabled");
+                    lblReindex.empty();
+                    lblReindex.html("Stop re-indexing process");
+                }
+                // Continually to check running jobs status every 500 milliseconds
+                setTimeout(function () {
+                    reIndexState()
+                }, 500);
+            } else if (state == 'STOPPED' || state == 'COMPLETED' || !status) {
+                if (state == 'STOPPED') {
+                    flog("RE-INDEXING PROCESS HAVE BEEN STOPPED BY SOMEONE!!!");
+                    $(".btn-reindex").removeAttr("disabled");
+                }
 
-				// Throw exception message
-				if (!status) {
-					Msg.error(state);
-				}
-				lblReindex.empty();
-				lblReindex.html("Re-index");
+                // Throw exception message
+                if (!status) {
+                    Msg.error(state);
+                }
+                lblReindex.empty();
+                lblReindex.html("Re-index");
 
-				setTimeout(function() {
-					// Should be closed message after 5 minutes
-					$(".pageMessage").css({"display":"none"});
-				}, 5000);
-			}
-		}
-	});
+                setTimeout(function () {
+                    // Should be closed message after 5 minutes
+                    $(".pageMessage").css({"display": "none"});
+                }, 5000);
+            }
+        }
+    });
 }
