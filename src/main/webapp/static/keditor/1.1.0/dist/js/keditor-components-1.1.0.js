eb9cc2c50ef18a7290698bc113381d052ec7655f
DucDHM <ducdhm@gmail.com>
2016-03-10 12:07:33 +0700

    https://github.com/Kademi/kademi-dev/issues/1443

--- src/main/webapp/static/keditor/1.1.0/dist/js/keditor-components-1.1.0.js
+++ src/main/webapp/static/keditor/1.1.0/dist/js/keditor-components-1.1.0.js
@@ -544,7 +544,7 @@
                 {name: 'styles', groups: ['styles']},
                 {name: 'colors', groups: ['colors']},
                 {name: 'tools', groups: ['tools']},
-                {name: 'others', groups: ['others']},
+                {name: 'others', groups: ['others']}
             ],
             title: false,
             allowedContent: true, // DISABLES Advanced Content Filter. This is so templates with classes: allowed through
