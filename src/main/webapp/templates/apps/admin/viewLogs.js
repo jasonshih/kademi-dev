ce5b3b08e23a65b42c8a8aa1bb8b02fb0caa76d7
brad <brad@DESKTOP-4JI0G1V>
2016-03-31 15:50:02 +1300

    https://github.com/Kademi/kademi-dev/issues/1568

--- src/main/webapp/templates/apps/admin/viewLogs.js
+++ src/main/webapp/templates/apps/admin/viewLogs.js
@@ -1,50 +1,87 @@
 var itemsToAppend = [];
 var appendTimer = null;
 var maxLimit;
+var lastLogDate = 0;
 
-function initWebsockets(orgId, txtQueryUser, txtQueryMessage) {
-    flog('initWebsockets', orgId);
+function initViewLogs(options) {
+    var orgId = options.orgId;
+    maxLimit = options.maxLimit;
+    var logsBody = $('#logsBody');
+    var txtQueryUser = $('#user-query');
+    var txtQueryMessage = $('#message-query');
 
-    var b64ContentId = Base64.encode(orgId);
-    try {
-        var proto = 'ws://';
-        if (window.location.protocol === 'https:') {
-            proto = 'wss://';
-        }
-        var url = window.location.host + '/comments/' + window.location.host + '/logging/' + b64ContentId;
-
-        wsocket = new WebSocket(proto + url);
-        wsocket.onmessage = function (evt) {
-            flog('Received Logs');
-
-            var c = $.parseJSON(evt.data);
-            var filterMsg = txtQueryUser.val().toLowerCase();
-            var filterUser = txtQueryMessage.val().toLowerCase();
-
-            for (var i = 0; i < c.length; i++) {
-                processReceivedLog(c[i], filterMsg, filterUser);
-            }
-
-            if (appendTimer != null) {
-                clearTimeout(appendTimer);
-                appendTimer = null;
-            }
-            if (itemsToAppend.length > 50) {
-                appendItems();
-            } else {
-                appendTimer = setTimeout(appendItems, 250);
-            }
-        };
-
-        flog('done initWebsockets');
-    } catch (e) {
-        // TODO: setup polling to load comments every minute or so
-        flog('Websocket initialisation failed');
+    var logsBodyMaxHeight = $(window).height() - 329;
+    if (logsBodyMaxHeight < 300) {
+        logsBodyMaxHeight = 300;
     }
+    logsBody.css('max-height', logsBodyMaxHeight);
+
+    initTimeAgo();
+
+    $('.logs-clear').click(function (e) {
+        e.preventDefault();
+
+        logsBody.html('');
+    });
+
+    txtQueryUser.on('change', function () {
+        var val = txtQueryUser.val();
+        flog('user-query change', val);
+
+        filterTableByUser(val);
+    });
+
+    txtQueryMessage.on('change', function () {
+        var val = txtQueryMessage.val();
+        flog('message-query change', val);
+
+        filterTableByMsg(val);
+    });
+
+    $('#level-filter').val('').on('change', function () {
+        var classLevel = this.value;
+
+        if (classLevel) {
+            logsBody.attr('class', classLevel);
+        } else {
+            logsBody.attr('class', '');
+        }
+    });
+
+    window.setTimeout(loadNewLogs, 1000);
+}
+
+function loadNewLogs() {
+    $.ajax({
+        type: 'GET',
+        url: window.location.pathname,
+        dataType: 'json',
+        data: {
+            since: lastLogDate,
+            user: $('#user-query').val(),
+            keyword: $('#message-query').val()
+        },
+        success: function (data) {
+            flog('success. items=', data.hits.hits.length);
+            if( data.hits.hits.length > 0 ) {
+                lastLogDate = data.hits.hits[0].fields.date[0];
+            }
+            flog("lastLogDate", lastLogDate);
+            $.each(data.hits.hits, function (i, n) {
+                processReceivedLog(n.fields);                
+            });
+            appendItems();
+            window.setTimeout(loadNewLogs, 5000);
+        },
+        error: function (resp) {
+            Msg.error('An error occured. Please check your internet connection');
+        }
+    });
+
 }
 
 function appendItems() {
-    flog('Appending with ' + itemsToAppend.length + ' logs');
+//    flog('Appending with ' + itemsToAppend.length + ' logs');
 
     appendTimer = null;
     $('#logsBody').append(itemsToAppend);
@@ -56,48 +93,46 @@ function appendItems() {
     }
 }
 
-function processReceivedLog(c, filterMsg, filterUser) {
-    var dt = moment(c.data);
-    var labelType = c.level.toLowerCase();
+function processReceivedLog(c) {
+    flog("log", c);
+    var dt = moment(c.date[0]);
+    var labelType = c.level[0].toLowerCase();
 
-    if ((filterMsg === '' || c.message.toLowerCase().contains(filterMsg))
-            && (filterUser == '' || c.userName.toLowerCase().contains(filterUser))) {
-        if (labelType == 'error') {
-            labelType = 'danger';
-        } else if (labelType == 'warn') {
-            labelType = 'warning';
-        } else if (labelType == 'trace') {
-            labelType = 'default';
-        }
-
-        var row = $(
-                '<div class="' + labelType + ' col-row clearfix">' +
-                '    <div class="col-lvl"><span class="label label-' + labelType + '" >' + c.level + '</span></div>' +
-                '    <div class="col-user"><a target="_blank" class="userName" href="' + c.userHref + '">' + c.userName + '</a></div>' +
-                '    <div class="col-msg"></div>' +
-                '    <div class="col-date"><abbr title="' + dt.format(moment.ISO_8601) + '" class="timeago">' + dt.format() + '</abbr></div>' +
-                '</div>'
-                );
-        row.find('.col-msg').text(c.message);
-
-        itemsToAppend.push(row);
-
-        if (c.callstack !== null && c.callstack.length > 0) {
-            row = $(
-                    '<div class="col-row clearfix no-bordered">' +
-                    '   <div class="causes"></div>' +
-                    '</div>'
-                    );
-
-            var ul = $('<ul />');
-            $.each(c.callstack, function (i, n) {
-                $('<li>').text(n).appendTo(ul);
-            });
-            row.find('.causes').append(ul);
-
-            itemsToAppend.push(row);
-        }
+    if (labelType == 'error') {
+        labelType = 'danger';
+    } else if (labelType == 'warn') {
+        labelType = 'warning';
+    } else if (labelType == 'trace') {
+        labelType = 'default';
     }
+
+    var row = $(
+            '<div class="' + labelType + ' col-row clearfix">' +
+            '    <div class="col-lvl"><span class="label label-' + labelType + '" >' + c.level + '</span></div>' +
+            '    <div class="col-user"><a target="_blank" class="userName" href="' + c.userHref + '">' + c.userName + '</a></div>' +
+            '    <div class="col-msg"></div>' +
+            '    <div class="col-date"><abbr title="' + dt.format(moment.ISO_8601) + '" class="timeago">' + dt.format() + '</abbr></div>' +
+            '</div>'
+            );
+    row.find('.col-msg').text(c.message);
+
+    itemsToAppend.push(row);
+//
+//    if (c.callstack !== null && c.callstack.length > 0) {
+//        row = $(
+//                '<div class="col-row clearfix no-bordered">' +
+//                '   <div class="causes"></div>' +
+//                '</div>'
+//                );
+//
+//        var ul = $('<ul />');
+//        $.each(c.callstack, function (i, n) {
+//            $('<li>').text(n).appendTo(ul);
+//        });
+//        row.find('.causes').append(ul);
+//
+//        itemsToAppend.push(row);
+//    }
 }
 
 function filterTableByMsg(val) {
@@ -157,55 +192,10 @@ function trimTable(limit) {
     if (logTable.length > limit) {
         logBody.find('.col-row:lt(' + (logTable.length - limit) + ')').remove()
     }
+    logTable = logBody.find('.col-row');
+    flog('New Table Size', logTable.length, 'Limit', limit)
 }
 
 function initTimeAgo() {
     $('abbr.timeago').timeago();
-}
-
-function initViewLogs(options) {
-    var orgId = options.orgId;
-    maxLimit = options.maxLimit;
-    var logsBody = $('#logsBody');
-    var txtQueryUser = $('#user-query');
-    var txtQueryMessage = $('#message-query');
-
-    var logsBodyMaxHeight = $(window).height() - 329;
-    if (logsBodyMaxHeight < 300) {
-        logsBodyMaxHeight = 300;
-    }
-    logsBody.css('max-height', logsBodyMaxHeight);
-
-    initTimeAgo();
-    initWebsockets(orgId, txtQueryUser, txtQueryMessage);
-
-    $('.logs-clear').click(function (e) {
-        e.preventDefault();
-
-        logsBody.html('');
-    });
-
-    txtQueryUser.on('change', function () {
-        var val = txtQueryUser.val();
-        flog('user-query change', val);
-
-        filterTableByUser(val);
-    });
-
-    txtQueryMessage.on('change', function () {
-        var val = txtQueryMessage.val();
-        flog('message-query change', val);
-
-        filterTableByMsg(val);
-    });
-
-    $('#level-filter').val('').on('change', function () {
-        var classLevel = this.value;
-
-        if (classLevel) {
-            logsBody.attr('class', classLevel);
-        } else {
-            logsBody.attr('class', '');
-        }
-    });
 }
