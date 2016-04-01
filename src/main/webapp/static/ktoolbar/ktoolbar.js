b6ec3cd68cc7d74a6bf7fefeeb75277743c8ba8e
anh <anhnt38@gmail.com>
2016-04-01 12:10:50 +0700

    https://github.com/Kademi/kademi-dev/issues/1327

--- src/main/webapp/static/ktoolbar/ktoolbar.js
+++ src/main/webapp/static/ktoolbar/ktoolbar.js
@@ -2,5 +2,9 @@
  * Created by Anh on 4/1/2016.
  */
 $(function(){
-    
+    var htmleditor = $('.htmleditor');
+    var ktoolbar = $('#ktoolbar');
+    if(htmleditor.length){
+        ktoolbar.find('.btn-inline-edit').removeClass('hide');
+    }
 })
