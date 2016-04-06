ac869cf24eaea25e408f6c804be0c1a08eaf5b9d
DucDHM <ducdhm@gmail.com>
2016-04-06 16:40:06 +0700

    https://github.com/Kademi/kademi-dev/issues/1551

--- src/main/webapp/templates/apps/admin/manageWebsite.js
+++ src/main/webapp/templates/apps/admin/manageWebsite.js
@@ -4,6 +4,437 @@ function initManageWebsite() {
     initApps();
     initManageMenu();
     initManageWebsiteImage();
+    initSwitchPublic();
+    initPublishingMenu("");
+    initHtmlEditors($(".htmleditor"), "60px", "200px", 'embed_video,modal', 'resize,elementspath');
+    initCerts();
+    initEmailServerTab();
+    initHttpsTab();
+
+    $("form.details").forms({
+        callback: function (resp) {
+            flog("Saved Ok!");
+            Msg.info("Saved");
+        }
+    });
+}
+
+function initHttpsTab() {
+    flog('initHttpsTab');
+
+    initInputFile();
+    initCSRHttps();
+    initCertHttps();
+    initPKHttps();
+}
+
+function initPKHttps() {
+    flog('initPKHttps');
+
+    var updatePKModal = $("#modalUpdatePK");
+    var updatePKForm = updatePKModal.find(" form");
+
+    updatePKForm.forms({
+        callback: function (resp) {
+            flog("done", resp);
+            updatePKModal.modal('hide');
+            Msg.success(resp.messages, 6000);
+            $('#certificate-types-table').reloadFragment();
+        }
+    });
+}
+
+function initCertHttps() {
+    flog('initCertHttps');
+
+    var addCertModal = $("#addWebsiteHttpsModal");
+    var addCertModalForm = addCertModal.find(" form");
+
+    addCertModalForm.forms({
+        callback: function (resp) {
+            flog("done", resp);
+            addCertModal.modal('hide');
+            Msg.success(addCertModalForm.find('[name=title]').val() + ' has been created!');
+            $('#certificate-types-table').reloadFragment();
+        }
+    });
+
+    var body = $(document.body);
+    var updateCertModal = $("#modalUpdateCert");
+    var updateCertForm = updateCertModal.find(" form");
+
+    updateCertForm.forms({
+        callback: function (resp) {
+            flog("done", resp);
+            updateCertModal.modal('hide');
+            Msg.success(resp.messages, 6000);
+            $('#certificate-types-table').reloadFragment();
+        }
+    });
+
+    body.on('click', '.btn-add-root-cert', function (e) {
+        e.preventDefault();
+        var certid = $(this).data("certid");
+        var modal = $('#modalUpdateRootCert');
+
+        modal.find(".certId").val(certid);
+
+        modal.modal("show");
+    });
+
+    var createRootCertModal = $('#modalUpdateRootCert');
+    createRootCertModal.find("form").forms({
+        callback: function (resp) {
+            flog("done", resp);
+            createRootCertModal.modal('hide');
+            Msg.success(resp.messages, 6000);
+            $('#certificate-types-table').reloadFragment();
+        }
+    });
+
+    body.on('click', '.btn-delete-rootCert', function (e) {
+        e.preventDefault();
+        var href = $(this).attr("href");
+        var certId = $(this).data("certid");
+        flog("rootCertId", href, "certId", certId);
+        if (confirm("Are you sure you want to delete this certificate?")) {
+            deleteRootCert(certId, href, $(this).closest("tr"));
+        }
+    });
+
+
+}
+
+function initCSRHttps() {
+    flog('initCSRHttps');
+
+    var genCSRModal = $("#genCsrModal");
+    var body = $(document.body);
+
+    body.on('click', '.viewCreateCsr', function (e) {
+        e.preventDefault();
+        var certId = $(this).data("certid");
+        viewCsr(certId);
+    });
+
+    body.on('click', '.btn-gen-new-csr', function (e) {
+        e.preventDefault();
+        if (confirm("Are you sure you want to create a new CSR? Please note this will also replace the current Private Key")) {
+            $("#csrTextGroup").hide();
+            genCSRModal.find('.viewCsr').hide();
+            genCSRModal.find('.csrForm').show();
+            genCSRModal.find('.btn-create').show("fade");
+        }
+    });
+
+    genCSRModal.find("form").forms({
+        validate: function () {
+            Msg.info("Sending CSR Request...");
+            return true;
+        },
+        callback: function (resp) {
+            flog("Saved Ok!");
+            Msg.info(resp.messages[0]);
+            var csrGroup = $("#csrTextGroup");
+            var csr = csrGroup.find("#csrText");
+            if (resp.data.csrText === "") {
+                csrGroup.hide();
+                genCSRModal.find('.viewCsr').hide();
+                genCSRModal.find('.csrForm').show();
+                genCSRModal.find('.btn-create').show("fade");
+            } else {
+                genCSRModal.find('.viewCsr').show();
+                genCSRModal.find('.csrForm').hide();
+                genCSRModal.find('.btn-create').hide("fade");
+                csrGroup.show();
+                csr.val(resp.data.csrText);
+            }
+        }
+    });
+}
+
+function initInputFile() {
+    flog('initInputFile');
+
+    $(document).on('change', '.btn-file :file', function () {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
+
+    $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log)
+                alert(log);
+        }
+    });
+}
+
+function initEmailServerTab() {
+    flog('initEmailServerTab');
+
+    var sendTestDkimModal = $("#modal-send-test");
+    var sendTestDkimForm = sendTestDkimModal.find("form");
+
+    sendTestDkimForm.forms({
+        validate: function () {
+            var domainName = $('#domainName').val();
+            var senderEmail = sendTestDkimForm.find('[name=senderEmail]').val();
+            var senderDomain = senderEmail.substr(senderEmail.indexOf("@") + 1);
+            if (domainName.length < 1) {
+                return {
+                    error: 1,
+                    errorFields: ["senderEmail"],
+                    errorMessages: ["Website domain can not be empty"]
+                }
+            } else if (senderDomain !== domainName) {
+                return {
+                    error: 1,
+                    errorFields: ["senderEmail"],
+                    errorMessages: ["Email domain doesn't match website domain"]
+                }
+            } else {
+                return true;
+            }
+        },
+        callback: function (resp) {
+            flog("Sent DKIM Test", resp);
+            sendTestDkimModal.modal("hide");
+            Msg.info("DKIM Successfully generated");
+            onTestResponse(resp);
+        }
+    });
+
+    var genDkimModal = $("#modal-gen-dkim");
+    var genDkimForm = genDkimModal.find(" form");
+
+    genDkimForm.forms({
+        callback: function (resp) {
+            flog("done", resp);
+            genDkimModal.modal('hide');
+            Msg.success(resp.messages, 6000);
+            $('#panel_mailserver').reloadFragment();
+        }
+    });
+
+    var uploadDkimKeyModal = $('#modalUploadPrivateKey');
+    var uploadDkimKeyForm = uploadDkimKeyModal.find('form');
+
+    uploadDkimKeyForm.forms({
+        callback: function (resp) {
+            flog("done", resp);
+            uploadDkimKeyModal.modal('hide');
+            Msg.success(resp.messages, 6000);
+            $('#panel_mailserver').reloadFragment();
+        }
+    });
+
+    $('body').on('click', '.btn-delete-dkim', function (e) {
+        e.preventDefault();
+
+        if (confirm('Are you sure you want to clear the DKIM Key?')) {
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: window.location.pathname,
+                data: {
+                    clearDKIM: true
+                },
+                success: function (resp) {
+                    flog("success", resp);
+                    $('#panel_mailserver').reloadFragment();
+                },
+                error: function (resp) {
+                    Msg.error("An error occured viewing the certificate");
+                }
+            });
+        }
+    });
+}
+
+function viewCsr(id) {
+    var certID = $("#certId");
+    var csr = $("#csrText");
+    var csrGroup = $("#csrTextGroup");
+    certID.val(id);
+    csr.val("");
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: window.location.pathname,
+        data: {
+            viewCsrId: id
+        },
+        success: function (resp) {
+            flog("success", resp);
+            if (resp.data.csrText == "") {
+                csrGroup.hide();
+                $("#genCsrModal").find('.viewCsr').hide("fade");
+                $("#genCsrModal").find('.csrForm').show("fade");
+                $("#genCsrModal").find('.btn-create').show("fade");
+            } else {
+                $("#genCsrModal").find('.viewCsr').show("fade");
+                $("#genCsrModal").find('.csrForm').hide("fade");
+                $("#genCsrModal").find('.btn-create').hide("fade");
+                csrGroup.show();
+                csr.val(resp.data.csrText);
+            }
+            $("#genCsrModal").modal('show');
+        },
+        error: function (resp) {
+            Msg.error("An error occured viewing the certificate");
+        }
+    });
+
+}
+
+function initCerts() {
+    flog('initCerts');
+
+    $("#certificate-types-table").on("click", ".cert-delete", function (e) {
+        e.preventDefault();
+        var id = $(e.target).closest("a").attr("href");
+        if (confirm("Are you sure you want to delete this certificate?")) {
+            deleteCert(id, $(e.target).closest("tr"));
+        }
+    });
+    $("#certificate-types-table").on("click", ".cert-activate", function (e) {
+        e.preventDefault();
+        var id = $(e.target).closest("a").attr("href");
+        updateCertStatus(id, "A");
+    });
+    $("#certificate-types-table").on("click", ".cert-passivate", function (e) {
+        e.preventDefault();
+        var id = $(e.target).closest("a").attr("href");
+        updateCertStatus(id, "");
+    });
+
+    $("#certificate-types-table").on("click", ".update_pk", function (e) {
+        e.preventDefault();
+
+        var id = $(e.target).closest("a").attr("href");
+        $("#updatePKid").val(id);
+        $("#privateKeyTextUpdate").text();
+
+    });
+
+    $("#certificate-types-table").on("click", ".update_cert", function (e) {
+        e.preventDefault();
+
+        var id = $(e.target).closest("a").attr("href");
+        $("#updateCertId").val(id);
+        $("#certificateTextUpdate").text();
+    });
+}
+
+function updateCertStatus(id, status) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: window.location.pathname,
+        data: {
+            certId: id,
+            updateCertStatus: status
+        },
+        success: function (data) {
+            Msg.info("Updated certificate");
+            $("#certificates-body").reloadFragment();
+        },
+        error: function (resp) {
+            Msg.error("An error occured updating the certificate");
+        }
+    });
+}
+
+function deleteCert(id, tr) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: window.location.pathname,
+        data: {
+            deleteCertId: id
+        },
+        success: function (data) {
+            Msg.info("Deleted certificate");
+            tr.remove();
+            //$("#certificates-body").reloadFragment();
+        },
+        error: function (resp) {
+            Msg.error("An error occured deleting the certificate");
+        }
+    });
+}
+
+function deleteRootCert(certId, rootCertid, tr) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: window.location.pathname,
+        data: {
+            deleteRootCertId: rootCertid,
+            certId: certId
+        },
+        success: function (data) {
+            Msg.info("Deleted certificate");
+            var tbody = tr.closest("tbody");
+            tr.remove();
+            var rows = tbody.find("tr");
+            $(".rootCertCount").text(rows.size());
+            if (rows.size() > 0) {
+                rows.each(function (index, item) {
+                    $(item).find("td.first").text(index + 1);
+                });
+            } else {
+                tbody.append('<tr>'
+                    + '<td colspan="3">You have no root/intermediate certificates</td>'
+                    + '</tr>');
+            }
+        },
+        error: function (resp) {
+            Msg.error("An error occured deleting the certificate");
+        }
+    });
+}
+
+function initSwitchPublic() {
+    flog('initSwitchPublic');
+
+    $('.make-switch input').on('change switchChange', function(e) {
+        flog("switch", e.target);
+        e.preventDefault();
+
+        var label = $(this);
+        var wrapper = label.parents('.make-switch');
+        var href = wrapper.attr('data-link');
+
+        setTimeout(function() {
+            var isChecked = wrapper.find('input:checked').val() === 'true';
+            flog("checked=", isChecked);
+            setRepoPublicAccess(href, isChecked);
+        }, 0);
+    });
+}
+
+function setRepoPublicAccess(href, isPublic) {
+    $.ajax({
+        type: 'POST',
+        data: {isPublic: isPublic},
+        url: href,
+        success: function(data) {
+        },
+        error: function(resp) {
+            flog("error updating: ", href, resp);
+            Msg.error('Sorry, couldnt update public access: ' + resp);
+            window.location.reload();
+        }
+    });
 }
 
 function initManageWebsiteImage() {
---
+++ src/main/webapp/templates/apps/admin/manageWebsites.js
@@ -0,0 +1,104 @@
+function initManageWebsites() {
+    flog('initManageWebsites');
+
+    initAddWebsite();
+    initRename();
+    initSwitchPublic();
+}
+
+function initAddWebsite() {
+    flog('initAddWebsite');
+
+    var modal = $("#addWebsiteModal");
+    var form = modal.find("form");
+
+    form.forms({
+        callback: function (resp) {
+            flog("done", resp);
+            modal.modal('hide');
+            Msg.success(form.find('[name=newName]').val() + ' is created!');
+            $('#website-wrapper').reloadFragment();
+        }
+    });
+}
+
+function initRename() {
+    flog('initRename');
+
+    $('body').on('click', '.btn-rename-website', function (e) {
+        e.preventDefault();
+        var btn = $(this);
+        var href = btn.attr('href');
+        var newName = prompt("Please enter a new name for " + href);
+        if (newName) {
+            if (newName.length > 0 && newName != href) {
+                doRename(window.location.pathname + href, newName, function () {
+                    $('#website-wrapper').reloadFragment();
+                });
+            }
+        }
+    });
+}
+
+function initSwitchPublic() {
+    flog('initSwitchPublic');
+
+    $('td.public input').on('change switchChange', function(e) {
+        flog("switch", e.target);
+        e.preventDefault();
+
+        var label = $(this);
+        var wrapper = label.parents('.make-switch');
+        var href = wrapper.attr('data-link');
+
+        setTimeout(function() {
+            var isChecked = wrapper.find('input:checked').val() === 'true';
+            flog("checked=", isChecked);
+            setRepoPublicAccess(href, isChecked);
+        }, 0);
+    });
+}
+
+function setRepoPublicAccess(href, isPublic) {
+    $.ajax({
+        type: 'POST',
+        data: {isPublic: isPublic},
+        url: href,
+        success: function(data) {
+        },
+        error: function(resp) {
+            flog("error updating: ", href, resp);
+            Msg.error('Sorry, couldnt update public access: ' + resp);
+            window.location.reload();
+        }
+    });
+}
+
+function doRename(href, newName, callback) {
+    var newUrl = getParentHref(href) + "/" + newName;
+    var targetUrl = href;
+    if (!targetUrl.endsWith("/")) {
+        targetUrl += "/";
+    }
+    targetUrl += "_DAV/MOVE";
+    flog("renameFile2", newUrl);
+    $.ajax({
+        type: 'POST',
+        url: targetUrl,
+        dataType: 'text',
+        data: "destination=" + newUrl,
+        success: function () {
+            flog('success');
+            if (callback) {
+                log("callback1");
+                callback();
+            }
+            Msg.success('Success');
+        },
+        error: function (resp) {
+            flog("failed", resp);
+            ajaxLoadingOff();
+            Msg.error("Failed to rename website, a website with that name might already exist.");
+        }
+    });
+}
