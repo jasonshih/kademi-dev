981e9239e60927a70f254af911b5388c9f5075e4
anh <anhnt38@gmail.com>
2016-04-11 10:48:58 +0700

    https://github.com/Kademi/kademi-dev/issues/1327

--- src/main/webapp/static/ktoolbar/ktoolbar.js
+++ src/main/webapp/static/ktoolbar/ktoolbar.js
@@ -2,6 +2,8 @@ $(function () {
     var htmleditor = $('.htmleditor');
     var ktoolbar = $('#ktoolbar');
     var btnEditInline = ktoolbar.find('.btn-inline-edit');
+    var btnSave = ktoolbar.find('.btn-inline-edit-save');
+    var btnDone = ktoolbar.find('.btn-inline-edit-done');
     var body = $(document.body);
     var head = $('head');
 
@@ -20,6 +22,9 @@ $(function () {
             count++;
 
             if (count === 6) {
+                btnSave.removeClass('hide');
+                btnDone.removeClass('hide');
+                btnEditInline.addClass('hide');
                 htmleditor.keditor({
                     snippetsUrl: '/_components/web/snippets.html',
                     onContentChanged: function () {
@@ -62,4 +67,64 @@ $(function () {
         $.getScriptOnce('/static/nicescroll/3.6.6/jquery.nicescroll.min.js', f);
     });
 
+    btnSave.on('click', function(e){
+        e.preventDefault();
+
+        $.ajax({
+            url: window.location.pathname,
+            type: 'post',
+            dataType: 'json',
+            data: {
+                body: htmleditor.keditor('getContent')
+            },
+            success: function (resp) {
+                if(resp && resp.status){
+                    flog('inline editing saved', resp);
+                    body.removeClass('content-changed');
+                    Msg.success('Saved');
+                    if(btnSave.hasClass('keditor-needs-destroy')){
+                        btnSave.removeClass('keditor-needs-destroy');
+                        btnDone.trigger('ktoolbar.done');
+                    }
+                }else{
+                    flog('inline editing error saving', resp);
+                    Msg.error('Could not save your changes. Please try again');
+                }
+            },
+            error: function(err){
+                flog('inline editing error saving', resp);
+                Msg.error('Could not save your changes. Please try again');
+            }
+        })
+    });
+
+    btnDone.on('click', function(e) {
+        e.preventDefault();
+
+        if(body.hasClass('content-changed')){
+            var c = confirm('Would you like to save changes before leaving the editor?');
+            if(c){
+                btnSave.addClass('keditor-needs-destroy').trigger('click');
+            } else{
+                btnDone.trigger('ktoolbar.done');
+            }
+        }else{
+            btnDone.trigger('ktoolbar.done');
+        }
+
+    });
+
+    btnDone.on('ktoolbar.done', function(){
+        // Todo: call Keditor destroy or disable method here
+        // Just a workaround
+        setTimeout(function(){
+            window.location.href = window.location.href;
+        },500);
+    });
+
+    window.onbeforeunload = function () {
+        if(body.hasClass('content-changed')){
+            return 'Are you sure to leave the editor? You will lose any unsaved changes';
+        }
+    };
 });
