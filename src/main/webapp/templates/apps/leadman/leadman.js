279d5e6c60dc4cf5447cd95ff0314f6f88809a16
DucDHM <ducdhm@gmail.com>
2016-05-04 23:40:09 +0700

    Update

--- src/main/webapp/templates/apps/leadman/leadman.js
+++ src/main/webapp/templates/apps/leadman/leadman.js
@@ -606,7 +606,6 @@ function bytesToSize(bytes) {
     var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
-;
 
 function initNewContactForm() {
     var modal = $('#newContactModal');
