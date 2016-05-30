6dc2c2b1b09622e065436050512265c05c777a04
DucDHM <ducdhm@gmail.com>
2016-05-30 11:39:10 +0700

    https://github.com/Kademi/kademi-dev/issues/1895

--- src/main/webapp/templates/apps/admin/viewEmailInBrowserComponent.js
+++ src/main/webapp/templates/apps/admin/viewEmailInBrowserComponent.js
@@ -2,23 +2,40 @@
     var KEditor = $.keditor;
     var flog = KEditor.log;
 
-    KEditor.components['viewEmailInBrowser'] = {
-        init: function (contentArea, container, component, keditor) {
-            // Do nothing
-        },
-
-        getContent: function (component, keditor) {
-            var componentContent = component.children('.keditor-component-content');
-            return componentContent.html();
-        },
-
-        destroy: function (component, keditor) {
-            // Do nothing
+    KEditor.components['viewEmailInBrowser'] = $.extend({}, KEditor.components['text'], {
+        options: {
+            title: false,
+            allowedContent: true, // DISABLES Advanced Content Filter. This is so templates with classes are allowed through
+            bodyId: 'editor',
+            templates_replaceContent: false,
+            toolbarGroups: [
+                {name: 'document', groups: ['mode', 'document', 'doctools']},
+                {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
+                {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+                {name: 'paragraph', groups: ['align', 'bidi', 'paragraph']},
+                {name: 'clipboard', groups: ['clipboard', 'undo']},
+                '/',
+                {name: 'styles', groups: ['styles']},
+                {name: 'colors', groups: ['colors']},
+                {name: 'tools', groups: ['tools']},
+                {name: 'others', groups: ['others']},
+                {name: 'about', groups: ['about']}
+            ],
+            extraPlugins: 'lineheight,onchange',
+            removePlugins: 'table,magicline,tabletools',
+            removeButtons: 'Save,NewPage,Preview,Print,Templates,PasteUnsubscribe,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,HiddenField,ImageButton,Button,Select,Unsubscribearea,UnsubscribeField,Radio,Checkbox,Outdent,Indent,Blockquote,CreateDiv,Language,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,BGColor,Maximize,About,ShowBlocks,BidiLtr,BidiRtl,Flash,Image,Subscript,Superscript,Anchor,Styles,Format',
+            enterMode: CKEDITOR.ENTER_DIV,
+            forceEnterMode: true,
+            filebrowserBrowseUrl: '/static/fckfilemanager/browser/default/browser.html?Type=Image&Connector=/fck_connector.html',
+            filebrowserUploadUrl: '/uploader/upload',
+            format_tags: 'p;h1;h2;h3;h4;h5;h6',
+            stylesSet: 'myStyles:' + stylesPath,
+            line_height: '1;1.2;1.5;2;2.2;2.5'
         },
 
         settingEnabled: true,
 
-        settingTitle: 'View email in browser Settings',
+        settingTitle: 'View In Browser Settings',
 
         initSettingForm: function (form, keditor) {
             flog('initSettingForm "viewEmailInBrowser" component');
@@ -49,11 +66,7 @@
 
             var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
             form.find('.message').val(dataAttributes['data-message']);
-        },
-
-        hideSettingForm: function (form, keditor) {
-            // Do nothing
         }
-    };
+    });
 
 })(jQuery);
