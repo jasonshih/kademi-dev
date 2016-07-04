43cab4c5e894a5bd24ef438e5aa60f23c047ea00
dylan <tuzzmaniandevil@gmail.com>
2016-07-04 17:24:34 +1200

    https://github.com/Kademi/kademi-dev/issues/2078

--- src/main/webapp/templates/apps/aws/manageHostedZone.js
+++ src/main/webapp/templates/apps/aws/manageHostedZone.js
@@ -1,7 +1,34 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function () {
+    function initNewRecordModal() {
+        var modal = $('#addRecordModal');
+        var form = modal.find('form');
 
+        form.forms({
+            onSuccess: function (resp) {
+                reloadRecords();
+                form.trigger('reset');
+                $('#record-type').change();
+                modal.modal('hide');
+            }
+        });
+    }
 
+    function initRecordTypeSelect() {
+        $('#record-type').on('change', function (e) {
+            var inp = $(this);
+
+            $('[id^=record-type-]').hide();
+
+            $('#record-type-' + inp.val()).show();
+        });
+    }
+
+    function reloadRecords() {
+        $('#records-tbody').reloadFragment();
+    }
+
+    $(function () {
+        initNewRecordModal();
+        initRecordTypeSelect();
+    });
+})();
