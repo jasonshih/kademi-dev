de2b05608f29ff111ea4eb06e09989b5ea940d5b
Brad Mac <brad@mcevoysoftware.co.nz>
2016-07-12 11:47:45 +1200

    https://github.com/Kademi/kademi-dev/issues/2095

---
+++ src/main/webapp/templates/apps/content/components/portletComponent.js
@@ -0,0 +1,29 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['portlet'] = {
+        settingEnabled: true,
+        settingTitle: 'Portlet Settings',
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "portlet" component');
+
+            var self = this;
+
+            $.ajax({
+                url: '_components/portlet?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                }
+            });
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "portlet" component');
+
+            var self = this;
+        }
+    };
+
+})(jQuery);
---
+++ src/main/webapp/templates/apps/forums/components/commentsComponent.js
@@ -0,0 +1,31 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['comments'] = {
+        settingEnabled: true,
+        settingTitle: 'Page Comments Settings',
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "comments" component');
+
+            var self = this;
+
+            $.ajax({
+                url: '_components/comments?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                }
+            });
+        },
+        initSelect: function (aggsSelect, selectedQuery, selectedAgg) {
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "comments" component');
+
+            var self = this;
+        }
+    };
+
+})(jQuery);
