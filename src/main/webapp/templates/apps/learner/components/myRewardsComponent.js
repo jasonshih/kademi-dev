c17f92dae96b579d37e145cdc5228ba58c2187bc
anh <anhnt38@gmail.com>
2016-07-18 17:13:58 +0700

    https://github.com/Kademi/kademi-dev/issues/2095 Learning app

---
+++ src/main/webapp/templates/apps/learner/components/myRewardsComponent.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Anh on 7/18/2016.
+ */
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['myRewards'] = {
+        settingEnabled: true,
+
+        settingTitle: 'My Rewards Settings',
+
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "myRewards" component', form, keditor);
+
+            $.ajax({
+                url: '_components/myRewards?settings',
+                type: 'get',
+                dataType: 'html',
+                success: function (resp) {
+                    form.html(resp);
+
+                    //form.find('[name=showCourseBody]').on('click', function(e){
+                    //    var component = keditor.getSettingComponent();
+                    //    var dynamicElement = component.find('[data-dynamic-href]');
+                    //    component.attr('data-show-course-body', this.value === 'true');
+                    //    keditor.initDynamicContent(dynamicElement);
+                    //});
+                }
+            });
+        },
+
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "myRewards" component', form, component, keditor);
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            //form.find('[name=showCourseBody][value='+dataAttributes['data-show-course-body']+']').prop('checked', true);
+        }
+    };
+
+})(jQuery);
