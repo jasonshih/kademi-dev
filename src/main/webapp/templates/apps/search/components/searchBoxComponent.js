c65cef3a9794029f8ca1f1c179afb55fd05db0d0
anh <anhnt38@gmail.com>
2016-08-22 17:30:04 +0700

    https://github.com/Kademi/kademi-dev/issues/2220

---
+++ src/main/webapp/templates/apps/search/components/searchBoxComponent.js
@@ -0,0 +1,36 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['searchBox'] = {
+        settingEnabled: true,
+        settingTitle: 'Search box Settings',
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "searchBox" component');
+
+            var self = this;
+
+            $.ajax({
+                url: '_components/searchBox?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+
+                    form.find('[name=scope]').on('change', function(e){
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        component.attr('data-scope', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+                }
+            });
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "searchBox" component');
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            form.find('[name=scope]').val(dataAttributes['data-scope']);
+        }
+    };
+
+})(jQuery);
