0788f580c7bd9f66a9fd202f39b0bbd3199c3c19
DucDHM <ducdhm@gmail.com>
2016-08-23 15:08:36 +0700

    https://github.com/Kademi/kademi-dev/issues/2227 Nodes for UserApp

---
+++ src/main/webapp/templates/apps/user/jb/addToGroupNode.js
@@ -0,0 +1,34 @@
+JBNodes['addToGroup'] = {
+    title: '<i class="fa fa-pencil-square-o"></i> <span class="node-type">Add to group</span>',
+    previewUrl: '/theme/apps/user/jb/addToGroupNode.png',
+    ports: {
+        nextNodeId: {
+            label: 'then',
+            title: 'When completed',
+            maxConnections: 1
+        }
+    },
+
+    initSettingForm: function (form) {
+        form.forms({
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
+    }
+};
---
+++ src/main/webapp/templates/apps/user/jb/removeProfileNode.js
@@ -0,0 +1,34 @@
+JBNodes['removeProfile'] = {
+    title: '<i class="fa fa-pencil-square-o"></i> <span class="node-type">Remove profile</span>',
+    previewUrl: '/theme/apps/user/jb/removeProfileNode.png',
+    ports: {
+        nextNodeId: {
+            label: 'then',
+            title: 'When completed',
+            maxConnections: 1
+        }
+    },
+
+    initSettingForm: function (form) {
+        form.forms({
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
+    }
+};
