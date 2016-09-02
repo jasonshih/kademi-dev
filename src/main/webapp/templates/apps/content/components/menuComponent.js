0e72472e614c007fc74cc9e6477f025b388a95ee
Brad Mac <brad@mcevoysoftware.co.nz>
2016-09-02 12:18:25 +1200

    https://github.com/Kademi/kademi-dev/issues/2300

--- src/main/webapp/templates/apps/content/components/menuComponent.js
+++ src/main/webapp/templates/apps/content/components/menuComponent.js
@@ -16,6 +16,13 @@
                 dataType: 'HTML',
                 success: function (resp) {
                     form.html(resp);
+                    form.find('[name=logo]').on('change', function(e){
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        component.attr('data-logo', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+                    
                 }
             });
         },
@@ -23,6 +30,8 @@
             flog('showSettingForm "menu" component');
 
             var self = this;
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            form.find('[name=logo]').val(dataAttributes['data-logo']);
         }
     };
 
