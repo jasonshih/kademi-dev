7cc627d685873721ff44e12b3ac64f7f8258c2e6
DucDHM <ducdhm@gmail.com>
2016-09-15 09:48:25 +0700

    z-index issue with jsplumb-connector

--- src/main/webapp/templates/apps/leadman/leadman.js
+++ src/main/webapp/templates/apps/leadman/leadman.js
@@ -69,7 +69,7 @@ $(function () {
                 }
             });
         }
-    })
+    });
     $("body").on("click", ".autoFillText", function (e) {
         e.preventDefault();
         var target = $(e.target).closest("a");
