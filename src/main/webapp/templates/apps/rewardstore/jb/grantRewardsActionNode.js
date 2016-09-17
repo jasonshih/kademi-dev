3db9c67406e4e704a541656d3ab9c2fded37f8f5
brad <brad@brad-little-laptop>
2016-09-17 19:39:37 +1200

    https://github.com/Kademi/kademi-dev/issues/2386

--- src/main/webapp/templates/apps/rewardstore/jb/grantRewardsActionNode.js
+++ src/main/webapp/templates/apps/rewardstore/jb/grantRewardsActionNode.js
@@ -1,6 +1,6 @@
-JBNodes['calendarEventAction'] = {
-    title: '<i class="fa fa-gift"></i> <span class="node-type">Give rewards</span>',
-    previewUrl: '/theme/apps/rewardstore/jb/grantRewardsActionNode.png',
+JBNodes['grantRewards'] = {
+    title: '<i class="fa fa-gift"></i> <span class="node-type">Grant rewards</span>',
+    previewUrl: '/theme/apps/rewardstore/jb/grantRewardsAction.png',
     ports: {
         nextNodeId: {
             label: 'then',
@@ -12,59 +12,25 @@ JBNodes['calendarEventAction'] = {
     settingEnabled: true,
 
     initSettingForm: function (form) {
-        form.append(
-            '<div class="form-group">' +
-            '    <div class="col-md-12">' +
-            '        <label>Task name</label>' +
-            '        <input type="text" class="form-control calendarName" value="" />' +
-            '    </div>' +
-            '</div>' +
-            '<div class="form-group">' +
-            '    <div class="col-md-12">' +
-            '        <label>Title</label>' +
-            '        <input type="text" class="form-control titleMvel" value="" />' +
-            '        <small class="text-muted help-block">Can use MVEL syntax</small>' +
-            '    </div>' +
-            '</div>' +
-            '<div class="form-group">' +
-            '    <div class="col-md-12">' +
-            '        <label for="expression">Description</label>' +
-            '        <textarea name="expression"  class="form-control descriptionMvel" rows="5"></textarea>' +
-            '        <small class="text-muted help-block">Can use MVEL syntax</small>' +
-            '    </div>' +
-            '</div>' +
-            '<div class="form-group">' +
-            '    <div class="col-md-12">' +
-            '        <label for="expression">Date</label>' +
-            '        <input type="text" class="form-control dateMvel" value="" />' +
-            '        <small class="text-muted help-block">Can use MVEL syntax</small>' +
-            '    </div>' +
-            '</div>'
-        );
-
         form.forms({
-            allowPostForm: false,
-            onValid: function () {
-                var calendarName = form.find('.calendarName').val();
-                var titleMvel = form.find('.titleMvel').val();
-                var descriptionMvel = form.find('.descriptionMvel').val();
-                var dateMvel = form.find('.dateMvel').val();
-
-                JBApp.currentSettingNode.calendarName = calendarName || null;
-                JBApp.currentSettingNode.titleMvel = titleMvel || null;
-                JBApp.currentSettingNode.descriptionMvel = descriptionMvel || null;
-                JBApp.currentSettingNode.dateMvel = dateMvel || null;
-                JBApp.saveFunnel('Funnel is saved');
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
                 JBApp.hideSettingPanel();
             }
         });
     },
 
     showSettingForm: function (form, node) {
-        form.find('.calendarName').val(node.calendarName !== null ? node.calendarName : '');
-        form.find('.titleMvel').val(node.titleMvel !== null ? node.titleMvel : '');
-        form.find('.descriptionMvel').val(node.descriptionMvel !== null ? node.descriptionMvel : '');
-        form.find('.dateMvel').val(node.dateMvel !== null ? node.dateMvel : '');
-        JBApp.showSettingPanel(node);
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
     }
 };
