825f33a97f0261c4f6f5ff88080e394fe33a626a
brad <brad@brad-little-laptop>
2016-09-19 10:55:27 +1200

    start funnel node

---
+++ src/main/webapp/templates/apps/funnels/jb/startFunnelActionNode.js
@@ -0,0 +1,36 @@
+JBNodes['startFunnel'] = {
+    title: '<i class="fa fa-gift"></i> <span class="node-type">Start journey</span>',
+    previewUrl: '/theme/apps/funnels/jb/startFunnelsAction.png',
+    ports: {
+        nextNodeId: {
+            label: 'then',
+            title: 'When completed',
+            maxConnections: 1
+        }
+    },
+
+    settingEnabled: true,
+
+    initSettingForm: function (form) {
+        form.forms({
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
+    }
+};
