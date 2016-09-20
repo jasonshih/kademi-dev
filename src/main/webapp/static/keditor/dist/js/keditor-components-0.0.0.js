4fc89f5bc9280c6cad99d9aadcc686448e6ab11f
DucDHM <ducdhm@gmail.com>
2016-09-20 23:13:08 +0700

    https://github.com/Kademi/kademi-dev/issues/2395

--- src/main/webapp/static/keditor/dist/js/keditor-components-0.0.0.js
+++ src/main/webapp/static/keditor/dist/js/keditor-components-0.0.0.js
@@ -823,33 +823,6 @@
         settingEnabled: true,
 
         settingTitle: 'Jumbotron Settings',
-        options: {
-            toolbarGroups: [
-                {name: 'document', groups: ['mode', 'document', 'doctools']},
-                {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
-                {name: 'forms', groups: ['forms']},
-                {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
-                {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
-                {name: 'links', groups: ['links']},
-                {name: 'insert', groups: ['insert']},
-                '/',
-                {name: 'clipboard', groups: ['clipboard', 'undo']},
-                {name: 'styles', groups: ['styles']},
-                {name: 'colors', groups: ['colors']},
-                {name: 'tools', groups: ['tools']},
-                {name: 'others', groups: ['others']}
-            ],
-            title: false,
-            allowedContent: true, // DISABLES Advanced Content Filter. This is so templates with classes: allowed through
-            bodyId: 'editor',
-            templates_replaceContent: false,
-            enterMode: 'P',
-            forceEnterMode: true,
-            format_tags: 'p;h1;h2;h3;h4;h5;h6',
-            removePlugins: 'table,magicline,tabletools',
-            removeButtons: 'Save,NewPage,Preview,Print,Templates,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,HiddenField,ImageButton,Button,Select,Textarea,TextField,Radio,Checkbox,Outdent,Indent,Blockquote,CreateDiv,Language,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,BGColor,Maximize,About,ShowBlocks,BidiLtr,BidiRtl,Flash,Image,Subscript,Superscript,Anchor',
-            minimumChangeMilliseconds: 100
-        },
         init: function(contentArea, container, component, keditor){
             var self = this;
             var options = self.options;
@@ -871,7 +844,7 @@
                 }
             });
 
-            var editor = componentContent.ckeditor(self.options).editor;
+            var editor = componentContent.ckeditor(keditor.options.ckeditorOptions).editor;
             editor.on('instanceReady', function () {
                 flog('CKEditor is ready', component);
 
@@ -879,6 +852,19 @@
                     options.onComponentReady.call(contentArea, component, editor);
                 }
             });
+        },
+
+        getContent: function (component, keditor) {
+            flog('getContent "jumbotron" component', component);
+
+            var componentContent = component.find('.keditor-component-content');
+            var id = componentContent.attr('id');
+            var editor = CKEDITOR.instances[id];
+            if (editor) {
+                return editor.getData();
+            } else {
+                return componentContent.html();
+            }
         },
 
         initSettingForm: function (form, keditor) {
@@ -1078,6 +1064,7 @@
 
         });
     }
+
 })(jQuery);
 
 
@@ -1376,34 +1363,6 @@
     // Text component
     // ---------------------------------------------------------------------
     KEditor.components['text'] = {
-        options: {
-            toolbarGroups: [
-                {name: 'document', groups: ['mode', 'document', 'doctools']},
-                {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
-                {name: 'forms', groups: ['forms']},
-                {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
-                {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
-                {name: 'links', groups: ['links']},
-                {name: 'insert', groups: ['insert']},
-                '/',
-                {name: 'clipboard', groups: ['clipboard', 'undo']},
-                {name: 'styles', groups: ['styles']},
-                {name: 'colors', groups: ['colors']},
-                {name: 'tools', groups: ['tools']},
-                {name: 'others', groups: ['others']}
-            ],
-            title: false,
-            allowedContent: true, // DISABLES Advanced Content Filter. This is so templates with classes: allowed through
-            bodyId: 'editor',
-            templates_replaceContent: false,
-            enterMode: 'P',
-            forceEnterMode: true,
-            format_tags: 'p;h1;h2;h3;h4;h5;h6',
-            removePlugins: 'table,magicline,tabletools',
-            removeButtons: 'Save,NewPage,Preview,Print,Templates,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,HiddenField,ImageButton,Button,Select,Textarea,TextField,Radio,Checkbox,Outdent,Indent,Blockquote,CreateDiv,Language,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,BGColor,Maximize,About,ShowBlocks,BidiLtr,BidiRtl,Flash,Image,Subscript,Superscript,Anchor',
-            minimumChangeMilliseconds: 100
-        },
-
         init: function (contentArea, container, component, keditor) {
             flog('init "text" component', component);
 
@@ -1427,7 +1386,7 @@
                 }
             });
 
-            var editor = componentContent.ckeditor(self.options).editor;
+            var editor = componentContent.ckeditor(keditor.options.ckeditorOptions).editor;
             editor.on('instanceReady', function () {
                 flog('CKEditor is ready', component);
 
