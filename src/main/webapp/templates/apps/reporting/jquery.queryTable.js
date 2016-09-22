a2c7eb9bc89b9517ef6bdce1b32b0d9b9a5a6d9c
Brad Mac <brad@mcevoysoftware.co.nz>
2016-09-22 16:25:52 +1200

    https://github.com/Kademi/kademi-dev/issues/1822

--- src/main/webapp/templates/apps/reporting/jquery.queryTable.js
+++ src/main/webapp/templates/apps/reporting/jquery.queryTable.js
@@ -8,10 +8,10 @@
     $.fn.queryTable = function (options) {
         var container = this;
 
-        flog("pieChartAgg", container);
+        flog("queryTable", container);
         container.each(function (i, n) {
             var cont = $(n);
-            flog("init pie chart events", cont);
+            flog("init queryTable chart events", cont);
             var config = $.extend({}, DEFAULT_PIECHART_OPTIONS, options);
 
             var opts = {
@@ -35,14 +35,41 @@
             }
 
             $(document).on('pageDateChanged', function (e, startDate, endDate) {
-                flog("piechart date change", e, startDate, endDate);
+                flog("queryTable date change", e, startDate, endDate);
 
                 loadGraphData(queryHref, aggName, {
                     startDate: startDate,
                     endDate: endDate
                 }, cont, config);
             });
+
+            cont.on('click', 'a', function (e) {
+                e.preventDefault();
+
+                var href = '/_components/queryTable';
+//                var href = $(this).attr('href');                
+//                if (href.indexOf('/_components/queryTable/') === -1) {
+//                    href = '/_components/queryTable' + href;
+//                }
+                var query = cont.parents('[data-type=component-queryTable]').attr('data-query');
+                var perPage = cont.parents('[data-type=component-queryTable]').attr('data-items-per-page');
+                var height = cont.parents('[data-type=component-queryTable]').attr('data-height');
+                href += '&data-query=' + encodeURI(query);
+                href += '&data-items-per-page=' + encodeURI(perPage);
+                href += '&data-height=' + encodeURI(height);
+                
+                var tbody = cont.find("tbody");
+                tbody.reloadFragment({
+                    url: href,
+                    whenComplete: function (resp) {
+                        comp.find('.panel-body').html($(resp).find('.panel-body').html());
+                    }
+                });
+            });
         });
     };
 
 })(jQuery);
+
+
+
---
+++ src/main/webapp/templates/apps/search/manageQuery.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    var editor = ace.edit("queryText");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/javascript");
+
+    function save(callback) {
+        var json = editor.getValue();
+        flog("save", json);
+        $.ajax({
+            type: 'POST',
+            data: {
+                query: json
+            },
+            dataType: "json",
+            url: "",
+            success: function (data) {
+                flog("done save");
+                Msg.info("Saved");
+                if (callback) {
+                    callback();
+                }
+            },
+            error: function (resp) {
+                Msg.error("An error occured while saving");
+            }
+        });
+    }
+
+
+    var heightUpdateFunction = function () {
+
+        // http://stackoverflow.com/questions/11584061/
+        var newHeight =
+                editor.getSession().getScreenLength()
+                * editor.renderer.lineHeight
+                + editor.renderer.scrollBar.getWidth();
+
+        $('#queryText').height(newHeight.toString() + "px");
+
+        // This call is required for the editor to fix all of
+        // its inner structure for adapting to a change in size
+        editor.resize();
+    };
+
+    // Set initial size to match initial content
+    heightUpdateFunction();
+
+    // Whenever a change happens inside the ACE editor, update
+    // the size again
+    editor.getSession().on('change', heightUpdateFunction);
+
+    $("body").on("click", ".runQuery", function (e) {
+        flog("Save, then run");
+        save(function () {
+            var dateOptions = getPageDateRange();
+            flog("Saved, now run", dateOptions);
+            var newHref = window.location.pathname;
+            if( dateOptions ) {
+                newHref += "?" + $.param(dateOptions); // from queryComponents.js, injected by ReportingApp
+            }
+            flog("Saved, now run2", newHref);
+            Msg.info("Running..", newHref);
+            $("#queryResults").reloadFragment({
+                url: newHref,
+                whenComplete: function () {
+                    flog("Finished");
+                    Msg.info("Query executed");
+                }
+            });
+        });
+    });
+
+    $(document).on('pageDateChanged', function (e, startDate, endDate) {
+        flog("page date changed", startDate, endDate);
+        var dateOptions = {
+            startDate: startDate,
+            endDate: endDate
+        };
+        var newHref = window.location.pathname + "?" + $.param(dateOptions); // from queryComponents.js, injected by ReportingApp
+        Msg.info("Running..", newHref);
+        $("#queryResults").reloadFragment({
+            url: newHref,
+            whenComplete: function () {
+                Msg.info("Query executed");
+            }
+        });
+    });
+
+
+
+    $("body").on("click", ".saveQuery", function (e) {
+        save();
+    });
+
+
+});
