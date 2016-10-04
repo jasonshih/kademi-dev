bf5269a636db16f8e23b943b7c9b189d1f49c31d
Brad Mac <brad@mcevoysoftware.co.nz>
2016-10-04 17:18:33 +1300

    https://github.com/Kademi/kademi-dev/issues/2489

--- src/main/webapp/templates/apps/funnels/jb/beginNode.js
+++ src/main/webapp/templates/apps/funnels/jb/beginNode.js
@@ -9,5 +9,28 @@ JBNodes['begin'] = {
         }
     },
 
-    settingEnabled: false
+    settingEnabled: true,    
+
+    initSettingForm: function (form) {
+        form.forms({
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
+    }
 };
