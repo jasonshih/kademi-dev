12adbceb5f4d52863633b3c5d4d8b33d6d0ff432
DucDHM <ducdhm@gmail.com>
2016-10-06 15:13:51 +0700

    https://github.com/Kademi/kademi-dev/issues/2494

---
+++ src/main/webapp/templates/apps/user/components/passwordDoResetComponent.js
@@ -0,0 +1,9 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['passwordDoReset'] = {
+        settingEnabled: false
+    };
+
+})(jQuery);
---
+++ src/main/webapp/templates/apps/user/components/passwordResetComponent.js
@@ -0,0 +1,9 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['passwordReset'] = {
+        settingEnabled: false
+    };
+
+})(jQuery);
