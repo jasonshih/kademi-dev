c217ac2e4c0f611df5caf66473ff7f9ae25827ea
brad <brad@brad-hp-linux>
2016-11-02 00:39:56 +1300

    https://github.com/Kademi/kademi-dev/issues/2583

---
+++ src/main/webapp/templates/apps/leadman/jb/taskGoalNode.js
@@ -0,0 +1,50 @@
+JBNodes['taskGoal'] = {
+    title: '<i class="fa fa-trophy"></i> <span class="node-type">Task</span>',
+    previewUrl: '/theme/apps/leadman/jb/taskGoalNode.png',
+    ports: {
+        timeoutNode: {
+            label: 'timeout',
+            title: 'When timeout',
+            maxConnections: 1
+        },
+        nextNodeId: {
+            label: 'then',
+            title: 'When completed',
+            maxConnections: 1
+        }
+    },
+
+    settingEnabled: true,
+
+    initSettingForm: function (form) {
+        form.append(
+            '<div class="form-group">' +
+            '    <div class="col-md-12">' +
+            '        <label>Task name</label>' +
+            '        <input type="text" class="form-control task-name" value="" />' +
+            '    </div>' +
+            '</div>' + JBApp.standardGoalSettingControls
+        );
+
+        JBApp.initStandardGoalSettingControls(form);
+
+        form.forms({
+            allowPostForm: false,
+            onValid: function () {
+                var taskName = form.find('.task-name').val();
+                JBApp.currentSettingNode.taskName = taskName || null;
+
+                JBApp.saveStandardGoalSetting(form);
+
+                JBApp.saveFunnel('Funnel is saved');
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        JBApp.showStandardGoalSettingControls(form, node);
+        form.find('.task-name').val(node.taskName !== null ? node.taskName : '');
+        JBApp.showSettingPanel(node);
+    }
+};
