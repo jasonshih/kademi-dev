96b1c229824770658cfe3d64d98b4eb44a5dcffd
dylan <tuzzmaniandevil@gmail.com>
2016-11-15 14:04:33 +1300

    https://github.com/Kademi/kademi-dev/issues/1303

---
+++ src/main/webapp/templates/apps/products/manageLocation.js
@@ -0,0 +1,42 @@
+(function () {
+
+    function initUpdateStock() {
+        $('body').on('click', '.adjustStock', function (e) {
+            e.preventDefault();
+
+            var btn = $(this);
+            var row = btn.closest('tr');
+            var href = row.data('href');
+            var stock = row.data('stock');
+
+            var newStock = prompt('Please enter new stock level', stock);
+
+            var newStockInt = parseint(newStock);
+            if (!isNaN(newStockInt) && newStockInt >= 0) {
+                $.ajax({
+                    url: href,
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
+                        updateStock: newStockInt
+                    },
+                    success: function (resp) {
+                        if (resp.status) {
+                            Msg.success('Successfully updated stock level');
+                        } else {
+                            Msg.error('Error updating stock level: ' + textStatus);
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        Msg.error('Error updating stock level: ' + textStatus);
+                    }
+                });
+            }
+        });
+    }
+
+    // Run Init Functions
+    $(function () {
+        initUpdateStock();
+    });
+})();
