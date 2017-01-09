eb0142d8a9645b8901b2cbf5161cb797dadbe145
DucDHM <ducdhm@gmail.com>
2017-01-09 11:22:58 +0700

    Update

--- src/main/webapp/templates/apps/admin/edmEditor.js
+++ src/main/webapp/templates/apps/admin/edmEditor.js
@@ -690,9 +690,11 @@ function getEdmContent() {
     }
 
     return (
-        '<!DOCTYPE HTML>\n' +
-        '<html>\n' +
+        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n' +
+        '<html xmlns="http://www.w3.org/1999/xhtml">\n' +
         '    <head>\n' +
+        '        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n' +
+        '        <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n' +
         '        <title>Kademi EDM Title</title>\n' +
         '        <style type="text/css">\n' +
         '            ' + $('#edm-style').html().trim() + '\n' +
