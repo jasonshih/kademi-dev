b324077d2d0a420267c60af6d5ccf270a92c133d
DucDHM <ducdhm@gmail.com>
2017-01-12 15:59:28 +0700

    https://github.com/Kademi/kademi-dev/issues/2793

--- src/main/webapp/templates/apps/rewardstore/jquery.pointsActivityChart.js
+++ src/main/webapp/templates/apps/rewardstore/jquery.pointsActivityChart.js
@@ -42,7 +42,7 @@
             success: function (resp) {
                 flog('Response from server', resp);
                 
-                //showPointsActivityChart(resp, container, config);
+                showPointsActivityChart(resp, container, config);
             }
         });
     }
@@ -51,83 +51,90 @@
         var svg = container.find('svg');
         svg.empty();
 
-        flog('done show pieChart');
+        flog('start show pointsActivityChart', svg);
         nv.addGraph(function () {
-            var chart = nv.models.multiBarChart()
-                .margin({top: 0, right: 0, bottom: 0, left: 0})
-                .showControls(false)
-                .showLegend(true)
-                .showYAxis(false)
-                .showXAxis(false);
+            var chart;
 
-            chart.xAxis     //Chart x-axis settings
-                .axisLabel('Date')
-                .tickFormat(function (d) {
-                    return moment(d).format("DD MMM");
+            try {
+                chart = nv.models.multiBarChart()
+                    .margin({top: 0, right: 0, bottom: 0, left: 0})
+                    .showControls(false)
+                    .showLegend(true)
+                    .showYAxis(false)
+                    .showXAxis(false);
+
+                chart.xAxis     //Chart x-axis settings
+                    .axisLabel('Date')
+                    .tickFormat(function (d) {
+                        return moment(d).format("DD MMM");
+                    });
+
+                chart.yAxis     //Chart y-axis settings
+                    .axisLabel('Count')
+                    .tickFormat(d3.format('.02f'));
+
+                /* Generate data */
+                var active = [];
+                var expired = [];
+                var myData = [
+                    {
+                        values: active,
+                        key: 'Awarded',
+                        color: '#2ca02c'
+                    },
+                    {
+                        values: expired,
+                        key: 'Expired',
+                        color: '#d9534f'
+                    }
+                ];
+
+                if (resp.aggregations) {
+                    var d = resp.aggregations;
+                    var sBuckets = d.awarded.dates.buckets;
+                    var cBuckets = d.expired.dates.buckets;
+
+                    flog('Awarded Buckets', sBuckets);
+                    flog('Expired Buckets', cBuckets);
+
+                    /* generate valid stats */
+                    for (var i in sBuckets) {
+                        var b = sBuckets[i];
+                        active.push({
+                            x: b.key,
+                            y: b.sum ? b.sum.value || 0 : 0
+                        });
+                    }
+
+                    /* generate expired stats */
+                    for (var i in cBuckets) {
+                        var b = cBuckets[i];
+                        expired.push({
+                            x: b.key,
+                            y: b.sum ? b.sum.value || 0 : 0
+                        });
+                    }
+                }
+
+                active.sort(dynamicSort('x'));
+                expired.sort(dynamicSort('x'));
+
+                d3.select(svg.get(0))
+                    .datum(myData)
+                    .call(chart);
+
+                // Update the chart when window resizes.
+                nv.utils.windowResize(function () {
+                    chart.update();
                 });
-
-            chart.yAxis     //Chart y-axis settings
-                .axisLabel('Count')
-                .tickFormat(d3.format('.02f'));
-
-            /* Generate data */
-            var active = [];
-            var expired = [];
-            var myData = [
-                {
-                    values: active,
-                    key: 'Awarded',
-                    color: '#2ca02c'
-                },
-                {
-                    values: expired,
-                    key: 'Expired',
-                    color: '#d9534f'
-                }
-            ];
-
-            if (resp.aggregations) {
-                var d = resp.aggregations;
-                var sBuckets = d.awarded.dates.buckets;
-                var cBuckets = d.expired.dates.buckets;
-
-                flog('Awarded Buckets', sBuckets);
-                flog('Expired Buckets', cBuckets);
-
-                /* generate valid stats */
-                for (var i in sBuckets) {
-                    var b = sBuckets[i];
-                    active.push({
-                        x: b.key,
-                        y: b.sum.value || 0
-                    });
-                }
-
-                /* generate expired stats */
-                for (var i in cBuckets) {
-                    var b = cBuckets[i];
-                    expired.push({
-                        x: b.key,
-                        y: b.sum.value || 0
-                    });
-                }
+            } catch (e) {
+                flog('Error when rendering pointsActivityChart: ' + e, e);
             }
-
-            active.sort(dynamicSort('x'));
-            expired.sort(dynamicSort('x'));
-
-            d3.select(svg)
-                .datum(myData)
-                .call(chart);
-
-            // Update the chart when window resizes.
-            nv.utils.windowResize(function () {
-                chart.update()
-            });
 
             return chart;
         });
-        flog('done show pieChart');
+
+        flog('done show pointsActivityChart');
     }
 
     function dynamicSort(property) {
