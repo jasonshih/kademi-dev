cd912db15d58081751c777347009ed6d6ef310e5
Brad Mac <brad@mcevoysoftware.co.nz>
2017-01-17 15:44:56 +1300

    https://github.com/Kademi/kademi-dev/issues/2813

---
+++ src/main/webapp/templates/apps/reporting/orgSelectorComponent.js
@@ -0,0 +1,11 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['orgSelector'] = {
+        init: function (contentArea, container, component, keditor) {
+            var self = this;
+        },
+        settingEnabled: false
+    };
+})(jQuery);
