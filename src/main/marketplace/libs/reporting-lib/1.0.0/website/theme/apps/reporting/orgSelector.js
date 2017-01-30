40ef43df4cc376f121487c76fbbba314f92cb1a0
brad <brad@brad-little-laptop>
2017-01-30 16:51:52 +1300

    improvementst to orgSelectorComponent

--- src/main/marketplace/libs/reporting-lib/1.0.0/website/theme/apps/reporting/orgSelector.js
+++ src/main/marketplace/libs/reporting-lib/1.0.0/website/theme/apps/reporting/orgSelector.js
@@ -1,11 +1,12 @@
-$(function(){
-    function initOrgSelector(){
-        $('.orgSelectorWrap').on('click', 'a', function(e){
+$(function () {
+    function initOrgSelector() {
+        flog("initOrgSelector");
+        $('.orgSelectorWrap').on('click', 'a', function (e) {
             e.preventDefault();
 
             var orgId = $(this).attr('data-orgId');
-            if(orgId){
-                $.cookie('selectedOrg', orgId, { expires: 360, path: '/' });                
+            if (orgId) {
+                $.cookie('selectedOrg', orgId, {expires: 360, path: '/'});
             } else {
                 $.cookie('selectedOrg', "");
             }
@@ -14,5 +15,42 @@ $(function(){
         })
     }
 
+
+    function initOrgSelectorSearch() {
+        var orgSearch = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url:  '/queries/?searchOrgs=%QUERY',
+                wildcard: '%QUERY'
+            }
+        });
+
+        $('.selectOrgSearch').typeahead({
+            highlight: true
+        }, {
+            display: 'title',
+            limit: 10,
+            source: orgSearch,
+            templates: {
+                empty: [
+                    '<div class="empty-message">',
+                    'No existing stores were found.',
+                    '</div>'
+                ].join('\n'),
+                suggestion: Handlebars.compile(
+                        '<div>'
+                        + '<strong>{{title}}</strong>'
+                        + '</div>')
+            }
+        });
+
+        $('.selectOrgSearch').bind('typeahead:select', function (ev, sug) {
+            $.cookie('selectedOrg', sug.orgId, {expires: 360, path: '/'});
+            window.location.reload();
+        });
+    }
+
     initOrgSelector();
+    initOrgSelectorSearch();
 });
