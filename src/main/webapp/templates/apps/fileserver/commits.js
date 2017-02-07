0c7a92408372df9558e26f62dcee7dfdca516466
dylan <tuzzmaniandevil@gmail.com>
2017-02-07 13:05:09 +1300

    Add Set Remote Hash to commits page

--- src/main/webapp/templates/apps/fileserver/commits.js
+++ src/main/webapp/templates/apps/fileserver/commits.js
@@ -1,63 +1,81 @@
 function initManageRepoHistory() {
-	initEvents();
+    initEvents();
+    initSetRemoteHash();
 }
 
 function initEvents() {
     var wrapper = $('#commit-wrapper');
 
-	$('abbr.timeago').timeago();
+    $('abbr.timeago').timeago();
 
     wrapper.on('click', '.btn-restore-repo', function (e) {
         e.preventDefault();
-		var node = $(e.target).closest("a");
-		var hash = node.attr('rel');
+        var node = $(e.target).closest("a");
+        var hash = node.attr('rel');
         var revertHref = ".history";
-		confirmRevert(hash, null, {
-			getPageUrl: function () {
-				return '.'
-			},
-			afterRevertFn: function () {
+        confirmRevert(hash, null, {
+            getPageUrl: function () {
+                return '.'
+            },
+            afterRevertFn: function () {
                 Msg.success('Reverted to ' + hash + '!');
                 wrapper.reloadFragment({
-                    whenComplete: function() {
+                    whenComplete: function () {
                         $('abbr.timeago').timeago();
                     }
                 });
-			}
-		}, revertHref);
-	});
+            }
+        }, revertHref);
+    });
 
-	var btnSetHash = $('.btn-set-hash');
-	btnSetHash.attr('data-href', btnSetHash.attr('href'));
-	btnSetHash.removeAttr('href');
-	btnSetHash.click(function (e) {
-		e.preventDefault();
-		var link = $(e.target).closest('a');
-		var newHash = prompt('Please enter a new hash');
-		if (newHash != null && newHash.trim().length > 0) {
-			setHash(link, newHash);
-		}
-	});
+    var btnSetHash = $('.btn-set-hash');
+    btnSetHash.attr('data-href', btnSetHash.attr('href'));
+    btnSetHash.removeAttr('href');
+    btnSetHash.click(function (e) {
+        e.preventDefault();
+        var link = $(e.target).closest('a');
+        var newHash = prompt('Please enter a new hash');
+        if (newHash != null && newHash.trim().length > 0) {
+            setHash(link, newHash);
+        }
+    });
 }
 
 function setHash(link, newHash) {
-	log('setHash', link, newHash, link);
-	$.ajax({
-		type: 'POST',
-		data: {newHash: newHash},
-		url: link.attr('data-href'),
-		dataType: 'json',
-		success: function (data) {
-			log('done', data);
-			if (data.status) {
+    log('setHash', link, newHash, link);
+    $.ajax({
+        type: 'POST',
+        data: {newHash: newHash},
+        url: link.attr('data-href'),
+        dataType: 'json',
+        success: function (data) {
+            log('done', data);
+            if (data.status) {
                 Msg.success('Reverted to ' + newHash + '!');
                 $('#commit-wrapper').reloadFragment();
-			} else {
-				Msg.error('An error occured updating the branch: ' + data.messages);
-			}
-		},
-		error: function (resp) {
-			Msg.error('Sorry, couldnt update public access: ' + resp);
-		}
-	});
+            } else {
+                Msg.error('An error occured updating the branch: ' + data.messages);
+            }
+        },
+        error: function (resp) {
+            Msg.error('Sorry, couldnt update public access: ' + resp);
+        }
+    });
+}
+
+function initSetRemoteHash() {
+    var modal = $('#modal-upload-remotehash');
+    var modal_form = modal.find('form');
+
+    modal_form.forms({
+        onSuccess: function (resp) {
+            Msg.success(resp.messages);
+            modal_form.trigger('reset');
+            modal.modal('hide');
+        }
+    });
+
+    modal.on('hidden.bs.modal', function (e) {
+        modal_form.trigger('reset');
+    });
 }
