7cefcb75990a089e06afa9cadb1b37654bf722c5
Brad Mac <brad@mcevoysoftware.co.nz>
2017-02-08 17:36:58 +1300

    order tracking

---
+++ src/main/marketplace/libs/rewardstore-lib/1.0.0/website/theme/apps/rewardstore/components/orderHistoryComponent.js
@@ -0,0 +1,13 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['orderHistory'] = {
+        settingEnabled: true,
+        settingTitle: 'Order History Settings',
+        showSettingForm: function (form, component, keditor) {
+
+        }
+    };
+
+})(jQuery);
