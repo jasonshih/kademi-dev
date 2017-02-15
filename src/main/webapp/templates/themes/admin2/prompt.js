e8c55d9390430da746b14ca051a9f96f5da481d3
DucDHM <ducdhm@gmail.com>
2017-02-15 22:58:05 +0700

    https://github.com/Kademi/kademi-dev/issues/2885

--- src/main/webapp/templates/themes/admin2/prompt.js
+++ src/main/webapp/templates/themes/admin2/prompt.js
@@ -10,52 +10,53 @@ var myPromptModal;
 function myPrompt(id, url, title, instructions, caption, buttonName, buttonText, inputClass, inputPlaceholder, callback) {
     flog('myPrompt');
     var existing = $('#' + id);
-    if(existing ) {
+    if (existing) {
         existing.remove();
     }
     
-    var modalString = '<div id="' + id + '" class="modal modal-sm fade" tabindex="-1" role="dialog" aria-labelledby="promptModalLabel" aria-hidden="true"></div>';
+    var modalString = '<div id="' + id + '" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="promptModalLabel" aria-hidden="true"></div>';
     myPromptModal = $(modalString);
     
     var inputId = id + '_';
     
-    myPromptModal.html(        
-
-        '<div class="modal-header">' +
-           '<button aria-hidden="true" data-dismiss="modal" class="close" type="button">&times;</button>' +
-           '<h4 class="modal-title">' + title + '</h4>' +
-        '</div>' +
-        '<form method="POST" class="form-horizontal" action="' + url + '">' +
-        '<div class="modal-body">' +
-                instructions + 
-                '<div class="clearfix"></div>' +
-                '<br /><br />' + 
-                '<p class="alert alert-danger modal-alert"></p>' +
-                '<div class="form-group">' + 
-                    '<label for="' + inputId + '" class="control-label col-md-3">' + caption + '</label>' +
-                    '<div class="col-md-8">' +
-                        '<input type="text" class="required form-control ' + inputClass + '" id="' + inputId + '" name="' + buttonName + '" placeholder="' + inputPlaceholder + '" />'+
-                    '</div>' +
-                '</div>' +
-        '</div>' +
-        '<div class="modal-footer">' +
-           '<button class="btn btn-sm btn-default" data-dismiss="modal" type="button">Close</button>' +
-           '<button class="btn btn-sm btn-primary" data-type="form-submit" type="submit">' + buttonText + '</button>' +
-        '</div>' +
-        '</form>'
+    myPromptModal.html(
+        '<div class="modal-dialog modal-sm">' +
+        '   <div class="modal-content">' +
+        '       <div class="modal-header">' +
+        '           <button aria-hidden="true" data-dismiss="modal" class="close" type="button">&times;</button>' +
+        '           <h4 class="modal-title">' + title + '</h4>' +
+        '       </div>' +
+        '       <form method="POST" class="form-horizontal" action="' + url + '">' +
+        '           <div class="modal-body">'+
+        '               <p class="alert alert-info">' + instructions + '</p>' +
+        '               <p class="alert alert-danger modal-alert"></p>' +
+        '               <div class="form-group">' +
+        '                   <label for="' + inputId + '" class="control-label col-md-3">' + caption + '</label>' +
+        '                   <div class="col-md-8">' +
+        '                       <input type="text" class="required form-control ' + inputClass + '" id="' + inputId + '" name="' + buttonName + '" placeholder="' + inputPlaceholder + '" />' +
+        '                   </div>' +
+        '               </div>' +
+        '           </div>' +
+        '           <div class="modal-footer">' +
+        '               <button class="btn btn-sm btn-default" data-dismiss="modal" type="button">Close</button>' +
+        '               <button class="btn btn-sm btn-primary" data-type="form-submit" type="submit">' + buttonText + '</button>' +
+        '           </div>' +
+        '       </form>' +
+        '   </div>' +
+        '</div>'
     );
     
     var form = myPromptModal.find('form');
     
-    form.submit(function(e) {
+    form.submit(function (e) {
         flog("submit");
         e.preventDefault();
         resetValidation(form);
 
         var checkResult = validateFormFields(form);
-        if(checkResult) {
+        if (checkResult) {
             var newName = form.find('input').val();
-            if( callback(newName, form) ) {
+            if (callback(newName, form)) {
                 myPromptModal.modal('hide');
             }
         }
@@ -67,7 +68,7 @@ function myPrompt(id, url, title, instructions, caption, buttonName, buttonText,
 }
 
 function closeMyPrompt() {
-    flog('closeMyPrompt');        
+    flog('closeMyPrompt');
     myPromptModal.modal('hide');
 }
 
