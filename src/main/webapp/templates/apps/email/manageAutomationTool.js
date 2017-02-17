2a52aafcc3458cc58bb3c46ace9c57309ca4f378
dylan <tuzzmaniandevil@gmail.com>
2017-02-17 14:49:23 +1300

    https://github.com/Kademi/kademi-dev/issues/2866

--- src/main/webapp/templates/apps/email/manageAutomationTool.js
+++ src/main/webapp/templates/apps/email/manageAutomationTool.js
@@ -2,14 +2,35 @@
     function initDoSearch() {
         var searchForm = $('#searchForm');
         var tbody = $('#tbody-results');
+        var paginator = $('#paginator');
 
         searchForm.on('submit', function (e) {
             e.preventDefault();
             e.stopPropagation();
 
+            var formParams = searchForm.serialize();
+
+            window.history.pushState(null, null, window.location.pathname + "?" + formParams);
+
             tbody.reloadFragment({
-                url: window.location.pathname + "?" + searchForm.serialize()
+                url: window.location.pathname + "?" + formParams,
+                whenComplete: function () {
+                    paginator.find('li a').each(function (i, item) {
+                        var link = $(item);
+                        var href = link.attr('href');
+                        href = href + '&' + formParams;
+                        link.attr('href', href);
+                    });
+                }
             });
+        });
+
+        var formParams = searchForm.serialize();
+        $('#paginator').find('li a').each(function (i, item) {
+            var link = $(item);
+            var href = link.attr('href');
+            href = href + '&' + formParams;
+            link.attr('href', href);
         });
     }
 
