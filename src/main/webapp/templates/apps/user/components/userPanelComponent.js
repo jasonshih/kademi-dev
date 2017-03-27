fe1ee7d8ec46edb2223e19e2f896eaab84db9b81
DucDHM <ducdhm@gmail.com>
2017-03-27 12:19:33 +0700

    https://github.com/Kademi/kademi-dev/issues/3043

--- src/main/webapp/templates/apps/user/components/userPanelComponent.js
+++ src/main/webapp/templates/apps/user/components/userPanelComponent.js
@@ -1,36 +1,74 @@
 (function ($) {
     var KEditor = $.keditor;
     var flog = KEditor.log;
-
+    
     KEditor.components['userPanel'] = {
         settingEnabled: true,
         settingTitle: 'User Panel Settings',
         initSettingForm: function (form, keditor) {
             flog('initSettingForm "userPanel" component');
-
+            
             return $.ajax({
                 url: '_components/userPanel?settings',
                 type: 'get',
                 dataType: 'HTML',
                 success: function (resp) {
                     form.html(resp);
-
-                    form.find('.select-reward').on('change', function () {
+                    
+                    var chks = form.find('.chk-rewards');
+                    chks.on('click', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
-                        component.attr('data-reward', this.value);
+                        var selectedRewards = [];
+                        chks.each(function () {
+                            if (this.checked) {
+                                selectedRewards.push(this.value);
+                            }
+                        });
+                        
+                        var input = $(this).parent().next();
+                        input.prop('disabled', !this.checked);
+                        
+                        component.attr('data-rewards', selectedRewards.join(','));
                         keditor.initDynamicContent(dynamicElement);
-                    })
+                    });
+                    
+                    var txts = form.find('.txt-rewards-titles');
+                    txts.on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        var rewardsTitles = [];
+                        txts.each(function () {
+                            if (!this.disabled) {
+                                rewardsTitles.push(this.value || '');
+                            }
+                        });
+                        
+                        component.attr('data-rewards-titles', rewardsTitles.join(','));
+                        keditor.initDynamicContent(dynamicElement);
+                    });
                 }
             });
         },
         showSettingForm: function (form, component, keditor) {
             flog('showSettingForm "userPanel" component');
-
+            
             var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
-            form.find('.select-reward').val(dataAttributes['data-reward'] || '');
+            var selectedRewards = dataAttributes['data-rewards'];
+            selectedRewards = selectedRewards ? selectedRewards.split(',') : [];
+            
+            var rewardsTitles = dataAttributes['data-rewards-titles'];
+            rewardsTitles = rewardsTitles ? rewardsTitles.split(',') : [];
+            
+            var chks = form.find('.chk-rewards');
+            var txts = form.find('.txt-rewards-titles');
+            chks.prop('checked', false);
+            txts.val('').prop('disabled', true);
+            $.each(selectedRewards, function (i, reward) {
+                chks.filter('[value="' + reward + '"]').prop('checked', true);
+                txts.filter('[name="' + reward + '"]').prop('disabled', false).val(rewardsTitles[i]);
+            });
         }
     };
-
+    
 })(jQuery);
