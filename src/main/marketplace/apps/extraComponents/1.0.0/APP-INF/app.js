3a6386f4d634df17dacc1d3eda3b118b78576e66
DucDHM <ducdhm@gmail.com>
2017-03-27 11:55:07 +0700

    https://github.com/Kademi/kademi-dev/issues/3047

--- src/main/marketplace/apps/extraComponents/1.0.0/APP-INF/app.js
+++ src/main/marketplace/apps/extraComponents/1.0.0/APP-INF/app.js
@@ -1,1 +1,3 @@
 controllerMappings.addComponent("extraComponents", "codeBlock", "html", "Code block", "Extra component");
+controllerMappings.addComponent("extraComponents", "rewardStoreCategoryImage", "html", "Shows image of category", "Reward Store,Extra component");
+controllerMappings.addComponent("extraComponents", "rewardStoreCategoryNotes", "html", "Shows notes of category", "Reward Store,Extra component");
