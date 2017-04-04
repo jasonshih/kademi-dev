83e175bba6c7465a5c84a358db083f2b2bdaeedd
DucDHM <ducdhm@gmail.com>
2017-04-04 16:51:13 +0700

    Update keditor latest

--- src/main/webapp/static/keditor/dist/js/keditor-0.0.0.js
+++ src/main/webapp/static/keditor/dist/js/keditor-0.0.0.js
@@ -358,8 +358,6 @@
                 iframeBody.html(originalContent);
             }
             
-            window.a = iframeBody;
-            
             // In frame, have to use default snippets container
             options.snippetsListId = KEditor.DEFAULTS.snippetsListId;
             
