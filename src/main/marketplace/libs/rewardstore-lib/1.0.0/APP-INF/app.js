5e91729ad78d6f9d3a6f153c3479f6f9a009ea0a
LamNguyen <lamnt.8x@gmail.com>
2017-04-10 17:40:49 +0700

    Merge branch 'master' of https://github.com/Kademi/kademi-dev

--- src/main/marketplace/libs/rewardstore-lib/1.0.0/APP-INF/app.js
+++ src/main/marketplace/libs/rewardstore-lib/1.0.0/APP-INF/app.js
@@ -1,3 +1,8 @@
+/**
+ * Created by Anh on 10/04/2017.
+ */
+controllerMappings.addComponent("rewardstore/components", "rewardProduct", "html", "Shows reward product with image, points and add to card button", "Reward Store");
+
 controllerMappings.addComponent("rewardstore/components", "productSearch", "html", "Add Product Search component", "Reward Store");
 
 controllerMappings.addComponent("rewardstore/components", "pointsRangeList", "html", "Add Point Range List component", "Reward Store");
