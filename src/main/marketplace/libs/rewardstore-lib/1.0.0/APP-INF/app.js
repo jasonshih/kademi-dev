84b405ed7b110a89fd09ccc04d04be74981b897a
LamNguyen <lamnt.8x@gmail.com>
2017-04-10 17:25:10 +0700

    https://github.com/Kademi/kademi-dev/issues/3060

---
+++ src/main/marketplace/libs/rewardstore-lib/1.0.0/APP-INF/app.js
@@ -0,0 +1,3 @@
+controllerMappings.addComponent("rewardstore/components", "productSearch", "html", "Add Product Search component", "Reward Store");
+
+controllerMappings.addComponent("rewardstore/components", "pointsRangeList", "html", "Add Point Range List component", "Reward Store");
