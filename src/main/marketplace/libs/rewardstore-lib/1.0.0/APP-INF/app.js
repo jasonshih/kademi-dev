c956d7cf40fb0295cbcef5bb570be501bf31ff4e
anh <anhnt38@gmail.com>
2017-04-10 12:15:16 +0700

    https://github.com/Kademi/kademi-dev/issues/3060

---
+++ src/main/marketplace/libs/rewardstore-lib/1.0.0/APP-INF/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Anh on 10/04/2017.
+ */
+controllerMappings.addComponent("rewardstore/components", "rewardProduct", "html", "Shows reward product with image, points and add to card button", "Reward Store");
