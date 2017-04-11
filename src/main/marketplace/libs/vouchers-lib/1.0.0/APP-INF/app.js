a4628d4e3eca41c6c0f80c269a09ab3b65e3238e
dylan <dylan@dylan-Kademi>
2017-04-11 16:23:27 +1200

    https://github.com/Kademi/kademi-dev/issues/3102

--- src/main/marketplace/libs/vouchers-lib/1.0.0/APP-INF/app.js
+++ src/main/marketplace/libs/vouchers-lib/1.0.0/APP-INF/app.js
@@ -3,3 +3,4 @@
  */
 controllerMappings.addComponent("vouchers/components", "voucher", "html", "Shows single voucher details", "Vouchers App");
 controllerMappings.addComponent("vouchers/components", "voucherRedemption", "html", "Shows list voucher redemption", "Vouchers App");
+controllerMappings.addComponent("vouchers/components", "voucherPrint", "html", "Component used when voucher generating PDFs", "Vouchers App");
