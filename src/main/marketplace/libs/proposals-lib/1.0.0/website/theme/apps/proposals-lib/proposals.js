85e5c8b8b8d3073d75f8ae4a68306042d9e58c49
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2017-05-04 09:19:56 +0300

    Fixed dependencies.json syntactical bug, showed error when proposals fails to accept - https://github.com/Kademi/kademi-dev/issues/3167

--- src/main/marketplace/libs/proposals-lib/1.0.0/website/theme/apps/proposals-lib/proposals.js
+++ src/main/marketplace/libs/proposals-lib/1.0.0/website/theme/apps/proposals-lib/proposals.js
@@ -1,24 +1,40 @@
-$(function () {    
+$(function () {
     var container = $(".proposals-container");
-    if (container.length == 0) {
+    
+    if (container.length === 0) {
         return;
     }
     flog("init proposals");
-    $("body").on("click", ".proposal-accept-btn", function (e) {        
+    $("body").on("click", ".proposal-accept-btn", function (e) {
         e.preventDefault();
+        
         var btn = $(e.target).closest("a");
         var quoteId = btn.data("quote");
         var href = btn.attr("href");
+        
         flog("proposals accept click..", quoteId);
+        
         $.ajax({
             type: 'POST',
             url: href,
             datatype: 'json',
-            data : "approveQuote=" + quoteId,
+            data: {
+                approveQuote: quoteId
+            },
             success: function (data) {
-                Msg.info('Accepted');
-                container.find(".proposal-quotes").hide();
-                container.find(".proposal-thankyou").show();
+                if (data.status) {
+                    Msg.info('Quote Accepted Successfully');
+                } else {
+                    if (data.messages.length > 0) {
+                        Msg.error(data.messages[0]);
+                    } else {
+                        Msg.error('An error occured accepting the quote');
+                    }
+                }
+
+                /*
+                 container.find(".proposal-quotes").hide();
+                 container.find(".proposal-thankyou").show();*/
             },
             error: function (resp) {
                 Msg.error('An error occured accepting the quote');
