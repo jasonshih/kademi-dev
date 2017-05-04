e9abaff91803b6779a0501e9acf70d3f0f2f9e1d
Brad Mac <brad@mcevoysoftware.co.nz>
2017-05-04 14:22:08 +1200

    https://github.com/Kademi/kademi-dev/issues/3167

---
+++ src/main/marketplace/libs/proposals-lib/1.0.0/website/theme/apps/proposals-lib/components/viewProposalDetailsComponent.js
@@ -0,0 +1,17 @@
+/**
+ * Created by m.elkhoudary on 4/7/2017.
+ */
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+
+    KEditor.components['viewProposalDetails'] = {
+        init: function (contentArea, container, component, keditor) {
+            flog('init "viewProposalDetails" component', component);
+        },
+
+        settingEnabled: false,
+
+    };
+
+})(jQuery);
