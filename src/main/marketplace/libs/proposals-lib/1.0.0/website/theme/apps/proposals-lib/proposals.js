06b672b0fcc70ebfa2bdb5c45b350ebce795ae09
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2017-05-04 10:15:58 +0300

    Created Proposal Acceptance Layout - https://github.com/Kademi/kademi-dev/issues/3167

--- src/main/marketplace/libs/proposals-lib/1.0.0/website/theme/apps/proposals-lib/proposals.js
+++ src/main/marketplace/libs/proposals-lib/1.0.0/website/theme/apps/proposals-lib/proposals.js
@@ -1,19 +1,23 @@
 $(function () {
     var container = $(".proposals-container");
-    
+
     if (container.length === 0) {
         return;
     }
     flog("init proposals");
     $("body").on("click", ".proposal-accept-btn", function (e) {
+        if (!confirm("Are you sure you want to accept this quote?")) {
+            return;
+        }
+
         e.preventDefault();
-        
+
         var btn = $(e.target).closest("a");
         var quoteId = btn.data("quote");
         var href = btn.attr("href");
-        
+
         flog("proposals accept click..", quoteId);
-        
+
         $.ajax({
             type: 'POST',
             url: href,
@@ -22,19 +26,16 @@ $(function () {
                 approveQuote: quoteId
             },
             success: function (data) {
-                if (data.status) {
-                    Msg.info('Quote Accepted Successfully');
-                } else {
+                if (!data.status) {
                     if (data.messages.length > 0) {
                         Msg.error(data.messages[0]);
                     } else {
                         Msg.error('An error occured accepting the quote');
                     }
+                } else {
+                    container.find(".proposals-container").slideUp(300);
+                    container.find(".proposal-thankyou").slideDown(300);
                 }
-
-                /*
-                 container.find(".proposal-quotes").hide();
-                 container.find(".proposal-thankyou").show();*/
             },
             error: function (resp) {
                 Msg.error('An error occured accepting the quote');
