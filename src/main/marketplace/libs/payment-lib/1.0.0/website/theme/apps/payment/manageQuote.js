71f5b6dfb78ce6bbcedd7698943b9236e1291415
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2017-05-07 10:24:44 +0200

    Supported File Upload for Quote, and Added a Description field for uploaded file - https://github.com/Kademi/kademi-dev/issues/3181

--- src/main/marketplace/libs/payment-lib/1.0.0/website/theme/apps/payment/manageQuote.js
+++ src/main/marketplace/libs/payment-lib/1.0.0/website/theme/apps/payment/manageQuote.js
@@ -428,6 +428,23 @@ Number.prototype.formatMoney = function (c, d, t) {
         $("#clone-quote-button").on("click", function () {
             $("#clone-quote-form").submit();
         });
+        
+        var modal = $('#uploadFileModal');
+        var form = modal.find('form');
+
+        $('body').on('click', '.upload-files', function (e) {
+            e.preventDefault();
+
+            modal.modal('show');
+        });
+
+        form.forms({
+            callback: function (resp) {
+                Msg.info('Files Uploaded');
+                reloadFileList();
+                modal.modal('hide');
+            }
+        });
     }
     ;
 
