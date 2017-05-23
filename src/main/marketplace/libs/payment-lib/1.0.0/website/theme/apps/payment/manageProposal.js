c7df666c02603bc7eee1407a49cdb6ea5c491f17
DucDHM <ducdhm@gmail.com>
2017-05-23 22:52:09 +0700

    https://github.com/Kademi/kademi-dev/issues/3256

--- src/main/marketplace/libs/payment-lib/1.0.0/website/theme/apps/payment/manageProposal.js
+++ src/main/marketplace/libs/payment-lib/1.0.0/website/theme/apps/payment/manageProposal.js
@@ -1,5 +1,7 @@
 $(function () {
     initProposalDetailsForm();
+    initProposalQuotes();
+    initQuoteModal();
 });
 
 function initProposalDetailsForm() {
@@ -13,7 +15,31 @@ function initProposalDetailsForm() {
         
         form.forms({
             onSuccess: function (resp) {
-                flog(resp, '=================')
+                if (resp.nextHref) {
+                    Msg.success('Proposal is created successfully. Redirecting to details page...');
+                    window.location.href = resp.nextHref;
+                } else {
+                    Msg.success('Details is saved');
+                }
+            }
+        });
+    }
+}
+
+function initProposalQuotes() {
+
+}
+
+function initQuoteModal() {
+    var modal = $('#modal-create-quote');
+    if (modal.length > 0) {
+        modal.on('hidden.bs.modal', function () {
+            modal.find('input').not(':hidden').val('');
+        });
+        
+        modal.find('form').forms({
+            onSuccess: function (resp) {
+                flog('=================', resp);
             }
         });
     }
