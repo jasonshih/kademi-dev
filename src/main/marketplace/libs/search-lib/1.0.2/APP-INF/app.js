f403c163255e357badfcc2949a0da312b0f468b6
tannguyen3489@gmail.com <freedom3489>
2017-05-26 09:42:31 +0700

    https://github.com/Kademi/kademi-dev/issues/3067 - Move components code and declaration to their libs

---
+++ src/main/marketplace/libs/search-lib/1.0.2/APP-INF/app.js
@@ -0,0 +1,3 @@
+controllerMappings.addComponent("search/components", "searchBox", "html", "Display search box", "Search App component");
+controllerMappings.addComponent("search/components", "searchResult", "html", "Display search result", "Search App component");
+controllerMappings.addComponent("search/components", "searchResultBucket", "html", "Display search result bucket", "Search App component");
