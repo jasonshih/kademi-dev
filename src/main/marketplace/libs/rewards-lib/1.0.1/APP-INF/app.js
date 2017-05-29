e72e8dbc120095e34f31c0b55d4d02e76e84a517
tannguyen3489 <tannguyen3489@gmail.com>
2017-05-29 09:28:28 +0700

    Implement #3067 - Move components code and declaration to their libs

---
+++ src/main/marketplace/libs/rewards-lib/1.0.1/APP-INF/app.js
@@ -0,0 +1,8 @@
+controllerMappings.addComponent("rewards-lib/components", "myRewards", "html", "Display user rewards", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "myRewardPoints", "html", "Display user points", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "myRewardLeaderboard", "html", "Display reward leaderboard", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "competitionForm", "html", "Display competition form", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "competitionResult", "html", "Display competition result", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "competitionList", "html", "List of current promotions", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "competitionStatusList", "html", "Status of current promotions", "Rewards and promotions");
+controllerMappings.addComponent("rewards-lib/components", "rewardSelector", "html", "Allows selecting a reward/promotion to filter data", "Rewards and promotions");
