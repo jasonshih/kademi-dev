932e0ec227722d822f2ff1d42d1547beecbabe3a
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2017-05-29 14:04:50 +0200

    Added visual support for setting set field new params - https://github.com/Kademi/kademi-dev/issues/3282

--- src/main/webapp/templates/apps/leadman/jb/setFieldNode.js
+++ src/main/webapp/templates/apps/leadman/jb/setFieldNode.js
@@ -27,6 +27,28 @@ JBNodes['setField'] = {
             '        <textarea name="expression"  class="form-control field-value" rows="5"></textarea>' +
             '        <small class="text-muted help-block">Can use MVEL syntax</small>' +
             '    </div>' +
+            '</div>' +
+            '<div class="form-group">' +
+            '    <div class="col-md-12">' +
+            '        <div class="checkbox">' +
+            '           <label>' +
+            '               <input type="checkbox" name="plainText" class="plain-text">' +
+            '               Plain Text Value' +
+            '           </label>' +
+            '        </div>' +
+            '        <small class="text-muted help-block">If set, value will not be evaluated as MVEL</small>' +
+            '    </div>' +
+            '</div>' +
+            '<div class="form-group">' +
+            '    <div class="col-md-12">' +
+            '        <div class="checkbox">' +
+            '           <label>' +
+            '               <input type="checkbox" name="setInSource" class="set-in-source">' +
+            '               Set for Source Lead' +
+            '           </label>' +
+            '        </div>' +
+            '        <small class="text-muted help-block">If set, value will be set to source lead field</small>' +
+            '    </div>' +
             '</div>'
         );
 
@@ -35,9 +57,14 @@ JBNodes['setField'] = {
             onValid: function () {
                 var fieldName = form.find('.field-name').val();
                 var fieldValueMvel = form.find('.field-value').val();
+                var plainText = form.find('.plain-text').prop("checked");
+                var setInSource = form.find('.set-in-source').prop("checked");
 
                 JBApp.currentSettingNode.fieldName = fieldName || null;
                 JBApp.currentSettingNode.fieldValueMvel = fieldValueMvel || null;
+                JBApp.currentSettingNode.plainText = plainText || null;
+                JBApp.currentSettingNode.setInSource = setInSource || null;
+                
                 JBApp.saveFunnel('Funnel is saved');
                 JBApp.hideSettingPanel();
             }
@@ -45,8 +72,12 @@ JBNodes['setField'] = {
     },
 
     showSettingForm: function (form, node) {
+        console.log(node);
         form.find('.field-name').val(node.fieldName !== null ? node.fieldName : '');
         form.find('.field-value').val(node.fieldValueMvel !== null ? node.fieldValueMvel : '');
+        form.find('.plain-text').prop("checked", node.plainText !== null ? node.plainText : false);
+        form.find('.set-in-source').prop("checked", node.setInSource !== null ? node.setInSource : false);
+        
         JBApp.showSettingPanel(node);
     }
 };
