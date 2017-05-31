33967a6c5205b179be8242ce9e8f2887966b8af5
Gonzalo Naveira <gonzaliito@gmail.com>
2017-05-31 19:21:41 -0300

    https://github.com/Kademi/kademi-dev/issues/3262 updating to remove the inline js to the manageSubscriptions file

--- src/main/webapp/templates/apps/subscription/manageSubscriptions.js
+++ src/main/webapp/templates/apps/subscription/manageSubscriptions.js
@@ -1,25 +1,5 @@
 $(function () {
     
-    jQuery("form.createSubscription").forms({
-        callback: function () {
-            $.ajax({
-                type: 'GET',
-                url: window.location.href,
-                dataType: "html",
-                success: function (resp) {
-                    var page = $(resp);
-                    var table = page.find("#subscriptionsTableContainer table");
-                    $("#subscriptionsTableContainer").html(table);
-                    $("#addSubscriptionModal").modal('hide');
-                },
-                error: function (resp) {
-                    flog('Error: ', resp);
-                    $("#addSubscriptionModal").modal('hide');
-                }
-            });
-        }
-    });
-    
     function initOrgFinder() {
         $('input#orgId').entityFinder({
             useActualId: false,
@@ -27,22 +7,69 @@ $(function () {
         });
     }
     
-    $('body').on('click', '.btn-delete-subscriptions', function (e) {
-        e.preventDefault();
-        var listToDelete = [];
-        $('body').find(':checkbox.subscription-check:checked').each(function () {
-            var s = $(this);
-            var id = s.data("id");
-            listToDelete.push(id);
+    function initCreateSubscription(){
+        jQuery("form.createSubscription").forms({
+            callback: function () {
+                $.ajax({
+                    type: 'GET',
+                    url: window.location.href,
+                    dataType: "html",
+                    success: function (resp) {
+                        var page = $(resp);
+                        var table = page.find("#subscriptionsTableContainer table");
+                        $("#subscriptionsTableContainer").html(table);
+                        $("#addSubscriptionModal").modal('hide');
+                    },
+                    error: function (resp) {
+                        flog('Error: ', resp);
+                        $("#addSubscriptionModal").modal('hide');
+                    }
+                });
+            }
         });
-        flog("List To Delete", listToDelete.join(','));
-        if (listToDelete.length > 0 && confirm("Are you sure you want to delete " + listToDelete.length + " subscriptions?")) {
-            $('body').find('.check-all').check(false).change();
-            deleteSubscriptions(listToDelete.join(','));
-        } else {
-            Msg.error('Please select the subscriptions you want to remove by clicking the checkboxes on the right');
-        }
-    });
+    }
+    
+    function initUpdateSubscription(){
+        jQuery("form#subscriptionDetails").forms({
+           onSuccess: function (resp) {
+                if (resp.status) {
+                    Msg.info(resp.messages);
+                } else {
+                    Msg.error("An error occured updating the subscription. Please check your internet connection");
+                }
+           },
+          onError: function( resp){
+            Msg.error("An error occured updating the subscriptions");
+          }
+       });        
+    }
+    
+    function initDeleteSubscriptions(){
+        $('body').on('click', '.btn-delete-subscriptions', function (e) {
+            e.preventDefault();
+            var listToDelete = [];
+            $('body').find(':checkbox.subscription-check:checked').each(function () {
+                var s = $(this);
+                var id = s.data("id");
+                listToDelete.push(id);
+            });
+            flog("List To Delete", listToDelete.join(','));
+            if (listToDelete.length > 0 && confirm("Are you sure you want to delete " + listToDelete.length + " subscriptions?")) {
+                $('body').find('.check-all').check(false).change();
+                deleteSubscriptions(listToDelete.join(','));
+            } else {
+                Msg.error('Please select the subscriptions you want to remove by clicking the checkboxes on the right');
+            }
+        });
+        
+        $('body').on('change', '.check-all', function (e) {
+            flog($(this).is(":checked"));
+            var checkedStatus = this.checked;
+            $('body').find(':checkbox.subscription-check').each(function () {
+                $(this).prop('checked', checkedStatus);
+            });
+        });
+    }
     
     function deleteSubscriptions(listToDelete) {
         $.ajax({
@@ -66,13 +93,10 @@ $(function () {
         });
     }
     
-    $('body').on('change', '.check-all', function (e) {
-        flog($(this).is(":checked"));
-        var checkedStatus = this.checked;
-        $('body').find(':checkbox.subscription-check').each(function () {
-            $(this).prop('checked', checkedStatus);
-        });
-    });
+    function initManageSubscriptions(){
+        initCreateSubscription();
+        initDeleteSubscriptions();
+    }
     
     initOrgFinder();
 });
