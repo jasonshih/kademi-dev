104d8aff218c24f5ce57de8719f2a8c18758392f
kevin3489 <tannguyen3489@gmail.com>
2017-06-06 09:01:24 +0700

    Top SKU component for Data Series #2990

---
+++ src/main/marketplace/libs/salesdata-lib/1.0.0/APP-INF/app.js
@@ -0,0 +1,5 @@
+controllerMappings.addComponent("salesdata/components", "topSkus", "html", "Shows top SKUs of data series in table format", "Sales data");
+controllerMappings.addComponent("salesdata", "kpiVis", "html", "Shows top SKUs of data series in table format", "Sales data");
+controllerMappings.addComponent("salesdata", "salesDataVis", "html", "Displays sales data in a visualisation, eg dateHistogram, sparkline, kpiLeaderboard, pointsLeaderboard or a summary", "Sales data");
+controllerMappings.addComponent("salesdata", "onTrack", "html", "An indicator to show if the user is on track to achieve the KPI target", "Sales data");
+controllerMappings.addComponent("salesdata", "kpiPeriodPicker", "html", "Allows select a KPI period from a dropdown list", "Sales data");
