7f72ae1e7f86d6ca2509fe75a7730411627cfe4a
kevin3489 <tannguyen3489@gmail.com>
2017-06-06 20:48:18 +0700

    Top SKU component for Data Series #2990

--- src/main/marketplace/libs/salesdata-lib/1.0.0/website/theme/apps/salesdata/topSkus.js
+++ src/main/marketplace/libs/salesdata-lib/1.0.0/website/theme/apps/salesdata/topSkus.js
@@ -40,6 +40,8 @@
             }
             doSearch(container);
         });
+
+        doSearch(container);
     }
     
     function doSearch(container) {
@@ -73,7 +75,7 @@
                 var bucket = searchRes.aggregations.skuCode.buckets[i];
                 
                 tbodyStr += '<tr>';
-                tbodyStr += '    <td>' + bucket.skuCategory.hits.hits[0]._source.category + '</td>';
+                // tbodyStr += '    <td>' + bucket.skuCategory.hits.hits[0]._source.category + '</td>';
                 tbodyStr += '    <td>' + resp.skuMap[bucket.key] + '</td>';
                 tbodyStr += '    <td class="text-right">' + bucket.totalPoints.value.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' }) + '</td>';
                 tbodyStr += '</tr>';
@@ -81,7 +83,7 @@
                 totalPoints += +bucket.totalPoints.value;
             }
             tfootStr += '<tr>';
-            tfootStr += '    <th></th>';
+            // tfootStr += '    <th></th>';
             tfootStr += '    <th></th>';
             tfootStr += '    <th class="text-right">' + totalPoints.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' }) + '</th>';
             tfootStr += '</tr>';
@@ -113,99 +115,6 @@
         }
     }
     
-    function initTopSkusChart(container) {
-        $(document.body).on('pageDateChanged', function (e, startDate, endDate) {
-            $.ajax({
-                type: 'get',
-                url: '/getTopSkusChart',
-                data: {
-                    startDate: startDate,
-                    endDate: endDate
-                },
-                dataType: 'json',
-                success: function (resp) {
-                    initChart(container, resp);
-                }
-            });
-        });
-    }
-    
-    function initChart(container, resp) {
-        var chartCategory = container.find('.chart-category-share');
-        var chartSkus = container.find('.chart-skus-share');
-        
-        var dataCategory = [];
-        var totalCategory = 0;
-        
-        var searchRes = resp.searchResults;
-        
-        if (searchRes && searchRes.aggregations && searchRes.aggregations.skuCategoryShare && searchRes.aggregations.skuCategoryShare.buckets && searchRes.aggregations.skuCategoryShare.buckets.length > 0) {
-            for (var i = 0; i < searchRes.aggregations.skuCategoryShare.buckets.length; i++) {
-                var bucket = searchRes.aggregations.skuCategoryShare.buckets[i];
-                dataCategory.push({
-                    key: bucket.key,
-                    doc_count: bucket.totalPoints.value
-                });
-                
-                totalCategory += bucket.totalPoints.value;
-            }
-        }
-        drawChart(chartCategory, dataCategory, totalCategory);
-        
-        var dataSkus = [];
-        var totalSkus = 0;
-        if (searchRes && searchRes.aggregations && searchRes.aggregations.skuCodeShare && searchRes.aggregations.skuCodeShare.buckets && searchRes.aggregations.skuCodeShare.buckets.length > 0) {
-            for (var i = 0; i < searchRes.aggregations.skuCodeShare.buckets.length; i++) {
-                if (i < 5) {
-                    var bucket = searchRes.aggregations.skuCodeShare.buckets[i];
-                    dataSkus.push({
-                        key: resp.skuMap[bucket.key],
-                        doc_count: bucket.totalPoints.value
-                    });
-                    
-                    totalSkus += bucket.totalPoints.value;
-                }
-            }
-        }
-        drawChart(chartSkus, dataSkus, totalSkus);
-    }
-    
-    function drawChart(container, data, total) {
-        var svg = container.find('svg');
-        svg.empty();
-        
-        nv.addGraph(function () {
-            flog('Data to rendering pieChart', data);
-            
-            var chart = nv.models.pieChart()
-                .x(function (d) {
-                    return d.key;
-                })
-                .y(function (d) {
-                    return d.doc_count;
-                })
-                .valueFormat(function (val) {
-                    return round((val / total * 100), 2) + '% (' + val.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' }) + ')';
-                })
-                .labelType('value')
-                .showLabels(false)
-                .labelThreshold(0.10)
-                .showLegend(false)
-                .color(['#555555', '#bbbbbb', '#888888', '#f5bf00'])
-                .margin({top: 0, right: 0, bottom: 0, left: 0});
-            
-            flog('Chart object', chart, svg.get(0));
-            d3.select(svg.get(0))
-                .datum(data)
-                .transition().duration(350)
-                .call(chart);
-            
-            nv.utils.windowResize(chart.update);
-            
-            return chart;
-        });
-        
-        flog('Done in drawing pieChart');
-    }
+
     
 })(jQuery);
