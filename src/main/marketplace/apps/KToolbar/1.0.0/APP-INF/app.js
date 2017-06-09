400171eb07432b11ca4d8fae7673adff2611be18
DucDHM <ducdhm@gmail.com>
2017-06-09 15:38:42 +0700

    https://github.com/Kademi/kademi-dev/issues/3357

--- src/main/marketplace/apps/KToolbar/1.0.0/APP-INF/app.js
+++ src/main/marketplace/apps/KToolbar/1.0.0/APP-INF/app.js
@@ -9,6 +9,19 @@ controllerMappings
     .enabled(true)
     .build();
 
-function getKToolbar() {
+function getKToolbar(page, params, context) {
     log.info('getKToolbar');
+    
+    var fontFamilyList = {
+        'arial,helvetica,sans-serif': 'Arial',
+        'comic sans ms,cursive': 'Comic Sans MS',
+        'courier new,courier,monospace': 'Courier New',
+        'lucida sans unicode,lucida grande,sans-serif': 'Lucida Sans Unicode',
+        'tahoma,geneva,sans-serif': 'Tahoma',
+        'times new roman,times,serif': 'Times New Roman',
+        'trebuchet ms,helvetica,sans-serif': 'Trebuchet MS',
+        'verdana,geneva,sans-serif': 'Verdana'
+    };
+    
+    context.put('fontFamilyList', fontFamilyList);
 }
