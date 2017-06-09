8005c0ffb4d3c3d1ac126a6428a9e62e006d1e48
DucDHM <ducdhm@gmail.com>
2017-06-09 17:15:48 +0700

    https://github.com/Kademi/kademi-dev/issues/3290

--- src/main/webapp/templates/apps/products/manageSkus.js
+++ src/main/webapp/templates/apps/products/manageSkus.js
@@ -1,6 +1,5 @@
-
 (function () {
-
+    
     $(document.body).on('keypress', '#data-query', function (e) {
         var code = e.keyCode || e.which;
         if (code == 13) {
@@ -9,83 +8,83 @@
             return false;
         }
     });
-
-	$("#pointsFooter a").on('click', function (e) {
-		e.preventDefault();
+    
+    $("#pointsFooter a").on('click', function (e) {
+        e.preventDefault();
         var uri = e.target.href;
-
+        
         $.ajax({
             type: 'GET',
             url: uri,
             success: function (data) {
                 flog('success', data);
                 window.history.pushState('', document.title, uri.toString());
-
+                
                 var newDom = $(data);
-
+                
                 var $tableContent = newDom.find('#productsTableContainer');
                 $('#productsTableContainer').replaceWith($tableContent);
-
+                
                 initUpdateSku();
-	            initUpdateSkuTitle();
-	            initUpdateBaseCost();
-	            initUploadSkuImage();
-	            initProductsCsv();
-	            initUpdateSkuStock();
+                initUpdateSkuTitle();
+                initUpdateBaseCost();
+                initUploadSkuImage();
+                initProductsCsv();
+                initUpdateSkuStock();
             },
             error: function (resp) {
                 Msg.error('err');
             }
         });
-	});
-
-
-	function doHistorySearch() {
-	    flog('doHistorySearch');
-
-	    Msg.info("Doing search...", 2000);
-
-	    var data = {
-	        dataQuery: $("#data-query").val(),
-	    };
-	    flog("data", data);
-
-	    $('.btn-export-points').attr('href', 'skus.csv?' + $.param(data));
-
-	    var target = $("#pointsTable");
-	    target.load();
-
-
-	    var link = window.location.pathname + "?" + $.param(data);
-	    flog("new link", link);
-
-	    $.ajax({
-	        type: "GET",
-	        url: link,
-	        dataType: 'html',
-	        success: function (content) {
-	            flog('response', content);
-	            Msg.success("Search complete", 2000);
-	            var newBody = $(content).find("#pointsTable");
-
-	            target.replaceWith(newBody);
-	            history.pushState(null, null, link);
-
-	            $("abbr.timeago").timeago();
-
-	            initUpdateSku();
-	            initUpdateSkuTitle();
-	            initUpdateBaseCost();
-	            initUploadSkuImage();
-	            initProductsCsv();
-	            initUpdateSkuStock();
-	        }
-	    });
-	}
-
+    });
+    
+    
+    function doHistorySearch() {
+        flog('doHistorySearch');
+        
+        Msg.info("Doing search...", 2000);
+        
+        var data = {
+            dataQuery: $("#data-query").val(),
+        };
+        flog("data", data);
+        
+        $('.btn-export-points').attr('href', 'skus.csv?' + $.param(data));
+        
+        var target = $("#pointsTable");
+        target.load();
+        
+        
+        var link = window.location.pathname + "?" + $.param(data);
+        flog("new link", link);
+        
+        $.ajax({
+            type: "GET",
+            url: link,
+            dataType: 'html',
+            success: function (content) {
+                flog('response', content);
+                Msg.success("Search complete", 2000);
+                var newBody = $(content).find("#pointsTable");
+                
+                target.replaceWith(newBody);
+                history.pushState(null, null, link);
+                
+                $("abbr.timeago").timeago();
+                
+                initUpdateSku();
+                initUpdateSkuTitle();
+                initUpdateBaseCost();
+                initUploadSkuImage();
+                initProductsCsv();
+                initUpdateSkuStock();
+            }
+        });
+    }
+    
     function initProductsCsv() {
         var modalUploadCsv = $("#modal-upload-csv");
-
+        
         var resultUploadCsv = modalUploadCsv.find('.upload-results');
         $("#do-upload-csv").mupload({
             buttonText: "<i class=\"clip-folder\"></i> Upload spreadsheet",
@@ -102,7 +101,7 @@
             }
         });
     }
-
+    
     function showUnmatched(resultUploadCsv, unmatched) {
         var unmatchedTable = resultUploadCsv.find("table");
         var tbody = unmatchedTable.find("tbody");
@@ -116,31 +115,31 @@
             tbody.append(tr);
         });
     }
-
+    
     function initUpdateSku() {
         $("#productsTableBody").on("change", ".input-sku", function (e) {
             var target = $(e.target);
             var newSku = target.val();
             var row = target.closest('tr');
             var rowId = $(row).attr("id");
-
+            
             var td = $(row).find('td:first-child');
-
+            
             var productId = td.data("product");
             var skuId = td.data('skuid');
             var options = td.data("options");
-
+            
             updateSku(productId, options, skuId, newSku, rowId);
         });
-
+        
         $("#productsTableBody").on("keyup", ".input-sku", function (e) {
             if ((e.keyCode || e.which) == keymap.ENTER) {
                 var target = $(e.target);
                 var newSku = target.val();
-
+                
                 var row = target.closest('tr');
                 var td = $(row).find('td:first-child');
-
+                
                 var rowId = $(row).attr("id");
                 var productId = td.data("product");
                 var skuId = td.data('skuid');
@@ -149,7 +148,7 @@
             }
         });
     }
-
+    
     function initUpdateSkuTitle() {
         $("#productsTableBody").on("change", ".input-sku-Title", function (e) {
             var target = $(e.target);
@@ -157,29 +156,29 @@
             var row = target.closest('tr');
             var rowId = $(row).attr("id");
             var td = $(row).find('td:first-child');
-
+            
             var skuId = td.data('skuid');
-
-
+            
+            
             updateSkuTitle(skuId, newSkuTitle, rowId);
         });
-
+        
         $("#productsTableBody").on("keyup", ".input-sku-Title", function (e) {
             if ((e.keyCode || e.which) == keymap.ENTER) {
                 var target = $(e.target);
                 var newSkuTitle = target.val();
-
+                
                 var row = target.closest('tr');
                 var rowId = $(row).attr("id");
                 var td = $(row).find('td:first-child');
-
+                
                 var skuId = td.data('skuid');
-
+                
                 updateSkuTitle(skuId, newSkuTitle, rowId);
             }
         });
     }
-
+    
     function initUpdateBaseCost() {
         $("#productsTableBody").on("change", ".input-sku-baseCost", function (e) {
             var target = $(e.target);
@@ -187,13 +186,13 @@
             var row = target.closest('tr');
             var rowId = $(row).attr("id");
             var td = $(row).find('td:first-child');
-
+            
             var skuId = td.data('skuid');
-
+            
             updateSkuBaseCost(skuId, newSkuTitle, rowId);
         });
     }
-
+    
     function initUpdateSkuStock() {
         $("#productsTableBody").on("change", ".input-sku-stock", function (e) {
             var target = $(e.target);
@@ -203,108 +202,110 @@
             var rowId = $(row).attr("id");
             var td = $(row).find('td:first-child');
             var skuId = td.data('skuid');
-
+            
             updateSkuStock(skuId, locid, newSkuStock, rowId, rowId);
         });
     }
-
+    
     function initUploadSkuImage() {
         $('body').on('click', '.btn-option-img', function (e) {
             e.preventDefault();
-
+            
             var btn = $(this);
             var row = btn.closest('tr');
             var td = $(row).find('td:first-child');
-
+            
             var skuId = td.data('skuid');
-            var imgHashes = td.data('selectableimg').split(',');
-
-            $('#modal-option-img').find('input[name=skuId]').val(skuId);
-            $('#modal-option-img').find('.img-list').empty();
-            for (var i in imgHashes) {
+            var imgHashes = td.attr('data-selectableimg').split(',');
+            var modalOptionImg = $('#modal-option-img');
+            modalOptionImg.find('input[name=skuId]').val(skuId);
+            
+            var imgsStr = '';
+            for (var i = 0; i < imgHashes.length; i++) {
                 var hash = imgHashes[i];
-                var imgDiv = '<div class="col-xs-6 col-md-3 product-image-thumb">'
-                        + '    <a href="' + hash + '" class="thumbnail select-opt-img"><img src="/_hashes/files/' + hash + '/alt-150-150.png"/></a>'
-                        + '    <a class="btn-image-selected btn btn-xs btn-success" style="display: none;" href="/_hashes/files/' + hash + '">'
-                        + '        <span class="fa fa-check"></span>'
-                        + '    </a>'
-                        + '</div>';
-
-                $('#modal-option-img').find('.img-list').append(imgDiv);
+                
+                imgsStr += '<div class="col-xs-6 col-md-3 product-image-thumb">';
+                imgsStr += '    <a href="' + hash + '" class="thumbnail select-opt-img"><img src="/_hashes/files/' + hash + '/alt-150-150.png"/></a>';
+                imgsStr += '    <a class="btn-image-selected btn btn-xs btn-success" style="display: none;" href="/_hashes/files/' + hash + '">';
+                imgsStr += '        <span class="fa fa-check"></span>';
+                imgsStr += '    </a>';
+                imgsStr += '</div>';
+                
             }
-
-            $('#modal-option-img').modal('show');
+            
+            modalOptionImg.find('.img-list').html(imgsStr);
+            modalOptionImg.modal('show');
         });
-
+        
         $('body').on('click', '.select-opt-img', function (e) {
             e.preventDefault();
-
+            
             $('#modal-option-img').find('.btn-image-selected ').removeClass('image-selected');
-
+            
             var img = $(this);
             img.closest('div').find('.btn-image-selected ').addClass('image-selected');
             var hash = img.attr('href');
-
+            
             var form = img.closest('form');
             form.find('input[name=updateSkuImageHash]').val(hash);
         });
-
+        
         $('body').on('click', '.image-change', function (e) {
             e.preventDefault();
-
+            
             var btn = $(this);
             var row = btn.closest('tr');
             var td = $(row).find('td:first-child');
             var skuId = td.data('skuid');
             var imgHashes = td.data('selectableimg').split(',');
-
+            
             $('#modal-option-img').find('input[name=skuId]').val(skuId);
             $('#modal-option-img').find('.img-list').empty();
             for (var i in imgHashes) {
                 var hash = imgHashes[i];
                 var imgDiv = '<div class="col-xs-6 col-md-3 product-image-thumb">'
-                        + '    <a href="' + hash + '" class="thumbnail select-opt-img"><img src="/_hashes/files/' + hash + '/alt-150-150.png"/></a>'
-                        + '    <a class="btn-image-selected btn btn-xs btn-success" style="display: none;" href="/_hashes/files/' + hash + '">'
-                        + '        <span class="fa fa-check"></span>'
-                        + '    </a>'
-                        + '</div>';
-
+                    + '    <a href="' + hash + '" class="thumbnail select-opt-img"><img src="/_hashes/files/' + hash + '/alt-150-150.png"/></a>'
+                    + '    <a class="btn-image-selected btn btn-xs btn-success" style="display: none;" href="/_hashes/files/' + hash + '">'
+                    + '        <span class="fa fa-check"></span>'
+                    + '    </a>'
+                    + '</div>';
+                
                 $('#modal-option-img').find('.img-list').append(imgDiv);
             }
-
+            
             $('#modal-option-img').find('input[name=skuId]').val(skuId);
-
+            
             $('#modal-option-img').modal('show');
         });
-
+        
         $('#modal-option-img').find('form').forms({
             callback: function (resp) {
                 $('#modal-option-img').modal('hide');
                 reloadSkuTable();
             }
         });
-
+        
         $('body').on('click', '.btn-sku-img-del', function (e) {
             e.preventDefault();
-
+            
             var btn = $(this);
             var row = btn.closest('tr');
             var rowId = $(row).attr("id");
             var td = $(row).find('td:first-child');
-
+            
             var skuId = td.data('skuid');
-
+            
             removeSkuImage(skuId, rowId);
         });
-
+        
         $('body').on('hidden.bs.modal', '#modal-option-img', function () {
             $('#modal-option-img').find('input[name=updateSkuImageHash]').val(null);
             $('#modal-option-img').find('input[name=skuId]').val(null);
             $('#modal-option-img').find('.btn-image-selected ').removeClass('image-selected');
         });
     }
-
-    function initUpCropImage(btn, skuId, rowId ){
+    
+    function initUpCropImage(btn, skuId, rowId) {
         btn.upcropImage({
             buttonContinueText: 'Save',
             url: window.location.pathname + '?skuId=' + skuId,
@@ -316,7 +317,7 @@
             onContinue: function (resp) {
                 flog("onContinue:", resp, resp.result.nextHref);
                 $.ajax({
-                    url: window.location.pathname+ '?skuId=' + skuId,
+                    url: window.location.pathname + '?skuId=' + skuId,
                     type: 'POST',
                     dataType: 'json',
                     data: {
@@ -339,21 +340,21 @@
             }
         });
     }
-
+    
     function initSkuImgUpload() {
-
+        
         $('.btn-option-img-upload').each(function (i, item) {
             var btn = $(item);
             var row = btn.closest('tr');
             var rowId = $(row).attr("id");
             var td = $(row).find('td:first-child');
-
+            
             var skuId = td.data('skuid');
-
+            
             initUpCropImage(btn, skuId, rowId);
         });
     }
-
+    
     function updateSku(productId, options, skuId, newSku, rowId) {
         $.ajax({
             type: 'POST',
@@ -378,7 +379,7 @@
             }
         });
     }
-
+    
     function updateSkuTitle(skuId, newSkuTitle, rowId) {
         $.ajax({
             type: 'POST',
@@ -401,7 +402,7 @@
             }
         });
     }
-
+    
     function updateSkuBaseCost(skuId, newSkuBaseCost, rowId) {
         $.ajax({
             type: 'POST',
@@ -424,7 +425,7 @@
             }
         });
     }
-
+    
     function updateSkuStock(skuId, locId, newStock, reloadId) {
         $.ajax({
             type: 'POST',
@@ -448,7 +449,7 @@
             }
         });
     }
-
+    
     function removeSkuImage(skuId, rowId) {
         $.ajax({
             type: 'POST',
@@ -472,37 +473,37 @@
             }
         });
     }
-
+    
     function reloadSkuTable() {
         $("#productsTableBody").reloadFragment({
             whenComplete: function () {
-
+                
                 //initSkuImgUpload();
             },
             url: window.location.href
         });
     }
-
+    
     function reloadRow(reloadId) {
         flog("Reloading row", reloadId);
-        $("#"+reloadId).reloadFragment({
+        $("#" + reloadId).reloadFragment({
             whenComplete: function () {
-                $("#"+reloadId+" td:first-child .btn-option-img-upload").each(function (i, item) {
+                $("#" + reloadId + " td:first-child .btn-option-img-upload").each(function (i, item) {
                     var btn = $(item);
                     var row = btn.closest('tr');
                     var rowId = $(row).attr("id");
                     var td = $(row).find('td:first-child');
-
+                    
                     var skuId = td.data('skuid');
-
+                    
                     initUpCropImage(btn, skuId, rowId);
                 });
             },
             url: window.location.href
         });
     }
-
-
+    
+    
     // Run Init functions
     $(function () {
         initUpdateSku();
@@ -513,5 +514,5 @@
         initProductsCsv();
         initUpdateSkuStock();
     });
-
+    
 })();
