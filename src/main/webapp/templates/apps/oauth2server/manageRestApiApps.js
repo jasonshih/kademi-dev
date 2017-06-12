72a4d6026d73944a4c6ed530339cb48fd452c505
DucDHM <ducdhm@gmail.com>
2017-06-13 00:21:23 +0700

    https://github.com/Kademi/kademi-dev/issues/2818 https://github.com/Kademi/kademi-dev/issues/2919

--- src/main/webapp/templates/apps/oauth2server/manageRestApiApps.js
+++ src/main/webapp/templates/apps/oauth2server/manageRestApiApps.js
@@ -1,9 +1,9 @@
 (function ($) {
-
+    
     function initCreateApiApp() {
         var modal = $('#modal-create-app');
         var modalForm = modal.find('form');
-
+        
         modalForm.forms({
             onSuccess: function (resp) {
                 modal.modal('hide');
@@ -12,45 +12,38 @@
             }
         });
     }
-
+    
     function initDeleteApp() {
         $('body').on('click', '.btn-delete-app', function (e) {
             e.preventDefault();
-
+            
             var btn = $(this);
             var href = btn.attr('href');
-
-            swal({
-                title: "Are you sure?",
-                text: "Your will not be able to recover this api app!",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonClass: "btn-danger",
-                confirmButtonText: "Yes, delete it!",
-                closeOnConfirm: false,
-                showLoaderOnConfirm: true
-            },
-            function () {
+            
+            Konfirm.error({
+                message: 'Your will not be able to recover this api app!',
+                confirmText: 'Yes, delete it!'
+            }, function () {
                 $.ajax({
                     type: 'DELETE',
                     url: href,
                     dataType: 'json',
                     success: function () {
                         refreshTable();
-                        swal('Deleted!', 'Your API App has been deleted', 'success');
+                        Kalert.success('Deleted!', 'Your API App has been deleted');
                     },
                     error: function () {
-                        swal('Oh No!', 'Sorry, an error occured deleting ' + href + '. Please check your internet connection', 'danger');
+                        Kalert.error('Oh No!', 'Sorry, an error occured deleting ' + href + '. Please check your internet connection');
                     }
                 });
             });
         });
     }
-
+    
     function refreshTable() {
         $('#restApiAppTable').reloadFragment();
     }
-
+    
     // Run init methods
     $(function () {
         initCreateApiApp();
