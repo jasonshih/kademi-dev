69e8d4af1b7b0233a1ed0610c3b2cf1c7d02cd2d
Brad Mac <brad@mcevoysoftware.co.nz>
2017-06-13 13:14:59 +1200

    only show bandwidth widget on admin dashboard if there are websites

--- src/main/marketplace/apps/KRewardStore/APP-INF/app.js
+++ src/main/marketplace/apps/KRewardStore/APP-INF/app.js
@@ -1,7 +1,9 @@
 function initRewardStoreApp(orgRoot, webRoot, enabled){
-    log.info("initRewardStoreApp: orgRoot={}", orgRoot);
+    var rewardStoreApp = applications.rewardStore;
+    log.info("initRewardStoreApp: orgRoot={} app={}", orgRoot, rewardStoreApp);
     var alertsApp = applications.alerts;
     if( webRoot ){
+        
         var webName = webRoot.websiteName;
         // check and create the participants group
         var groupName = "participants";
@@ -14,30 +16,23 @@ function initRewardStoreApp(orgRoot, webRoot, enabled){
             }
         }
         
-        // check and create the points bucket
-        var manageBuckets = orgRoot.find("points-buckets");
+        // check and create the points bucket        
         var bucketName = webName + "-points";
-        var defaultBucket = manageBuckets.child(bucketName);
-        if( defaultBucket == null ) {
-            manageBuckets.createPointsBucket(bucketName, group.name, webName);
+        if( rewardStoreApp.checkCreatePointsBucket(bucketName, group.name, webName) ) {
             log.info("Created points bucket {}", bucketName);
             if( alertsApp ) {
                 alertsApp.createAdminAlert("Reward Store", "We've created a points bucket called " + bucketName + " for your new reward store. You can <a href='/points-buckets/" + bucketName + "/'>manage it here</a>");
-            }            
+            }                        
         }
         
         // check and create the reward store
         var rewardStoreName = webName + "-store";
-        var manageStores = orgRoot.find("reward-store");
-        var defaultRewardStore = manageStores.child(rewardStoreName);
-        if( defaultRewardStore == null ) {
-            manageStores.createRewardStore(rewardStoreName, bucketName, webName);
+        if( rewardStoreApp.checkCreateRewardStore(rewardStoreName, bucketName, webName) ) {
             log.info("Created reward store");
             if( alertsApp ) {
                 alertsApp.createAdminAlert("Reward Store", "We've created a reward store called " + rewardStoreName + ". You can <a href='/reward-store/" + rewardStoreName + "/'>manage it here</a>");
-            }                        
-        }
-        
+            }                                    
+        }        
     } else {
         log.info("I'm in an organisation");
     }
