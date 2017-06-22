47f67ea1373d135714594213174738ce75193a63
kevin3489 <tannguyen3489@gmail.com>
2017-06-22 11:53:38 +0700

    calendar lib js error #3437

--- src/main/marketplace/libs/content-lib/website/theme/apps/content/page.js
+++ src/main/marketplace/libs/content-lib/website/theme/apps/content/page.js
@@ -1,21 +1,29 @@
 (function ($) {
     $(document).ready(function () {
-        pageInitFunctions.push(function () {
-            if ( $editMode) {
-                edify($(".editableContent"), function (resp) {
-                    log("page saved", resp);
-                    if (resp.status) {
-                        log("next href", resp.nextHref);
-                        if (resp.nextHref) {
-                            window.location = resp.nextHref;
-                        } else {
-                            window.location.href = window.location.pathname;
-                        }
-                    } else {
-                        Msg.error("There was a problem saving the page");
-                    }
-                });
+        var $content = $('.content-page');
+        if($content.length > 0) {
+            if(!pageInitFunctions) {
+                pageInitFunctions = new Array();
             }
-        });
+            var $editMode = $content.data('editmode');
+            pageInitFunctions.push(function () {
+                if ($editMode) {
+                    edify($(".editableContent"), function (resp) {
+                        log("page saved", resp);
+                        if (resp.status) {
+                            log("next href", resp.nextHref);
+                            if (resp.nextHref) {
+                                window.location = resp.nextHref;
+                            } else {
+                                window.location.href = window.location.pathname;
+                            }
+                        } else {
+                            Msg.error("There was a problem saving the page");
+                        }
+                    });
+                }
+            });
+        }
+
     });
 }(jQuery));
