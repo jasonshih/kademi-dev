b256b5b94ea10606a7a7bd7d954b034b1ec01312
DucDHM <ducdhm@gmail.com>
2017-06-30 16:18:51 +0700

    https://github.com/Kademi/kademi-dev/issues/2628 - googlemap-lib

---
+++ src/main/marketplace/libs/googlemap-lib/APP-INF/app.js
@@ -0,0 +1,29 @@
+(function (g) {
+    var APP_NAME = 'googlemap-lib';
+    
+    g._saveSettings = function (page, params) {
+        var apiKey = params.apiKey || '';
+        
+        page.setAppSetting(APP_NAME, "apiKey", apiKey);
+        
+        return views.jsonResult(true);
+    };
+    
+    
+    g.getAppSettings = function (page) {
+        var websiteFolder = page.closest('websiteVersion');
+        var org = page.organisation;
+        var branch = null;
+        
+        if (websiteFolder !== null && typeof websiteFolder !== 'undefined') {
+            branch = websiteFolder.branch;
+        }
+        
+        var app = applications.get(APP_NAME);
+        if (app !== null) {
+            var settings = app.getAppSettings(org, branch);
+            return settings;
+        }
+        return null;
+    };
+})(this);
