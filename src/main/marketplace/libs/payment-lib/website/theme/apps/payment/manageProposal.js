af1656ce007765cb0012719a1d61fc90bc45561d
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2017-07-09 15:25:03 +0200

    Various Fixes

--- src/main/marketplace/libs/payment-lib/website/theme/apps/payment/manageProposal.js
+++ src/main/marketplace/libs/payment-lib/website/theme/apps/payment/manageProposal.js
@@ -3,14 +3,35 @@ $(function () {
 });
 
 function initProposalDetailsForm() {
+    var modal = $("#addQuoteInProposalModal");
+
+    if (modal.length > 0) {
+        var form = modal.find("form");
+
+        form.forms({
+            callback: function (resp) {
+                flog("done", resp);
+                modal.modal('hide');
+                Msg.success('Quote is created!');
+                reloadQuoteTable();
+            }
+        });
+
+        form.find("#supplier").entityFinder({
+            url: '/custs',
+            useActualId: true
+        });
+    }
+
     var form = $('.proposal-details-form');
+
     if (form.length > 0) {
         form.find('.proposal-customer').entityFinder({
             url: '/custs',
             useActualId: true,
             type: 'profile'
         });
-        
+
         form.forms({
             onSuccess: function (resp) {
                 if (resp.nextHref) {
@@ -21,21 +42,21 @@ function initProposalDetailsForm() {
                 }
             }
         });
-        
+
         $('.btn-quick-add-line-item').on('click', function (e) {
             e.preventDefault();
-            
+
             $('.add-quote-line-item-wrapper').toggle();
         });
-        
+
         var ddlStatus = $('.proposal-status');
         if (ddlStatus.length > 0) {
             ddlStatus.find('a').on('click', function (e) {
                 e.preventDefault();
-                
+
                 var a = $(this);
                 var status = a.attr('href').replace('#', '');
-                
+
                 $.ajax({
                     url: window.location.pathname,
                     type: 'post',
@@ -52,9 +73,20 @@ function initProposalDetailsForm() {
                             Msg.error('Can\'t update status. Please contact your administrator to resolve this issue');
                             flog('Error when updating status', resp);
                         }
-                     }
+                    }
                 });
             });
         }
+    }
+
+    function reloadQuoteTable() {
+        var href = window.location.pathname;
+
+        $('#quotesTable').reloadFragment({
+            url: href,
+            whenComplete: function () {
+                $('abbr.timeago').timeago();
+            }
+        });
     }
 }
