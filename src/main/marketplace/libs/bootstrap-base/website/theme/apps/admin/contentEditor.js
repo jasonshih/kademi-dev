120100416829b3f1ea618f3e18c6d31938b96781
DucDHM <ducdhm@gmail.com>
2017-07-10 17:30:17 +0700

    https://github.com/Kademi/kademi-dev/issues/3533

--- src/main/marketplace/libs/bootstrap-base/website/theme/apps/admin/contentEditor.js
+++ src/main/marketplace/libs/bootstrap-base/website/theme/apps/admin/contentEditor.js
@@ -1,6 +1,19 @@
 var win = $(window);
 var contentEditor = true;
 
+$(document).on({
+    'show.bs.modal': function () {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function () {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        }, 0);
+    },
+    'hidden.bs.modal': function () {
+        $('.modal:visible').length && $(document.body).addClass('modal-open');
+    }
+}, '.modal');
+
 function initContentEditorPage(options) {
     flog('initContentEditorPage fileName=' + options.fileName);
     
@@ -22,7 +35,7 @@ function initNavbar() {
     
     $('.content-editor-toggle').on('click', function (e) {
         e.preventDefault();
-    
+        
         nav.toggleClass('closed');
         this.setAttribute('title', nav.hasClass('closed') ? 'Open navbar' : 'Close navbar');
     });
@@ -140,9 +153,15 @@ function initKEditor(options) {
     
     // Stop prevent reloading page or redirecting to other pages
     $(document.body).on('click', '.keditor-component-content a', function (e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        e.stopPropagation();
+        var a = $(this);
+        
+        if (a.is('[data-slide]') || a.is('[data-slide-to]')) {
+        
+        } else {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            e.stopPropagation();
+        }
     });
 }
 
