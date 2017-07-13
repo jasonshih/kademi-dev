855708a96c1a963906de325fc6f5e8721aaf1b6b
DucDHM <ducdhm@gmail.com>
2017-07-13 16:39:47 +0700

    https://github.com/Kademi/kademi-dev/issues/3586

--- src/main/webapp/templates/apps/email/manageUsersEmailPortlet.js
+++ src/main/webapp/templates/apps/email/manageUsersEmailPortlet.js
@@ -1,45 +1,27 @@
 function initManagerUserEmailPortlet() {
-    $("abbr.timeago").timeago();
-
     var modal = $('#modal-compose-email');
     var form = modal.find('form');
-
+    
+    initHtmlEditors(modal.find('.htmleditor'), getStandardEditorHeight(), null, null, standardRemovePlugins + ',autogrow');
+    
     form.forms({
         callback: function (resp) {
             modal.modal('hide');
             $('#emailHistoryBody').reloadFragment();
         }
     });
-
-    $('body').on('show.bs.modal', '#modal-compose-email', function () {
-        $('#email_message').wysihtml5({
-            toolbar: {
-                "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
-                "emphasis": true, //Italics, bold, etc. Default true
-                "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
-                "html": false, //Button which allows you to edit the generated HTML. Default false
-                "link": true, //Button to insert a link. Default true
-                "image": false, //Button to insert an image. Default true,
-                "color": false, //Button to change color of font  
-                "blockquote": true, //Blockquote  
-            }
-        });
-    });
-
-    $('body').on('hidden.bs.modal', '#modal-compose-email', function () {
-        $('.wysihtml5-sandbox, .wysihtml5-toolbar').remove();
-        $('#email_message').show();
-
+    
+    $(document.body).on('hidden.bs.modal', '#modal-compose-email', function () {
         form.trigger('reset');
     });
-
+    
     var receivedCheckbox = $('input[type="checkbox"].icheck.received');
     receivedCheckbox.iCheck({
         checkboxClass: 'icheckbox_flat-red',
         radioClass: 'iradio_flat-red',
         increaseArea: '10%' // optional
     });
-
+    
     receivedCheckbox.on('ifToggled', function (e) {
         var btn = $(this);
         var checked = btn.is(':checked');
@@ -50,15 +32,15 @@ function initManagerUserEmailPortlet() {
             $('tr.email-received').hide();
         }
     });
-
+    
     var sentCheckbox = $('input[type="checkbox"].icheck.sent');
-
+    
     sentCheckbox.iCheck({
         checkboxClass: 'icheckbox_flat-green',
         radioClass: 'iradio_flat-green',
         increaseArea: '10%'
     });
-
+    
     sentCheckbox.on('ifToggled', function (e) {
         var btn = $(this);
         var checked = btn.is(':checked');
