687170fb9ff3b5262958a4ecbf0a23798d909229
DucDHM <ducdhm@gmail.com>
2017-07-16 11:16:48 +0700

    Move full-height container to container.js of content-lib

--- src/main/marketplace/libs/content-lib/website/theme/apps/content/container.js
+++ src/main/marketplace/libs/content-lib/website/theme/apps/content/container.js
@@ -2,23 +2,46 @@
  * Created by Anh on 14/07/2017.
  */
 (function ($) {
+    var win = $(window);
+    
     $(function () {
-        $(window).on('load', function () {
+        initFullHeightContainer();
+        initMultiBackgroundContainer();
+    });
+    
+    function initFullHeightContainer() {
+        flog('initFullHeightContainer');
+        
+        var winTimer;
+        win.on('resize', function () {
+            clearTimeout(winTimer);
+            winTimer = setTimeout(function () {
+                $('.container-full-height').css('min-height', win.height());
+            }, 250);
+            
+        }).trigger('resize');
+    }
+    
+    function initMultiBackgroundContainer() {
+        flog('initMultiBackgroundContainer');
+        
+        win.on('load', function () {
             $('.container-bg').each(function () {
                 var item = $(this);
                 var isMulti = item.attr('data-multiple-bg') == 'true';
                 var imagesStr = item.attr('data-images');
-                var transition = item.attr('data-bg-transition')*1000 || 2000;
-
-                if (isMulti && imagesStr){
+                var transition = item.attr('data-bg-transition') * 1000 || 2000;
+                
+                if (isMulti && imagesStr) {
                     var imagesArr = imagesStr.split(',');
-                    if (imagesArr.length){
-                        for (var i = 0; i < imagesArr.length; i++){
-                            var img = $('<img>').attr('src', imagesArr[i]).css('position','absolute').css('left','-9999999px');
+                    if (imagesArr.length) {
+                        for (var i = 0; i < imagesArr.length; i++) {
+                            var img = $('<img>').attr('src', imagesArr[i]).css('position', 'absolute').css('left', '-9999999px');
                             $(document.body).append(img);
                         }
-
+                        
                         var count = 1;
+                        
                         function slide() {
                             if (count >= imagesArr.length) {
                                 count = 0
@@ -27,10 +50,12 @@
                             bgFor.addClass('multiple-background-effect').css("background-image", 'url("' + imagesArr[count] + '")');
                             count++;
                         }
+                        
                         setInterval(slide, transition);
                     }
                 }
             })
         });
-    });
+    }
+    
 })(jQuery);
