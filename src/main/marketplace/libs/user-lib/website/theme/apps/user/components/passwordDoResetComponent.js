64e73d7c0be51a5d63d488a28a3c5a2c3abcc325
DucDHM <ducdhm@gmail.com>
2017-07-17 12:49:58 +0700

    https://github.com/Kademi/kademi-dev/issues/3350

--- src/main/marketplace/libs/user-lib/website/theme/apps/user/components/passwordDoResetComponent.js
+++ src/main/marketplace/libs/user-lib/website/theme/apps/user/components/passwordDoResetComponent.js
@@ -1,9 +1,53 @@
 (function ($) {
     var KEditor = $.keditor;
     var flog = KEditor.log;
-
+    
     KEditor.components['passwordDoReset'] = {
-        settingEnabled: false
+        settingEnabled: true,
+        settingTitle: 'Password Do Reset Settings',
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "passwordDoReset" component');
+            
+            return $.ajax({
+                url: '_components/passwordDoReset?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                    
+                    var attrs = ['reset-password-text', 'create-password-text', 'description-text', 'cant-reset-password-text', 'cant-reset-password-description-text', 'placeholder-text', 'button-text', 'redirection-text'];
+                    
+                    $.each(attrs, function (i, value) {
+                        form.find('.txt-' + value).on('change', function () {
+                            var component = keditor.getSettingComponent();
+                            var dynamicElement = component.find('[data-dynamic-href]');
+                            
+                            component.attr('data-' + value, this.value);
+                            keditor.initDynamicContent(dynamicElement);
+                        });
+                    });
+                }
+            });
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "passwordDoReset" component');
+            
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            var attrs = {
+                'reset-password-text': 'Reset your password',
+                'create-password-text': 'Create a password',
+                'description-text': 'Enter a new password below.',
+                'cant-reset-password-text': 'Cannot reset password',
+                'cant-reset-password-description-text': 'Sorry, the user account with your email address does not have access to this site',
+                'placeholder-text': 'New Password',
+                'button-text': 'Update password',
+                'redirection-text': 'Done! Redirecting to your profile page...'
+            };
+            
+            $.each(attrs, function (name, value) {
+                form.find('.txt-' + name).val(dataAttributes['data-' + name] || value);
+            });
+        }
     };
-
+    
 })(jQuery);
