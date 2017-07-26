f1b66dd228481b96bb20e6bce1cc701f4a20eebe
DucDHM <ducdhm@gmail.com>
2017-07-26 15:47:30 +0700

    https://github.com/Kademi/kademi-dev/issues/3645

--- src/main/webapp/templates/apps/admin/profile.js
+++ src/main/webapp/templates/apps/admin/profile.js
@@ -20,11 +20,11 @@ function initProfile() {
     
     flog("init delete membersip");
     $(document).on("click", ".btn-delete-membership", function (e) {
-        flog("click", this);
+        var a = $(this);
         e.preventDefault();
         e.stopPropagation();
+        
         Kalert.confirm("Are you sure you want to delete this group membership?", function () {
-            var a = $(this);
             var href = a.attr("href");
             deleteFile(href, function () {
                 reloadMemberships();
