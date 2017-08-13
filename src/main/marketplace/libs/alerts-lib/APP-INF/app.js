bf656daa2540e8ec84b375ae2efa23935b8d6678
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2017-08-13 17:06:51 +0200

    Another silly fix to alerts-lib

--- src/main/marketplace/libs/alerts-lib/APP-INF/app.js
+++ src/main/marketplace/libs/alerts-lib/APP-INF/app.js
@@ -1,5 +1,5 @@
 controllerMappings.addComponent("alerts/components", "alerts", "html", "Alerts displayed on dashboard", "Alerts App component");
-controllerMappings.addActionNodeType("createAlertAction", "vayaExtra/jb/createAlertActionNode.js", 'createAlertActionNodeEntered');
+controllerMappings.addActionNodeType("createAlertAction", "alerts/jb/createAlertActionNode.js", 'createAlertActionNodeEntered');
 
 function createAlertActionNodeEntered(rootFolder, lead, funnel, exitingNode, settings) {
     log.info('createAlertActionNodeEntered {} {}', lead, funnel);
