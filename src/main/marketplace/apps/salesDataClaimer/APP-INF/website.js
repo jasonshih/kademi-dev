233e3367a4c7230e7b72174b6f6717f1ee2b7c1c
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-08-21 23:55:21 +0700

    https://github.com/Kademi/kademi-dev/issues/3724

--- src/main/marketplace/apps/salesDataClaimer/APP-INF/website.js
+++ src/main/marketplace/apps/salesDataClaimer/APP-INF/website.js
@@ -17,45 +17,87 @@ controllerMappings
     .enabled(true)
     .build();
 
+controllerMappings
+    .websiteController()
+    .path('/salesDataClaims/(?<claimId>[^/]*)')
+    .addMethod('GET', 'checkRedirect')
+    .enabled(true)
+    .build();
+
+controllerMappings
+    .websiteController()
+    .path('/salesDataClaims/(?<claimId>[^/]*)/')
+    .addMethod('GET', 'getClaim')
+    .addMethod('POST', 'updateClaim', 'updateClaim')
+    .postPriviledge('WRITE_CONTENT')
+    .enabled(true)
+    .build();
+
 function getClaims(page, params) {
     log.info('getClaims > page={}, params={}', page, params);
     
-    try {
-        var currentUser = securityManager.getCurrentUser();
-        var queryJson = {
-            'stored_fields': [
-                'recordId',
-                'soldDate',
-                'enteredDate',
-                'modifiedDate',
-                'amount',
-                'status',
-                'productSku',
-                'field1',
-                'field2',
-                'field3',
-                'field4',
-                'field5',
-                'tags'
-            ],
-            'size': 10000,
-            'query': {
-                'bool': {
-                    'must': [
-                        {'type': {'value': TYPE_RECORD}},
-                        {'term': {'soldBy': currentUser.name}}
-                    ]
+    if (!params.claimId) {
+        try {
+            var currentUser = securityManager.getCurrentUser();
+            var queryJson = {
+                'stored_fields': [
+                    'recordId',
+                    'soldDate',
+                    'enteredDate',
+                    'modifiedDate',
+                    'amount',
+                    'status',
+                    'productSku',
+                    'field1',
+                    'field2',
+                    'field3',
+                    'field4',
+                    'field5',
+                    'tags'
+                ],
+                'size': 10000,
+                'query': {
+                    'bool': {
+                        'must': [
+                            {'type': {'value': TYPE_RECORD}},
+                            {'term': {'soldBy': currentUser.name}}
+                        ]
+                    }
                 }
-            }
-        };
-        
-        var searchResult = doDBSearch(page, queryJson);
-        
-        page.attributes.searchResult = searchResult;
-        
-    } catch (e) {
-        log.error('ERROR in getClaims: ' + e);
+            };
+            
+            var searchResult = doDBSearch(page, queryJson);
+            
+            page.attributes.searchResult = searchResult;
+        } catch (e) {
+            log.error('ERROR in getClaims: ' + e);
+        }
     }
+}
+
+function getClaim(page, params) {
+    log.info('getClaim > page={}, params={}', page, params);
+    
+    var result = {
+        status: true
+    };
+    
+    try {
+        var db = getDB(page);
+        var claim = db.child(page.attributes.claimId);
+        
+        if (claim !== null) {
+            result.data = claim.jsonObject + '';
+        } else {
+            result.status = false;
+            result.messages = ['This claim does not exist'];
+        }
+    } catch (e) {
+        result.status = false;
+        result.messages = ['Error when getting claim: ' + e];
+    }
+    
+    return views.jsonObjectView(JSON.stringify(result));
 }
 
 function createClaim(page, params) {
@@ -111,6 +153,69 @@ function createClaim(page, params) {
     } catch (e) {
         result.status = false;
         result.messages = ['Error when creating claim: ' + e];
+    }
+    
+    return views.jsonObjectView(JSON.stringify(result))
+}
+
+function updateClaim(page, params) {
+    log.info('updateClaim > page={}, params={}', page, params);
+    
+    var result = {
+        status: true
+    };
+    
+    try {
+        var db = getDB(page);
+        var id = page.attributes.claimId;
+        var claim = db.child(id);
+        
+        if (claim !== null) {
+            var amount = +params.amount;
+            if (isNaN(amount)) {
+                result.status = false;
+                result.messages = ['Amount must be digits'];
+                return views.jsonObjectView(JSON.stringify(result))
+            }
+            
+            var tags = (params.tags || '').trim();
+            tags = tags !== '' ? tags.split(',') : null;
+            if (tags && tags.length > 5) {
+                result.status = false;
+                result.messages = ['Maximum tags is 5'];
+                return views.jsonObjectView(JSON.stringify(result))
+            }
+            
+            var tempDateTime = params.soldDate;
+            var tempDate = tempDateTime.substring(0, tempDateTime.indexOf(' ')).split('/');
+            var tempTime = tempDateTime.substring(tempDateTime.indexOf(' ') + 1, tempDateTime.length).split(':');
+            var soldDate = new Date(+tempDate[2], +tempDate[1] - 1, +tempDate[0], +tempTime[0], +tempTime[1], 00, 00);
+            
+            
+            var obj = {
+                recordId: id,
+                soldBy: claim.jsonObject.soldBy,
+                amount: amount,
+                soldDate: soldDate,
+                enteredDate: claim.jsonObject.enteredDate,
+                modifiedDate: new Date(),
+                productSku: params.productSku || '',
+                status: claim.jsonObject.status,
+                tags: claim.jsonObject.tags
+            };
+            
+            if (tags) {
+                obj.tags = tags.join(',');
+            }
+            
+            claim.update(JSON.stringify(obj), TYPE_RECORD);
+        } else {
+            result.status = false;
+            result.messages = ['This claim does not exist'];
+        }
+    } catch (e) {
+        result.status = false;
+        result.messages = ['Error when updating claim: ' + e];
     }
     
     return views.jsonObjectView(JSON.stringify(result))
--- src/main/marketplace/apps/salesDataClaimer/website/theme/apps/salesDataClaimer/claimsList.js
+++ src/main/marketplace/apps/salesDataClaimer/website/theme/apps/salesDataClaimer/claimsList.js
@@ -1,4 +1,6 @@
 (function ($) {
+    var MAIN_URL = '/salesDataClaims/';
+    
     $(function () {
         var components = $('.claims-list-component');
         
@@ -11,9 +13,54 @@
     function initClaimsTable() {
         var table = $('#table-claims');
         var tbody = $('#table-claims-body');
+        var modal = $('#modal-add-claim');
+        var form = modal.find('form');
         
         table.find('.chk-all').on('click', function () {
             tbody.find(':checkbox').prop('checked', this.checked);
+        });
+        
+        $('.btn-add-claim').on('click', function (e) {
+            e.preventDefault();
+            
+            form.attr('action', MAIN_URL);
+            modal.find('.modal-action').attr('name', 'createClaim');
+        });
+        
+        table.on('click', '.btn-edit-claim', function (e) {
+            e.preventDefault();
+            
+            var id = $(this).attr('data-id');
+            var url = MAIN_URL + id + '/';
+            
+            $.ajax({
+                url: url,
+                type: 'get',
+                dataType: 'json',
+                success: function (resp) {
+                    if (resp && resp.status) {
+                        form.attr('action', url);
+                        modal.find('.modal-action').attr('name', 'updateClaim');
+                        
+                        $.each(resp.data, function (key, value) {
+                            var newValue = value;
+                            if (key === 'soldDate') {
+                                newValue = moment(value).format('DD/MM/YYYY HH:mm');
+                            }
+                            
+                            modal.find('[name=' + key + ']').val(newValue);
+                        });
+                        
+                        modal.modal('show');
+                    } else {
+                        alert('Error in getting claim data. Please contact your administrators to resolve this issue.');
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert('Error in getting claim data: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
+                    flog('Error in getting claim data', jqXHR, textStatus, errorThrown);
+                }
+            })
         });
         
         tbody.find('.timeago').timeago();
@@ -27,33 +74,33 @@
                 var isConfirmed = confirm('Are you that you want to request approval for ' + checked.length + ' selected ' + (checked.length > 1 ? 'claims' : 'claim') + '?');
                 
                 if (isConfirmed) {
-                var ids = [];
-                checked.each(function () {
-                    ids.push(this.value);
-                });
-                
-                $.ajax({
-                    url: '/salesDataClaims/',
-                    type: 'POST',
-                    dataType: 'JSON',
-                    data: {
-                        requestApproval: true,
-                        ids: ids.join(',')
-                    },
-                    success: function (resp) {
-                        if (resp && resp.status) {
-                            reloadClaimsList(function () {
-                                Msg.success('Request approval succeed');
-                            })
-                        } else {
-                            alert('Error in requesting approval. Please contact your administrators to resolve this issue.');
+                    var ids = [];
+                    checked.each(function () {
+                        ids.push(this.value);
+                    });
+                    
+                    $.ajax({
+                        url: MAIN_URL,
+                        type: 'POST',
+                        dataType: 'JSON',
+                        data: {
+                            requestApproval: true,
+                            ids: ids.join(',')
+                        },
+                        success: function (resp) {
+                            if (resp && resp.status) {
+                                reloadClaimsList(function () {
+                                    Msg.success('Request approval succeed');
+                                })
+                            } else {
+                                alert('Error in requesting approval. Please contact your administrators to resolve this issue.');
+                            }
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            alert('Error in requesting approval: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
+                            flog('Error in requesting approval', jqXHR, textStatus, errorThrown);
                         }
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) {
-                        alert('Error in requesting approval: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
-                        flog('Error in requesting approval', jqXHR, textStatus, errorThrown);
-                    }
-                });
+                    });
                 }
             } else {
                 alert('Please select claims which you want to request approval');
@@ -69,33 +116,33 @@
                 var isConfirmed = confirm('Are you that you want to delete ' + checked.length + ' selected ' + (checked.length > 1 ? 'claims' : 'claim') + '?');
                 
                 if (isConfirmed) {
-                var ids = [];
-                checked.each(function () {
-                    ids.push(this.value);
-                });
-                
-                $.ajax({
-                    url: '/salesDataClaims/',
-                    type: 'POST',
-                    dataType: 'JSON',
-                    data: {
-                        deleteClaims: true,
-                        ids: ids.join(',')
-                    },
-                    success: function (resp) {
-                        if (resp && resp.status) {
-                            reloadClaimsList(function () {
-                                Msg.success('Deleted');
-                            })
-                        } else {
-                            alert('Error in deleting. Please contact your administrators to resolve this issue.');
+                    var ids = [];
+                    checked.each(function () {
+                        ids.push(this.value);
+                    });
+                    
+                    $.ajax({
+                        url: MAIN_URL,
+                        type: 'POST',
+                        dataType: 'JSON',
+                        data: {
+                            deleteClaims: true,
+                            ids: ids.join(',')
+                        },
+                        success: function (resp) {
+                            if (resp && resp.status) {
+                                reloadClaimsList(function () {
+                                    Msg.success('Deleted');
+                                })
+                            } else {
+                                alert('Error in deleting. Please contact your administrators to resolve this issue.');
+                            }
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            alert('Error in deleting: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
+                            flog('Error in deleting', jqXHR, textStatus, errorThrown);
                         }
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) {
-                        alert('Error in deleting: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
-                        flog('Error in deleting', jqXHR, textStatus, errorThrown);
-                    }
-                });
+                    });
                 }
             } else {
                 alert('Please select claims which you want to delete');
@@ -130,6 +177,10 @@
                     modal.modal('hide');
                 });
             }
+        });
+        
+        modal.on('hidden.bs.modal', function () {
+            form.find('input').not('[name=soldBy]').val('');
         });
     }
     
