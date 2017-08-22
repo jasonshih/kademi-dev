77d150bbbce2d8b4bfb8626839e635778ce5081d
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-08-22 17:22:29 +0700

    https://github.com/Kademi/kademi-dev/issues/3724

--- src/main/marketplace/apps/salesDataClaimer/admin/theme/apps/salesDataClaimer/viewClaims.js
+++ src/main/marketplace/apps/salesDataClaimer/admin/theme/apps/salesDataClaimer/viewClaims.js
@@ -1,30 +1,91 @@
 (function ($) {
-    var MAIN_URL = '/salesDataClaims/';
+    var MAIN_URL = '/manageSaleDataClaimer/';
+    var RECORD_STATUS = {
+        NEW: 0,
+        REQUESTING: 1,
+        APPROVED: 2,
+        REJECTED: -1
+    };
     
     $(function () {
         var components = $('.claims-list-component');
         
         if (components.length > 0) {
-            initModalAddClaim();
+            initModalReviewClaim();
             initClaimsTable();
         }
     });
     
+    function changeClaimsStatus(status) {
+        var table = $('#table-claims');
+        var tbody = $('#table-claims-body');
+        
+        var action;
+        var actionCapitalize;
+        var actionVing;
+        switch (status) {
+            case RECORD_STATUS.APPROVED:
+                action = 'approve';
+                actionCapitalize = 'Approve';
+                actionVing = 'approving';
+                break;
+                
+            case RECORD_STATUS.REJECTED:
+                action = 'reject';
+                actionCapitalize = 'Reject';
+                actionVing = 'Rejecting';
+                break;
+        }
+        
+        var checked = tbody.find(':checkbox:checked');
+        
+        if (checked.length > 0) {
+            var isConfirmed = confirm('Are you that you want to ' + action + ' ' + checked.length + ' selected ' + (checked.length > 1 ? 'claims' : 'claim') + '?');
+            
+            if (isConfirmed) {
+                var ids = [];
+                checked.each(function () {
+                    ids.push(this.value);
+                });
+                
+                var data = {
+                    ids: ids.join(',')
+                };
+                data[action + 'Claims'] = true;
+                
+                $.ajax({
+                    url: MAIN_URL,
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: data,
+                    success: function (resp) {
+                        if (resp && resp.status) {
+                            reloadClaimsList(function () {
+                                Msg.success(actionCapitalize + ' succeed');
+                            })
+                        } else {
+                            alert('Error in ' + actionVing + ' claims. Please contact your administrators to resolve this issue.');
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        alert('Error in ' + actionVing + ' claims: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
+                        flog('Error in ' + actionVing + ' claims', jqXHR, textStatus, errorThrown);
+                    }
+                });
+            }
+        } else {
+            alert('Please select claims which you want to ' + action);
+        }
+    }
+    
     function initClaimsTable() {
         var table = $('#table-claims');
         var tbody = $('#table-claims-body');
-        var modalAdd = $('#modal-add-claim');
-        var formAdd = modalAdd.find('form');
+        var modal = $('#modal-add-claim');
+        var form = modal.find('form');
         
         table.find('.chk-all').on('click', function () {
             tbody.find(':checkbox').prop('checked', this.checked);
-        });
-        
-        $('.btn-add-claim').on('click', function (e) {
-            e.preventDefault();
-            
-            formAdd.attr('action', MAIN_URL);
-            modalAdd.find('.modal-action').attr('name', 'createClaim');
         });
         
         var uri = new URI(window.location.href);
@@ -87,8 +148,8 @@
                 dataType: 'json',
                 success: function (resp) {
                     if (resp && resp.status) {
-                        formAdd.attr('action', url);
-                        modalAdd.find('.modal-action').attr('name', 'updateClaim');
+                        form.attr('action', url);
+                        modal.find('.modal-action').attr('name', 'updateClaim');
                         
                         $.each(resp.data, function (key, value) {
                             var newValue = value;
@@ -96,10 +157,10 @@
                                 newValue = moment(value).format('DD/MM/YYYY HH:mm');
                             }
                             
-                            modalAdd.find('[name=' + key + ']').val(newValue);
+                            modal.find('[name=' + key + ']').val(newValue);
                         });
                         
-                        modalAdd.modal('show');
+                        modal.modal('show');
                     } else {
                         alert('Error in getting claim data. Please contact your administrators to resolve this issue.');
                     }
@@ -113,46 +174,16 @@
         
         tbody.find('.timeago').timeago();
         
-        $('.btn-request-approval').on('click', function (e) {
+        $('.btn-approve-claims').on('click', function (e) {
             e.preventDefault();
             
-            var checked = tbody.find(':checkbox:checked');
+            changeClaimsStatus(RECORD_STATUS.APPROVED);
+        });
+        
+        $('.btn-reject-claims').on('click', function (e) {
+            e.preventDefault();
             
-            if (checked.length > 0) {
-                var isConfirmed = confirm('Are you that you want to request approval for ' + checked.length + ' selected ' + (checked.length > 1 ? 'claims' : 'claim') + '?');
-                
-                if (isConfirmed) {
-                    var ids = [];
-                    checked.each(function () {
-                        ids.push(this.value);
-                    });
-                    
-                    $.ajax({
-                        url: MAIN_URL,
-                        type: 'POST',
-                        dataType: 'JSON',
-                        data: {
-                            requestApproval: true,
-                            ids: ids.join(',')
-                        },
-                        success: function (resp) {
-                            if (resp && resp.status) {
-                                reloadClaimsList(function () {
-                                    Msg.success('Request approval succeed');
-                                })
-                            } else {
-                                alert('Error in requesting approval. Please contact your administrators to resolve this issue.');
-                            }
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            alert('Error in requesting approval: ' + errorThrown + '. Please contact your administrators to resolve this issue.');
-                            flog('Error in requesting approval', jqXHR, textStatus, errorThrown);
-                        }
-                    });
-                }
-            } else {
-                alert('Please select claims which you want to request approval');
-            }
+            changeClaimsStatus(RECORD_STATUS.REJECTED);
         });
         
         $('.btn-delete-claims').on('click', function (e) {
@@ -198,33 +229,9 @@
         });
     }
     
-    function initModalAddClaim() {
-        flog('initModalAddClaim');
-        
-        var modal = $('#modal-add-claim');
-        var form = modal.find('.form-new-claim');
-        
-        form.find('.date-time-picker').each(function () {
-            var input = $(this);
-            var format = input.attr('data-format') || 'DD/MM/YYYY';
-            
-            input.datetimepicker({
-                format: format
-            });
-        });
-        
-        form.forms({
-            onSuccess: function () {
-                reloadClaimsList(function () {
-                    Msg.success('New claim is created!');
-                    modal.modal('hide');
-                });
-            }
-        });
-        
-        modal.on('hidden.bs.modal', function () {
-            form.find('input').not('[name=soldBy]').val('');
-        });
+    function initModalReviewClaim() {
+        var modal = $('#modal-review-claim');
+        var form = modal.find('form');
     }
     
     function reloadClaimsList(callback) {
