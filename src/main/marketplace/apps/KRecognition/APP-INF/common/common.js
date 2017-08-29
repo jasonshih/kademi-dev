7a0dc4e9026dd27436d4cfe053d5be81e34bd331
dylan <dylan@dylan-Kademi>
2017-08-29 17:09:26 +1200

    https://github.com/Kademi/kademi-dev/issues/3762

--- src/main/marketplace/apps/KRecognition/APP-INF/common/common.js
+++ src/main/marketplace/apps/KRecognition/APP-INF/common/common.js
@@ -1,4 +1,4 @@
-/* global log, views, applications, Utils, formatter */
+/* global log, views, applications, Utils, formatter, fileManager */
 
 (function (g) {
     g.initApp = function (orgRoot, webRoot, enabled) {
@@ -58,5 +58,26 @@
         }
 
         return views.redirectView(href);
+    };
+
+    g.cropImage = function (name, params) {
+        var hash = Utils.safeString(params.uploadedHref).replace('/_hashes/files/', '');
+        var imageDim = fileManager.utils.getImageDimensions(hash);
+
+        var x = Utils.safeInt(params.x);
+        var y = Utils.safeInt(params.y);
+        var w = Utils.safeInt(params.w);
+        var h = Utils.safeInt(params.h);
+
+        if (w > imageDim.width) {
+            w = imageDim.width;
+        }
+
+        if (h > imageDim.height) {
+            h = imageDim.height;
+        }
+
+        var fileExt = (Utils.isStringNotBlank(name) ? formatter.fileExt(name) : null);
+        return fileManager.utils.cropImage(hash, x, y, w, h, fileExt);
     };
 })(this);
