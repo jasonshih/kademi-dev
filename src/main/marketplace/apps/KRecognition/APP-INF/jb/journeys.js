3da74343a833c627c1263cb105f0cf2268c7294e
dylan <dylan@dylan-Kademi>
2017-08-30 17:50:09 +1200

    https://github.com/Kademi/kademi-dev/issues/3762

--- src/main/marketplace/apps/KRecognition/APP-INF/jb/journeys.js
+++ src/main/marketplace/apps/KRecognition/APP-INF/jb/journeys.js
@@ -3,9 +3,9 @@
 (function (g) {
 
     controllerMappings
-            .addActionNodeType('krecognition-awardBadgeActionNode', 'KRecognition/jb/awardBadgeActionNode.js', '_onAwardBadgeEnter')
+            .addActionNodeType(g.AWARD_BADGE_ACTION_NAME, 'KRecognition/jb/awardBadgeActionNode.js', '_onAwardBadgeEnter')
             .addGoalNodeType(g.BADGE_AWARDED_GOAL_NAME, 'KRecognition/jb/badgeAwardedGoalNode.js', '_onBadgeAwardedGoalEnter')
-            .addGoalNodeType('krecognition-levelAchievedGoal', 'KRecognition/jb/levelAchievedGoal.js', '_onLevelAchievedGoalEnter');
+            .addGoalNodeType(g.LEVEL_ACHIEVED_GOAL_NAME, 'KRecognition/jb/levelAchievedGoal.js', '_onLevelAchievedGoalEnter');
 
     /**
      * 
@@ -42,27 +42,46 @@
      * 
      * @param {type} rf
      * @param {type} lead
-     * @param {type} funnel
-     * @param {type} exitingNode
+     * @param {type} f
+     * @param {type} eventParams
+     * @param {type} customNextNodes
      * @param {type} customSettings
      * @param {type} event
-     * @returns {undefined}
+     * @returns {undefined|Boolean}
      */
-    g._onBadgeAwardedGoalEnter = function (rf, lead, funnel, exitingNode, customSettings, event) {
+    g._onBadgeAwardedGoalEnter = function (rf, lead, f, eventParams, customNextNodes, customSettings, event) {
+        var topicId = Utils.safeInt(customSettings.topic);
+        var badgeId = Utils.safeInt(customSettings.badge);
 
+        var topic = applications.userApp.recognitionService.getTopic(topicId);
+        var badge = null;
+        if (Utils.isNotNull(topic)) {
+            badge = topic.getBadge(badgeId);
+        }
+
+        if (Utils.isNotNull(badge)) {
+            // We have a configured badge so make sure it matches
+            return Utils.safeInt(eventParams.badgeid) === badgeId;
+        } else {
+            // No badge has been configured so just return true
+            return true;
+        }
+
+        return false;
     };
 
     /**
      * 
      * @param {type} rf
      * @param {type} lead
-     * @param {type} funnel
-     * @param {type} exitingNode
+     * @param {type} f
+     * @param {type} eventParams
+     * @param {type} customNextNodes
      * @param {type} customSettings
      * @param {type} event
      * @returns {undefined}
      */
-    g._onLevelAchievedGoalEnter = function (rf, lead, funnel, exitingNode, customSettings, event) {
+    g._onLevelAchievedGoalEnter = function (rf, lead, f, eventParams, customNextNodes, customSettings, event) {
 
     };
 })(this);
