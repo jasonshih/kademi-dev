a0349a1aa3c49d83507138948a95195336a1447e
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-08-31 12:29:26 +0700

    Remove components which are declared in Java out of salesdata-lib

--- src/main/marketplace/libs/salesdata-lib/APP-INF/app.js
+++ src/main/marketplace/libs/salesdata-lib/APP-INF/app.js
@@ -1,9 +1,5 @@
 controllerMappings.addComponent("salesdata/components", "topSkus", "html", "Shows top SKUs of data series in table format", "Sales data");
 controllerMappings.addComponent("salesdata/components", "panelKpiTarget", "html", "Shows total amounts of last and period, KPI Target and an indicator for on track or not", "Sales data");
-controllerMappings.addComponent("salesdata", "kpiVis", "html", "Displays a KPI visualisation, eg dateHistogram, sparkline, kpiLeaderboard, pointsLeaderboard or a summary", "Sales data");
-controllerMappings.addComponent("salesdata", "salesDataVis", "html", "Displays sales data in a visualisation, eg dateHistogram, sparkline, kpiLeaderboard, pointsLeaderboard or a summary", "Sales data");
-controllerMappings.addComponent("salesdata", "onTrack", "html", "An indicator to show if the user is on track to achieve the KPI target", "Sales data");
-controllerMappings.addComponent("salesdata", "kpiPeriodPicker", "html", "Allows select a KPI period from a dropdown list", "Sales data");
 controllerMappings.addComponent("salesdata/components", "salesTable", "html", "Displays sales data in a table", "Sales data");
 
 controllerMappings
