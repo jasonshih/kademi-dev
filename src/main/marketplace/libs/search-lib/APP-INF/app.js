04904b54a74bea9dd5f1e89562b20151365c6a4e
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-09-19 14:07:43 +0700

    https://github.com/Kademi/kademi-dev/issues/3903

--- src/main/marketplace/libs/search-lib/APP-INF/app.js
+++
@@ -1,3 +0,0 @@
-controllerMappings.addComponent("search/components", "searchBox", "html", "Display search box", "Search App component");
-controllerMappings.addComponent("search/components", "searchResult", "html", "Display search result", "Search App component");
-controllerMappings.addComponent("search/components", "searchResultBucket", "html", "Display search result bucket", "Search App component");
