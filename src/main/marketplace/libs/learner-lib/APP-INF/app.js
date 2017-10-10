9d6b8c26d3ce2d8b0203d321fc197f9178c9b98c
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-10-10 17:58:46 +0700

    https://github.com/Kademi/kademi-dev/issues/4128

--- src/main/marketplace/libs/learner-lib/APP-INF/app.js
+++ src/main/marketplace/libs/learner-lib/APP-INF/app.js
@@ -1,2 +1,4 @@
 controllerMappings.addComponent("learner/components", "modulesTile", "html", "Displays available modules for current user in tile format", "Learning App;Tile component");
 controllerMappings.addComponent("learner/components", "modulesList", "html", "Displays modules in list or grid format", "Learning App");
+controllerMappings.addComponent("learner/components", "coursePicker", "html", "Displays course picker", "Learning App");
+controllerMappings.addComponent("learner/components", "courseDescription", "html", "Displays course description", "Learning App");
