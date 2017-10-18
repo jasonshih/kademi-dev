9f27137baa7646b16340d9425a13aa7f207e7d8a
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-10-18 23:30:05 +0700

    https://github.com/Kademi/kademi-dev/issues/4072 - Coming Up Auction

---
+++ src/main/marketplace/libs/auctions-lib/APP-INF/app.js
@@ -0,0 +1,6 @@
+controllerMappings.addComponent("extraComponents", "auctionsList", "html", "Shows all auctions include coming-up, in-bidding and ended auctions", "Auction App");
+controllerMappings.addComponent("extraComponents", "auctionTitle", "html", "Shows title of current auction", "Auction App");
+controllerMappings.addComponent("extraComponents", "auctionImage", "html", "Shows image of current auction", "Auction App");
+controllerMappings.addComponent("extraComponents", "auctionDescription", "html", "Shows description of current auction", "Auction App");
+controllerMappings.addComponent("extraComponents", "auctionBidForm", "html", "Shows bidding form and related information of current auction", "Auction App");
+controllerMappings.addComponent("extraComponents", "auctionBidHistory", "html", "Shows bid history of current auction", "Auction App");
--- src/main/marketplace/libs/auctions-lib/common/theme/apps/auctions/components/comingUpAuctionComponent.js
+++ src/main/marketplace/libs/auctions-lib/common/theme/apps/auctions/components/comingUpAuctionComponent.js
@@ -1,9 +1,36 @@
 (function ($) {
     var KEditor = $.keditor;
     var flog = KEditor.log;
-
+    
     KEditor.components['comingUpAuction'] = {
-        settingEnabled: false
+        settingEnabled: true,
+        settingTitle: 'Coming Up Auction',
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "comingUpAuction" component');
+            
+            return $.ajax({
+                url: '_components/comingUpAuction?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                    
+                    form.find('.chk-show-description').on('click', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        
+                        component.attr('data-show-description', this.checked);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+                }
+            });
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "comingUpAuction" component');
+            
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            form.find('.chk-show-description').prop('checked', dataAttributes['data-show-description'] === 'true');
+        }
     };
-
+    
 })(jQuery);
