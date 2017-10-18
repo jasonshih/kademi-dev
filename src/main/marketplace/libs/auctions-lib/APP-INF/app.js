86018a1fd1e9ec9e3161c3328f665ffc58207378
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-10-19 00:52:30 +0700

    https://github.com/Kademi/kademi-dev/issues/4072 - View Auction components

--- src/main/marketplace/libs/auctions-lib/APP-INF/app.js
+++ src/main/marketplace/libs/auctions-lib/APP-INF/app.js
@@ -1,6 +1,6 @@
-controllerMappings.addComponent("extraComponents", "auctionsList", "html", "Shows all auctions include coming-up, in-bidding and ended auctions", "Auction App");
-controllerMappings.addComponent("extraComponents", "auctionTitle", "html", "Shows title of current auction", "Auction App");
-controllerMappings.addComponent("extraComponents", "auctionImage", "html", "Shows image of current auction", "Auction App");
-controllerMappings.addComponent("extraComponents", "auctionDescription", "html", "Shows description of current auction", "Auction App");
-controllerMappings.addComponent("extraComponents", "auctionBidForm", "html", "Shows bidding form and related information of current auction", "Auction App");
-controllerMappings.addComponent("extraComponents", "auctionBidHistory", "html", "Shows bid history of current auction", "Auction App");
+controllerMappings.addComponent("auctions/components", "auctionsList", "html", "Shows all auctions include coming-up, in-bidding and ended auctions", "Auction App");
+controllerMappings.addComponent("auctions/components", "auctionTitle", "html", "Shows title of current auction", "Auction App");
+controllerMappings.addComponent("auctions/components", "auctionImage", "html", "Shows image of current auction", "Auction App");
+controllerMappings.addComponent("auctions/components", "auctionDescription", "html", "Shows description of current auction", "Auction App");
+controllerMappings.addComponent("auctions/components", "auctionBidForm", "html", "Shows bidding form and related information of current auction", "Auction App");
+controllerMappings.addComponent("auctions/components", "auctionBidHistory", "html", "Shows bid history of current auction", "Auction App");
--- src/main/marketplace/libs/auctions-lib/common/theme/apps/auctions/components/auctionsListComponent.js
+++ src/main/marketplace/libs/auctions-lib/common/theme/apps/auctions/components/auctionsListComponent.js
@@ -1,3 +1,36 @@
-/**
- * Created by Anh on 7/21/2016.
- */
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+    
+    KEditor.components['auctionsList'] = {
+        settingEnabled: true,
+        settingTitle: 'Auctions List',
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "auctionsList" component');
+            
+            return $.ajax({
+                url: '_components/auctionsList?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                    
+                    form.find('.chk-show-description').on('click', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        
+                        component.attr('data-show-description', this.checked);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+                }
+            });
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "auctionsList" component');
+            
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            form.find('.chk-show-description').prop('checked', dataAttributes['data-show-description'] === 'true');
+        }
+    };
+    
+})(jQuery);
