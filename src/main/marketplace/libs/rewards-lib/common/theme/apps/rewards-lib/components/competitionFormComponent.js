f33869c7eb4289ecc106f37346fd93007e84442b
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-10-31 18:04:58 +0700

    https://github.com/Kademi/kademi-dev/issues/3735

--- src/main/marketplace/libs/rewards-lib/common/theme/apps/rewards-lib/components/competitionFormComponent.js
+++ src/main/marketplace/libs/rewards-lib/common/theme/apps/rewards-lib/components/competitionFormComponent.js
@@ -1,50 +1,50 @@
 (function ($) {
     var KEditor = $.keditor;
     var flog = KEditor.log;
-
+    
     KEditor.components['competitionForm'] = {
         settingEnabled: true,
-
+        
         settingTitle: 'Competition Form Settings',
-
+        
         initSettingForm: function (form, keditor) {
             flog('initSettingForm "competitionForm" component');
-
+            
             return $.ajax({
                 url: '_components/competitionForm?settings',
                 type: 'get',
                 dataType: 'html',
                 success: function (resp) {
                     form.html(resp);
-    
+                    
                     form.find('.select-promotion').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-        
+                        
                         component.attr('data-promotion', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
-    
+                    
                     form.find('.select-photos-per-row').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-        
+                        
                         component.attr('data-photos-per-row', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
-    
+                    
                     form.find('.chk-show-user-photo').on('click', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-        
+                        
                         component.attr('data-show-user-photo', this.checked);
                         keditor.initDynamicContent(dynamicElement);
                     });
-    
+                    
                     form.find('.chkShowFirstName').on('click', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-        
+                        
                         component.attr('data-show-first-name', this.checked);
                         keditor.initDynamicContent(dynamicElement);
                     });
@@ -88,71 +88,70 @@
                         component.attr('data-show-message', this.checked);
                         keditor.initDynamicContent(dynamicElement);
                     });
-
+                    
                     form.find('#txtSubmitText').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
+                        
                         component.attr('data-submit-text', this.value || 'Send message');
                         keditor.initDynamicContent(dynamicElement);
                     });
-
+                    
                     form.find('#cbbSubmitSize').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
+                        
                         component.attr('data-submit-size', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
-
+                    
                     form.find('#cbbSubmitColor').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
+                        
                         component.attr('data-submit-color', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
-
+                    
                     form.find('#txtSubmitClass').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
+                        
                         component.attr('data-submit-class', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
-
+                    
                     form.find('#thankYou').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
+                        
                         component.attr('data-thank-you', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
-
+                    
                     form.find('#path').on('change', function () {
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
-
+                        
                         component.attr('data-path', this.value);
                         keditor.initDynamicContent(dynamicElement);
                     });
                 }
             });
         },
-
+        
         showSettingForm: function (form, component, keditor) {
             flog('showSettingForm "competitionForm" component');
-
+            
             var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            
             form.find('.select-promotion').val(dataAttributes['data-promotion'] || '');
             form.find('.select-photos-per-row').val(dataAttributes['data-photos-per-row'] || '2');
             form.find('.chk-show-user-photo').prop('checked', dataAttributes['data-show-user-photo'] !== 'false');
-            form.find('.chkShowFirstName').prop('checked', dataAttributes['data-first-name'] === 'true');
-            form.find('.chkShowSurname').prop('checked', dataAttributes['data-sur-name'] === 'true');
-            form.find('.chkShowEmail').prop('checked', dataAttributes['data-email'] === 'true');
-            form.find('.chkShowCompany').prop('checked', dataAttributes['data-company'] === 'true');
-            form.find('.chkShowPhone').prop('checked', dataAttributes['data-phone'] === 'true');
-            form.find('.chkShowMessage').prop('checked', dataAttributes['data-message'] === 'true');
+            form.find('.chkShowFirstName').prop('checked', dataAttributes['data-show-first-name'] === 'true');
+            form.find('.chkShowSurname').prop('checked', dataAttributes['data-show-sur-name'] === 'true');
+            form.find('.chkShowEmail').prop('checked', dataAttributes['data-show-email'] === 'true');
+            form.find('.chkShowPhone').prop('checked', dataAttributes['data-show-phone'] === 'true');
             form.find('#txtSubmitText').val(dataAttributes['data-submit-text']);
             form.find('#cbbSubmitSize').val(dataAttributes['data-submit-size']);
             form.find('#cbbSubmitColor').val(dataAttributes['data-submit-color']);
@@ -161,5 +160,5 @@
             form.find('#path').val(dataAttributes['data-path']);
         }
     };
-
+    
 })(jQuery);
