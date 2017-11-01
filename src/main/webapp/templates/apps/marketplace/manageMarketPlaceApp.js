586efa2b1f9c89d6ef28d252e2f1096301af4b6b
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-11-01 10:12:54 +0700

    Update

--- src/main/webapp/templates/apps/marketplace/manageMarketPlaceApp.js
+++ src/main/webapp/templates/apps/marketplace/manageMarketPlaceApp.js
@@ -8,9 +8,9 @@ function initMarketPlaceApp() {
 
 function initMasonryAdapter() {
     flog('initMasonryAdapter');
-
+    
     var screenshotSwitcher = $('a[href=#screenshots]');
-
+    
     screenshotSwitcher.on('shown.bs.tab', function () {
         if (!screenshotSwitcher.attr('data-masonry-adapted')) {
             $('.masonry-panel').masonry('layout');
@@ -52,26 +52,28 @@ function initSubmitReview() {
 function initInstallApp() {
     $('body').on('click', '.btn-install-app', function (e) {
         e.preventDefault();
+        
         var btn = $(this);
         var title = btn.data('title');
         var websiteName = btn.data('websitename');
         var websiteBranch = btn.data('websitebranch');
-        if (confirm('Are you sure you want to install ' + title + '?')) {
+        
+        Kalert.confirm('You want to install ' + title + '?', 'Install', function () {
             $.ajax({
                 type: 'POST',
                 url: window.location.pathname,
                 data: {
                     installItem: true,
-                    websiteName : websiteName,
-                    websiteBranch : websiteBranch
+                    websiteName: websiteName,
+                    websiteBranch: websiteBranch
                 },
                 dataType: 'json',
                 success: function (data) {
                     flog("success", data);
-                    if(data.status){
+                    if (data.status) {
                         Msg.success('Successfully installed ' + title);
                         window.location.reload();
-                    }else{
+                    } else {
                         Msg.warning('There was a problem installing ' + title + ". " + data.messages)
                     }
                 },
@@ -79,7 +81,7 @@ function initInstallApp() {
                     Msg.error("An error occured doing the publish. Please check your internet connection and try again");
                 }
             });
-        }
+        });
     });
 }
 
@@ -91,31 +93,31 @@ function addReview(data) {
     }
     var reviewedOn = new Date(parseInt(data.reviewedOn, 10));
     var template = $('<section class="comment-list">'
-            + '    <article class="row">'
-            + '        <div class="col-md-2 col-sm-2 hidden-xs">'
-            + '            <figure class="thumbnail">'
-            + '                <img class="img-responsive" src="' + profileImg + '" />'
-            + '            </figure>'
-            + '        </div>'
-            + '        <div class="col-md-10 col-sm-10">'
-            + '            <div class="panel panel-default arrow left">'
-            + '                <div class="panel-body">'
-            + '                    <header class="text-left">'
-            + '                        <div class="comment-user"><i class="fa fa-user"></i> ' + data.reviewedBy + '</div>'
-            + '                        <div><i class="fa fa-smile-o"></i> ' + genRating(data.rating) + '</div>'
-            + '                        <span><i class="fa fa-clock-o"></i> <abbr title=" ' + reviewedOn + '" class="timeago">' + reviewedOn.toISOString() + '</abbr></span>'
-            + '                    </header>'
-            + '                    <div class="comment-post">'
-            + '                        <p>' + data.review + '</p>'
-            + '                    </div>'
-            + '                </div>'
-            + '            </div>'
-            + '        </div>'
-            + '    </article>'
-            + '</section>');
-
+        + '    <article class="row">'
+        + '        <div class="col-md-2 col-sm-2 hidden-xs">'
+        + '            <figure class="thumbnail">'
+        + '                <img class="img-responsive" src="' + profileImg + '" />'
+        + '            </figure>'
+        + '        </div>'
+        + '        <div class="col-md-10 col-sm-10">'
+        + '            <div class="panel panel-default arrow left">'
+        + '                <div class="panel-body">'
+        + '                    <header class="text-left">'
+        + '                        <div class="comment-user"><i class="fa fa-user"></i> ' + data.reviewedBy + '</div>'
+        + '                        <div><i class="fa fa-smile-o"></i> ' + genRating(data.rating) + '</div>'
+        + '                        <span><i class="fa fa-clock-o"></i> <abbr title=" ' + reviewedOn + '" class="timeago">' + reviewedOn.toISOString() + '</abbr></span>'
+        + '                    </header>'
+        + '                    <div class="comment-post">'
+        + '                        <p>' + data.review + '</p>'
+        + '                    </div>'
+        + '                </div>'
+        + '            </div>'
+        + '        </div>'
+        + '    </article>'
+        + '</section>');
+    
     template.find('.timeago').timeago();
-
+    
     reviewsList.append(template);
 }
 
@@ -132,20 +134,20 @@ function genRating(rating) {
         }
     }
     r = r + '</span>';
-
+    
     return r;
 }
 
 (function (e) {
     var t, o = {
-        className: "autosizejs",
-        append: "",
-        callback: !1,
-        resizeDelay: 10
-    },
-    i = '<textarea tabindex="-1" rows="5" cols="100" style="position:absolute; top:-999px; left:0; right:auto; bottom:auto; border:0; padding: 0; -moz-box-sizing:content-box; -webkit-box-sizing:content-box; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden; transition:none; -webkit-transition:none; -moz-transition:none;"/>',
-            n = ["fontFamily", "fontSize", "fontWeight", "fontStyle", "letterSpacing", "textTransform", "wordSpacing", "textIndent"],
-            s = e(i).data("autosize", !0)[0];
+            className: "autosizejs",
+            append: "",
+            callback: !1,
+            resizeDelay: 10
+        },
+        i = '<textarea tabindex="-1" rows="5" cols="100" style="position:absolute; top:-999px; left:0; right:auto; bottom:auto; border:0; padding: 0; -moz-box-sizing:content-box; -webkit-box-sizing:content-box; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden; transition:none; -webkit-transition:none; -moz-transition:none;"/>',
+        n = ["fontFamily", "fontSize", "fontWeight", "fontStyle", "letterSpacing", "textTransform", "wordSpacing", "textIndent"],
+        s = e(i).data("autosize", !0)[0];
     s.style.lineHeight = "99px", "99px" === e(s).css("lineHeight") && n.push("lineHeight"), s.style.lineHeight = "", e.fn.autosize = function (i) {
         return this.length ? (i = e.extend({}, o, i || {}), s.parentNode !== document.body && e(document.body).append(s), this.each(function () {
             function o() {
@@ -154,40 +156,41 @@ function genRating(rating) {
                     o -= parseInt(t[i], 10)
                 }), s.style.width = o + "px") : s.style.width = Math.max(p.width(), 0) + "px"
             }
-
+            
             function a() {
                 var a = {};
                 if (t = u, s.className = i.className, d = parseInt(p.css("maxHeight"), 10), e.each(n, function (e, t) {
-                    a[t] = p.css(t)
-                }), e(s).css(a), o(), window.chrome) {
+                        a[t] = p.css(t)
+                    }), e(s).css(a), o(), window.chrome) {
                     var r = u.style.width;
                     u.style.width = "0px", u.offsetWidth, u.style.width = r
                 }
             }
-
+            
             function r() {
                 var e, n;
                 t !== u ? a() : o(), s.value = u.value + i.append, s.style.overflowY = u.style.overflowY, n = parseInt(u.style.height, 10), s.scrollTop = 0, s.scrollTop = 9e4, e = s.scrollTop, d && e > d ? (u.style.overflowY = "scroll", e = d) : (u.style.overflowY = "hidden", c > e && (e = c)), e += w, n !== e && (u.style.height = e + "px", f && i.callback.call(u, u))
             }
-
+            
             function l() {
                 clearTimeout(h), h = setTimeout(function () {
                     var e = p.width();
                     e !== g && (g = e, r())
                 }, parseInt(i.resizeDelay, 10))
             }
+            
             var d, c, h, u = this,
-                    p = e(u),
-                    w = 0,
-                    f = e.isFunction(i.callback),
-                    z = {
-                        height: u.style.height,
-                        overflow: u.style.overflow,
-                        overflowY: u.style.overflowY,
-                        wordWrap: u.style.wordWrap,
-                        resize: u.style.resize
-                    },
-            g = p.width();
+                p = e(u),
+                w = 0,
+                f = e.isFunction(i.callback),
+                z = {
+                    height: u.style.height,
+                    overflow: u.style.overflow,
+                    overflowY: u.style.overflowY,
+                    wordWrap: u.style.wordWrap,
+                    resize: u.style.resize
+                },
+                g = p.width();
             p.data("autosize") || (p.data("autosize", !0), ("border-box" === p.css("box-sizing") || "border-box" === p.css("-moz-box-sizing") || "border-box" === p.css("-webkit-box-sizing")) && (w = p.outerHeight() - p.height()), c = Math.max(parseInt(p.css("minHeight"), 10) - w || 0, p.height()), p.css({
                 overflow: "hidden",
                 overflowY: "hidden",
@@ -231,6 +234,7 @@ var __slice = [].slice;
             });
             this.$el.on("starrr:change", this.options.change)
         }
+        
         t.prototype.defaults = {
             rating: void 0,
             numStars: 5,
@@ -294,7 +298,7 @@ $(function () {
 })
 
 $(function () {
-
+    
     $('#new-review').autosize({
         append: "\n"
     });
