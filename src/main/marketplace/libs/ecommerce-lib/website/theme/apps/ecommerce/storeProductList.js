a19db75b089d65890a802cd912842cb11cda4d93
anh <anhnt38@gmail.com>
2017-11-03 17:06:26 +0700

    https://github.com/Kademi/kademi-dev/issues/4272

--- src/main/marketplace/libs/ecommerce-lib/website/theme/apps/ecommerce/storeProductList.js
+++ src/main/marketplace/libs/ecommerce-lib/website/theme/apps/ecommerce/storeProductList.js
@@ -1,9 +1,8 @@
 (function ($) {
     $(document).ready(function(){
 
-        if($('.ecom-store-category-products').length > 0) {
+        if($('.ecom-store-category-products.grid-layout').length > 0) {
 
-            if($('.ecom-store-category-products.grid-layout')) {
                 var win = $(window);
                 var timer = null;
 
@@ -11,7 +10,9 @@
                     clearTimeout(timer);
                     timer = setTimeout(function () {
                         var brieves = $('.products-list').find('.product-brief');
+                        var productTitles = $('.products-list').find('.product-title');
                         brieves.css('height', '');
+                        productTitles.css('height', '');
 
                         if (win.width() > 767) {
                             var heights = [];
@@ -20,10 +21,16 @@
                             });
 
                             brieves.css('height', Math.max.apply(Math, heights));
+
+                            var heightTitles = [];
+                            productTitles.each(function () {
+                                heightTitles.push($(this).innerHeight());
+                            });
+
+                            productTitles.css('height', Math.max.apply(Math, heightTitles));
                         }
                     }, 150);
                 }).trigger('resize');
-            }
         }
     });
 
