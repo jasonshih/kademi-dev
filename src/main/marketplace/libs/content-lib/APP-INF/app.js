df956f3dbc5c8be98b62d291944bb79f9a3b86e1
anh <anhnt38@gmail.com>
2017-11-08 12:52:28 +0700

    https://github.com/Kademi/kademi-dev/issues/4234

--- src/main/marketplace/libs/content-lib/APP-INF/app.js
+++ src/main/marketplace/libs/content-lib/APP-INF/app.js
@@ -3,3 +3,4 @@ controllerMappings.addComponent("content/components", "errorDetails", "html", "D
 controllerMappings.addComponent("content/components", "htmlPanel", "html", "Displays panel with text and icon", "Content App component");
 controllerMappings.addComponent("content/components", "showHideButton", "html", "A button which causes all following content to be initially hidden, and is only shown when clicked", "Content App component");
 controllerMappings.addComponent("content/components", "rating", "html", "Shows a rating stars which is predefined by admin", "Content App component");
+controllerMappings.addComponent("content/components", "tile", "html", "Shows a tile which contains image, text and link", "Content App component");
