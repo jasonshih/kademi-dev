6d1b22dd0c3f6a542bacdf26fa60d4453c8d3563
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-11-09 12:25:55 +0700

    https://github.com/Kademi/kademi-dev/issues/4340#event-1330818173

--- src/main/marketplace/libs/rewards-lib/website/theme/apps/rewards-lib/competitionPage.js
+++ src/main/marketplace/libs/rewards-lib/website/theme/apps/rewards-lib/competitionPage.js
@@ -1,121 +1,141 @@
-$(function () {
-    $('form.entryForm').forms({
-        onSuccess: function (resp, form) {
-            flog('onSuccess', resp, form);
-            var f = $(form);
-            flog('hide', f);
-            f.hide(1000);
-            f.closest('.competitionForm').find('.thankyou').show(1000);
+(function ($) {
+    $(function () {
+        var components = $('[data-type="component-competitionForm"]');
+        if (components.length > 0) {
+            components.each(function () {
+                var component = $(this);
+                
+                initForm(component);
+                initOrgSearchPromo(component);
+                initSkuSearchPromo(component);
+            });
         }
     });
-
-    var myPromotionUpload = $('#myPromotionUpload');
-    var uploadUrl = myPromotionUpload.attr('data-url');
-    $('#myPromotionUpload').mupload({
-        url: uploadUrl,
-        buttonText: 'Upload a photo',
-        oncomplete: function (data, name, href) {
-            // Using the hash, if not the name
-            if (href.indexOf("uploads/") > 0) {
-                var hash = href.split("uploads/")[1]
-                $('form input[name=userAttachmentHash]').val(hash);
-            } else {
-                $('form input[name=userAttachmentHash]').val(name);
+    
+    function initForm(component) {
+        var form = component.find('form.entryForm');
+        var thanks = component.find('.thankyou');
+        
+        component.find('form.entryForm').forms({
+            onSuccess: function () {
+                form.fhide();
+                thanks.fshow();
             }
-            $('div.viewUploaded').css('background-image', 'url("' + href + '/alt-150-150.png' + '")');
-        }
-    });
-
-    var rewardQuizes = $('.viewQuiz');
-    rewardQuizes.each(function (i, n) {
-        var quiz = $(n);
-        var json = quiz.text();
-        quiz.text('');
-        quiz.formRender({
-            dataType: 'json',
-            formData: json,
-            labelClasses: 'control-label',
-            inputClasses: 'form-control'
         });
-        quiz.show();
-    });
-
-    initOrgSearchPromo();
-    initSkuSearchPromo();
-
-    $('.go-again').click(function (e) {
-        e.preventDefault();
-        var f = $(e.target);
-        var div = f.closest('.competitionForm');
-        div.find('.reset-on-resubmit').val('');
-        div.find('.thankyou').hide(1000);
-        div.find('.entryForm').show(1000);
-    });
-});
-
-function initOrgSearchPromo() {
-    var orgSearch = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        remote: {
-            url: window.location.pathname + '?searchOrgs=%QUERY',
-            wildcard: '%QUERY'
+        
+        var myPromotionUpload = component.find('.promotion-photo-uploader');
+        var viewUploaded = form.find('div.viewUploaded');
+        var uploadUrl = myPromotionUpload.attr('data-url');
+        if (uploadUrl) {
+            myPromotionUpload.mupload({
+                url: uploadUrl,
+                buttonText: 'Upload a photo',
+                oncomplete: function (data, name, href) {
+                    // Using the hash, if not the name
+                    if (href.indexOf("uploads/") > 0) {
+                        var hash = href.split("uploads/")[1]
+                        form.find('input[name=userAttachmentHash]').val(hash);
+                    } else {
+                        form.find('input[name=userAttachmentHash]').val(name);
+                    }
+                    
+                    viewUploaded.css('background-image', 'url("' + href + '/alt-640-640.png' + '")');
+                    viewUploaded.attr('data-url', href);
+                }
+            });
         }
-    });
-
-    $('.relatedOrgTitlePromo').typeahead({
-        highlight: true
-    }, {
-        display: 'title',
-        limit: 10,
-        source: orgSearch,
-        templates: {
-            empty: '<div class="empty-message">No existing stores were found.</div>',
-            suggestion: Handlebars.compile('<div><strong>{{title}}</strong></div>')
-        }
-    });
-
-    $('.relatedOrgTitlePromo').bind('typeahead:select', function (ev, sug) {
-        var inp = $(this);
-        var form = inp.closest('form');
-
-        form.find('input[name=relatedOrg]').val(sug.orgId);
-    });
-}
-
-function initSkuSearchPromo() {
-    var orgSearch = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        remote: {
-            url: window.location.pathname + '?searchProducts=%QUERY',
-            wildcard: '%QUERY',
-            filter: function (data) {
-                var newData = data.map(function (item) {
-                    item.tokens.splice(2, 1);
-                    return {value: item.value, skuTitle: item.tokens.join(' - ')}
-                })
-                return newData;
+        
+        var rewardQuizes = component.find('.viewQuiz');
+        rewardQuizes.each(function (i, n) {
+            var quiz = $(n);
+            var json = quiz.text();
+            quiz.text('');
+            quiz.formRender({
+                dataType: 'json',
+                formData: json,
+                labelClasses: 'control-label',
+                inputClasses: 'form-control'
+            });
+            quiz.show();
+        });
+        
+        component.find('.go-again').click(function (e) {
+            e.preventDefault();
+            
+            viewUploaded.parent().after(
+                '<div class="' + viewUploaded.attr('data-class') + ' promotion-photo promotion-photo-user">' +
+                '    <a href="' + viewUploaded.attr('data-url') + '" class=" promotion-photo-inner" style="background-image: url(\"' + viewUploaded.attr('data-url') + '/alt-640-640.png\")">' +
+                '        <span class="promotion-photo-label">My photo</span>' +
+                '    </a>' +
+                '</div>'
+            )
+            viewUploaded.css('background-image', 'url("/static/images/photo_holder_squared.png")');
+            viewUploaded.attr('data-url', '');
+            
+            form.fshow();
+            thanks.fhide();
+            form.find('.reset-on-resubmit').val('');
+        });
+    }
+    
+    function initOrgSearchPromo(component) {
+        var orgSearch = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: window.location.pathname + '?searchOrgs=%QUERY',
+                wildcard: '%QUERY'
             }
-        }
-    });
-
-    $('.relatedProductSkuTitlePromo').typeahead({
-        highlight: true
-    }, {
-        display: 'skuTitle',
-        limit: 10,
-        source: orgSearch,
-        templates: {
-            empty: '<div class="empty-message">No existing SKUs were found.</div>',
-            suggestion: Handlebars.compile('<div><span>{{skuTitle}}</span></div>')
-        }
-    });
-
-    $('.relatedProductSkuTitlePromo').bind('typeahead:select', function (ev, sug) {
-        var inp = $(this);
-        var form = inp.closest('form');
-
-        form.find('input[name=relatedProductSku]').val(sug.value);
-    });
-}
+        });
+        
+        component.find('.relatedOrgTitlePromo').typeahead({
+            highlight: true
+        }, {
+            display: 'title',
+            limit: 10,
+            source: orgSearch,
+            templates: {
+                empty: '<div class="empty-message">No existing stores were found.</div>',
+                suggestion: Handlebars.compile('<div><strong>{{title}}</strong></div>')
+            }
+        });
+        
+        component.find('.relatedOrgTitlePromo').bind('typeahead:select', function (ev, sug) {
+            component.find('input[name=relatedOrg]').val(sug.orgId);
+        });
+    }
+    
+    function initSkuSearchPromo(component) {
+        var orgSearch = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: window.location.pathname + '?searchProducts=%QUERY',
+                wildcard: '%QUERY',
+                filter: function (data) {
+                    var newData = data.map(function (item) {
+                        item.tokens.splice(2, 1);
+                        return {value: item.value, skuTitle: item.tokens.join(' - ')}
+                    })
+                    return newData;
+                }
+            }
+        });
+        
+        component.find('.relatedProductSkuTitlePromo').typeahead({
+            highlight: true
+        }, {
+            display: 'skuTitle',
+            limit: 10,
+            source: orgSearch,
+            templates: {
+                empty: '<div class="empty-message">No existing SKUs were found.</div>',
+                suggestion: Handlebars.compile('<div><span>{{skuTitle}}</span></div>')
+            }
+        });
+        
+        component.find('.relatedProductSkuTitlePromo').bind('typeahead:select', function (ev, sug) {
+            component.find('input[name=relatedProductSku]').val(sug.value);
+        });
+    }
+})(jQuery);
