7ff09eed495c764826a9dc49ce1c19126820578d
Gonzalo Naveira <gonzaliito@gmail.com>
2017-11-13 09:40:31 +1300

    https://github.com/Kademi/kademi-dev/issues/4351

--- src/main/webapp/templates/apps/admin/manageTableUploader.js
+++ src/main/webapp/templates/apps/admin/manageTableUploader.js
@@ -1,6 +1,7 @@
 var importUrl = window.location.pathname;
 var importWizardStarted = false;
 var importTotalCount = 0;
+var importTotalDeleteCount = 0;
 var hasCustomForm = false;
 
 var optionsRow = null;
@@ -10,9 +11,9 @@ var reviewRow = 3;
 function initManageTableUploader(hcf) {
     initUploads();
     initSaveMappings();
-    
+
     hasCustomForm = hcf;
-    
+
     if (hasCustomForm) {
         optionsRow = 2;
         mapColumnsRow++;
@@ -23,19 +24,19 @@ function initManageTableUploader(hcf) {
 function initSaveMappings() {
     var modal = $('#modal-tableUploader-saveMapping');
     var form = modal.find('form');
-    
+
     var importerHead = $('#importerHead');
-    
+
     form.forms({
         onValid: function (form, config) {
             var selectedCols = [];
-            
+
             importerHead.find('select').each(function () {
                 if (this.value) {
                     selectedCols.push(this.value);
                 }
             });
-            
+
             if (!selectedCols.length) {
                 Msg.error('Please select at least 1 destination field to continue.');
                 importerHead.find('select').first().trigger('focus');
@@ -44,12 +45,12 @@ function initSaveMappings() {
         },
         beforePostForm: function (form, config, data) {
             var fields = $("#importerWizard .customForm :input").serializeArray();
-            
+
             for (var i in fields) {
                 var f = fields[i];
                 f.name = 'options-' + f.name;
             }
-            
+
             return data + '&' + importerHead.find(':input').serialize() + '&' + $.param(fields);
         },
         onSuccess: function (resp) {
@@ -57,7 +58,7 @@ function initSaveMappings() {
             Msg.success(resp.messages);
             $('#savedMappings').reloadFragment();
         }
-        
+
     });
 }
 
@@ -65,7 +66,7 @@ function initUploads() {
     var importer = $("#importerWizard");
     var form = $("#importerWizard form");
     var wizard = $('#myWizard');
-    
+
     wizard.wizard();
     importer.on('show.bs.collapse', function () {
         var curStep = wizard.wizard('selectedItem');
@@ -84,7 +85,7 @@ function initUploads() {
         var resultStatus = $('#job-status');
         resultStatus.text('');
     });
-    
+
     wizard.on('changed.fu.wizard', function (evt, data) {
         if (data.step === 1) {
             // IE 11 fix
@@ -94,7 +95,7 @@ function initUploads() {
             }
         }
     });
-    
+
     wizard.on('actionclicked.fu.wizard', function (evt, data) {
         if (data.step === 1) {
             if (form.find("input[name=fileHash]").val() == "") {
@@ -104,20 +105,20 @@ function initUploads() {
                 evt.preventDefault();
             }
         }
-        
+
         if (hasCustomForm && data.step === optionsRow && data.direction === 'next') {
             var fileHash = form.find('[name=fileHash]').val();
             var formData = {fetchTableHeaders: 'fetchTableHeaders', fileHash: fileHash};
-            
+
             var fields = $("#importerWizard .customForm :input").serializeArray();
-            
+
             for (var i in fields) {
                 var f = fields[i];
                 formData[f.name] = f.value;
             }
-            
+
             flog('Data', formData);
-            
+
             $.ajax({
                 url: importUrl,
                 data: formData,
@@ -126,20 +127,20 @@ function initUploads() {
                 success: function (resp, textStatus, jqXHR) {
                     if (resp.status && resp.data) {
                         populateImportTable($('#importerWizard'), resp.data);
-                        
+
                         updateSelectedConfig(false);
-                        
+
                         wizard.wizard('selectedItem', {step: mapColumnsRow});
                     } else {
                         Msg.error(resp.messages);
                     }
                 }
             });
-            
+
             evt.preventDefault();
             return false;
         }
-        
+
         if (data.step === mapColumnsRow && data.direction === 'next') {
             var startRow = $('#startRow').val();
             if (!startRow) {
@@ -150,23 +151,23 @@ function initUploads() {
             } else {
                 $('#startRow').parents('.form-group').removeClass('has-error');
             }
-            
+
             var importerHead = $('#importerHead');
             var selectedCols = [];
-            
+
             importerHead.find('select').each(function () {
                 if (this.value) {
                     selectedCols.push(this.value);
                 }
             });
-            
+
             if (!selectedCols.length) {
                 Msg.error('Please select at least 1 destination field to continue.');
                 importerHead.find('select').first().trigger('focus');
                 evt.preventDefault();
                 return false;
             }
-            
+
             var fileHash = form.find('[name=fileHash]').val();
             var startRow = form.find('[name=startRow]').val();
             var formData = {beforeImport: 'beforeImport', fileHash: fileHash, startRow: startRow};
@@ -177,10 +178,10 @@ function initUploads() {
             });
             form.find('#noValidRow').addClass('hide');
             form.find('[type=submit]').addClass('hide');
-            
+
             $('#processing').show();
             $('#result').hide();
-            
+
             $.ajax({
                 url: importUrl,
                 data: formData,
@@ -188,18 +189,19 @@ function initUploads() {
                 dataType: 'json',
                 success: function (resp) {
                     flog(resp);
-                    
+
                     if (resp.status && resp.data) {
                         wizard.wizard('selectedItem', {step: reviewRow});
                         form.find('[type=submit]').removeClass('hide');
                         form.find(".beforeImportNumNew").text(resp.data.newImportsCount);
                         form.find(".beforeImportNumExisting").text(resp.data.existingImportsCount);
+                        form.find(".beforeImportNumToDelete").text(resp.data.existingToDeleteCount);
                         var invalidRows = resp.data.invalidRows.length;
                         if (resp.data.invalidRows.length != 0) {
                             invalidRows--;
                         }
                         form.find(".beforeImportNumInvalid").text(invalidRows);
-                        
+
                         var invalidRowsBody = form.find(".beforeImportInvalidRows");
                         invalidRowsBody.html("");
                         if (resp.data.invalidRows) {
@@ -218,14 +220,15 @@ function initUploads() {
                                 invalidRowsBody.append(tr);
                             }
                         }
-                        
+
                         importTotalCount = resp.data.newImportsCount + resp.data.existingImportsCount;
-                        
+                        importTotalDeleteCount = resp.data.existingToDeleteCount;
+
                         $('#result').show();
                         $('#processing').hide();
-                        if (importTotalCount == 0 || resp.data.toManyErrors) {
+                        if ((importTotalCount == 0 && importTotalDeleteCount == 0) || resp.data.toManyErrors) {
                             form.find('[type=submit]').attr('disabled', true);
-                            
+
                             if (resp.data.toManyErrors) {
                                 $('#toManyErrors').show();
                             } else {
@@ -252,11 +255,11 @@ function initUploads() {
                     form.find(".beforeImportInfo").text('Cannot verify data to import');
                 }
             });
-            
+
             evt.preventDefault();
             return false;
         }
-        
+
         if (data.step === reviewRow && data.direction === 'next') {
             if (!importWizardStarted) {
                 Msg.error("Importing process hasn't been started yet");
@@ -265,7 +268,7 @@ function initUploads() {
             }
         }
     });
-    
+
     importer.on('show.bs.collapse', function () {
         var curStep = wizard.wizard('selectedItem');
         if (!form.find('input[name=fileHash]').val()) {
@@ -273,19 +276,19 @@ function initUploads() {
         }
         wizard.wizard('selectedItem', curStep);
     });
-    
+
     flog("Init importer form", form);
     form.forms({
         postUrl: importUrl,
         validate: function () {
             if (importWizardStarted) {
                 wizard.wizard("next");
-                
+
                 var resultCustomValidate = {
                     error: 1,
                     errorMessages: [" That task is already in progress. Please cancel it or wait until it finishes"]
                 };
-                
+
                 return resultCustomValidate;
             }
         },
@@ -303,7 +306,7 @@ function initUploads() {
             checkProcessStatus();
         }
     });
-    
+
     $('#btn-upload').mupload({
         url: importUrl,
         useJsonPut: false,
@@ -311,15 +314,15 @@ function initUploads() {
         acceptedFiles: '.csv,.xlsx,.xls,.txt',
         oncomplete: function (resp, name, href) {
             flog("oncomplete", resp, name, href);
-            
+
             populateImportTable(form, resp.result.data);
-            
+
             updateSelectedConfig(true);
-            
+
             wizard.wizard("next");
         }
     });
-    
+
     $('#btn-cancel-import').on('click', function (e) {
         e.preventDefault();
         Kalert.confirm('Are you sure you want to cancel this process?', function () {
@@ -336,7 +339,7 @@ function initUploads() {
             });
         });
     });
-    
+
     checkProcessStatus();
 }
 
@@ -356,15 +359,15 @@ function populateImportTable(form, data) {
         thead.append(td);
         var select = $("<select class='form-control' name='col" + col + "'>");
         select.append("<option value=''>[Do not import]</option>");
-        
+
         for (var field in fields) {
             select.append("<option value='" + field + "'>" + fields[field] + "</option>");
         }
-        
+
         td.append(select);
     }
     flog("done head", thead);
-    
+
     var tbody = $("#importerBody");
     tbody.html("");
     var numRows = 0;
@@ -386,13 +389,13 @@ function populateImportTable(form, data) {
 
 function updateSelectedConfig(loadOptions) {
     var importerHead = $('#importerHead');
-    
+
     var opt = $('#savedMappings');
     var val = opt.val();
-    
+
     if (val !== '[NONE]') {
         var json = JSON.parse(val);
-        
+
         var mappings = json.mappings;
         for (var i in mappings) {
             var cols = mappings[i];
@@ -400,12 +403,12 @@ function updateSelectedConfig(loadOptions) {
                 importerHead.find('[name=col' + cols[o] + ']').val(i).change();
             }
         }
-        
+
         if (loadOptions) {
             var options = json.options;
             if (options !== null && typeof options !== 'undefined') {
                 var inputs = $("#importerWizard .customForm");
-                
+
                 for (var o in options) {
                     inputs.find('[name=' + o + ']').val(options[o]);
                 }
@@ -416,11 +419,11 @@ function updateSelectedConfig(loadOptions) {
 
 function checkProcessStatus() {
     flog("checkProcessStatus");
-    
+
     var jobTitle = $(".job-title");
     var resultStatus = $('#job-status');
     var wizard = $('#myWizard');
-    
+
     $.ajax({
         type: 'GET',
         dataType: "json",
@@ -429,17 +432,17 @@ function checkProcessStatus() {
             flog("success", result);
             if (result.status) {
                 resultStatus.text(result.messages[0]);
-                
+
                 if (result.data) {
                     if (!result.data.statusInfo.complete && !result.data.statusInfo.cancelledDate) {
                         wizard.wizard('selectedItem', {step: reviewRow + 1});
                     }
-                    
+
                     var totalRows = 0;
                     var currentRow = 0;
                     var state = result.data.state;
                     flog("state", state);
-                    
+
                     if (state !== null && typeof state !== 'undefined') {
                         if (typeof state.updatedCount !== 'undefined') {
                             wizard.find('.updatedCount').text(state.updatedCount);
@@ -453,16 +456,16 @@ function checkProcessStatus() {
                         if (typeof state.errorCount !== 'undefined') {
                             wizard.find('.errorCount').text(state.errorCount);
                         }
-                        
+
                         totalRows = state.totalRows;
                         currentRow = state.currentRow;
                     }
-                    
+
                     if (result.data.statusInfo.complete) {
                         var dt = result.data.statusInfo.completedDate;
                         flog("Process Completed", dt);
                         jobTitle.text("Process finished at " + pad2(dt.hours) + ":" + pad2(dt.minutes));
-                        
+
                         flog("finished state", state, state.resultHash);
                         if (typeof state.resultHash !== 'undefined' && state.resultHash != null) {
                             var href = "/_hashes/files/" + state.resultHash + ".csv";
@@ -470,7 +473,7 @@ function checkProcessStatus() {
                         } else {
                             wizard.find('.errorRows').closest("a").hide();
                         }
-                        
+
                         var selectedItem = wizard.wizard('selectedItem');
                         if (selectedItem.step !== 1) {
                             wizard.wizard("next");
@@ -496,7 +499,7 @@ function checkProcessStatus() {
                     jobTitle.text("Waiting for process job to start ...");
                 }
                 window.setTimeout(checkProcessStatus, 2500);
-                
+
             } else {
                 flog("No task");
             }
@@ -511,14 +514,14 @@ function sortObjectByValue(obj) {
     var sorted = {};
     var prop;
     var arr = [];
-    
+
     for (prop in obj) {
         if (obj.hasOwnProperty(prop)) {
             var o = {key: prop, value: obj[prop]};
             arr.push(o);
         }
     }
-    
+
     arr.sort(function (a, b) {
         if (a.value > b.value)
             return 1;
@@ -526,10 +529,10 @@ function sortObjectByValue(obj) {
             return -1;
         return 0;
     });
-    
+
     for (i = 0; i < arr.length; i++) {
         sorted[arr[i].key] = arr[i].value;
     }
-    
+
     return sorted;
 }
