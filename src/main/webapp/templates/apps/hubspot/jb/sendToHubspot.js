4635ff500371db09c1ff707075e9617451de0e89
Gonzalo Naveira <gonzaliito@gmail.com>
2017-11-14 14:09:50 +1300

    Hubspot integration [to be finished]

---
+++ src/main/webapp/templates/apps/hubspot/jb/sendToHubspot.js
@@ -0,0 +1,38 @@
+JBNodes['sendToHubspot'] = {
+    icon: 'fa fa-users',
+    title: 'Send To Hubspot',
+    type: JB_NODE_TYPE.ACTION,
+    previewUrl: '/theme/apps/hubspot/jb/sendToHubspot.png',
+    ports: {
+        nextNodeId: {
+            label: 'then',
+            title: 'When completed',
+            maxConnections: 1
+        }
+    },
+
+    settingEnabled: true,
+
+    initSettingForm: function (form) {
+        form.forms({
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
+    }
+};
