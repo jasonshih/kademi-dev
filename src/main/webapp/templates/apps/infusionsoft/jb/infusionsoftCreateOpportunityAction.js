a436cb97745e1403f2d9b6095e7f2a0c424cf7d8
Wesley Tuzza <wesley@tuzza.co>
2017-11-15 11:10:09 +1300

    https://github.com/Kademi/kademi-dev/issues/4398

---
+++ src/main/webapp/templates/apps/infusionsoft/jb/infusionsoftCreateOpportunityAction.js
@@ -0,0 +1,38 @@
+JBNodes['InfusionsoftCreateOpportunity'] = {
+    icon: 'fa fa-envelope',
+    title: 'Create Infusionsoft Opportunity',
+    type: JB_NODE_TYPE.ACTION,
+
+    ports: {
+        nextNodeId: {
+            label: 'then',
+            title: 'When completed',
+            maxConnections: 1
+        }
+    },
+
+    settingEnabled: true,
+
+    initSettingForm: function (form) {
+        form.forms({
+            onSuccess: function () {
+                JBApp.reloadFunnelJson();
+                JBApp.hideSettingPanel();
+            }
+        });
+    },
+
+    showSettingForm: function (form, node) {
+        var href = window.location.pathname;
+        if (!href.endsWith('/')) {
+            href += '/';
+        }
+        href = href + node.nodeId + '?mode=settings';
+
+        form.load(href + ' #frmDetails > *', function () {
+            form.attr('action', href);
+
+            JBApp.showSettingPanel(node);
+        });
+    }
+};
