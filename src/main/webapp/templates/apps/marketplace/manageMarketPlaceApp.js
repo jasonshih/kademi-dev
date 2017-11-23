1045ca3de61d6d9a270dbd59b0eb30f531ae4d51
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-11-23 12:03:08 +0700

    https://github.com/Kademi/kademi-dev/issues/3365

--- src/main/webapp/templates/apps/marketplace/manageMarketPlaceApp.js
+++ src/main/webapp/templates/apps/marketplace/manageMarketPlaceApp.js
@@ -3,7 +3,6 @@ function initMarketPlaceApp() {
     initSubmitReview();
     initInstallApp();
     initMasonryAdapter();
-    $('abbr.timeago').timeago();
 }
 
 function initMasonryAdapter() {
@@ -28,29 +27,17 @@ function initInputLimiter() {
 }
 
 function initSubmitReview() {
-    var reviewForm = $('#reviewForm');
-    var reviewBox = $('#post-review-box');
-    var newReview = $('#new-review');
-    var openReviewBtn = $('#open-review-box');
-    var closeReviewBtn = $('#close-review-box');
-    reviewForm.forms({
+    $('#reviewForm').forms({
         onSuccess: function (resp) {
-            flog("Callback: resp", resp);
-            addReview(resp.data);
-            closeReviewBtn.click(function (e) {
-                e.preventDefault();
-                reviewBox.slideUp(300, function () {
-                    newReview.focus();
-                    openReviewBtn.fadeIn(200);
-                });
-                closeReviewBtn.hide();
+            $('#app-reviews-list').reloadFragment({
+                url: window.location.href
             });
         }
     });
 }
 
 function initInstallApp() {
-    $('body').on('click', '.btn-install-app', function (e) {
+    $(document.body).on('click', '.btn-install-app', function (e) {
         e.preventDefault();
         
         var btn = $(this);
@@ -85,42 +72,6 @@ function initInstallApp() {
     });
 }
 
-function addReview(data) {
-    var reviewsList = $('#reviews-list');
-    var profileImg = '/templates/apps/user/profile.png';
-    if (data.photoHash !== null && data.photoHash.length > 0) {
-        profileImg = "/_hashes/files/" + data.photoHash;
-    }
-    var reviewedOn = new Date(parseInt(data.reviewedOn, 10));
-    var template = $('<section class="comment-list">'
-        + '    <article class="row">'
-        + '        <div class="col-md-2 col-sm-2 hidden-xs">'
-        + '            <figure class="thumbnail">'
-        + '                <img class="img-responsive" src="' + profileImg + '" />'
-        + '            </figure>'
-        + '        </div>'
-        + '        <div class="col-md-10 col-sm-10">'
-        + '            <div class="panel panel-default arrow left">'
-        + '                <div class="panel-body">'
-        + '                    <header class="text-left">'
-        + '                        <div class="comment-user"><i class="fa fa-user"></i> ' + data.reviewedBy + '</div>'
-        + '                        <div><i class="fa fa-smile-o"></i> ' + genRating(data.rating) + '</div>'
-        + '                        <span><i class="fa fa-clock-o"></i> <abbr title=" ' + reviewedOn + '" class="timeago">' + reviewedOn.toISOString() + '</abbr></span>'
-        + '                    </header>'
-        + '                    <div class="comment-post">'
-        + '                        <p>' + data.review + '</p>'
-        + '                    </div>'
-        + '                </div>'
-        + '            </div>'
-        + '        </div>'
-        + '    </article>'
-        + '</section>');
-    
-    template.find('.timeago').timeago();
-    
-    reviewsList.append(template);
-}
-
 function genRating(rating) {
     var r = '<span class="star-rating" title="' + rating + '/5 stars">';
     for (var i = 0; i < 5; i++) {
@@ -138,74 +89,6 @@ function genRating(rating) {
     return r;
 }
 
-(function (e) {
-    var t, o = {
-            className: "autosizejs",
-            append: "",
-            callback: !1,
-            resizeDelay: 10
-        },
-        i = '<textarea tabindex="-1" rows="5" cols="100" style="position:absolute; top:-999px; left:0; right:auto; bottom:auto; border:0; padding: 0; -moz-box-sizing:content-box; -webkit-box-sizing:content-box; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden; transition:none; -webkit-transition:none; -moz-transition:none;"/>',
-        n = ["fontFamily", "fontSize", "fontWeight", "fontStyle", "letterSpacing", "textTransform", "wordSpacing", "textIndent"],
-        s = e(i).data("autosize", !0)[0];
-    s.style.lineHeight = "99px", "99px" === e(s).css("lineHeight") && n.push("lineHeight"), s.style.lineHeight = "", e.fn.autosize = function (i) {
-        return this.length ? (i = e.extend({}, o, i || {}), s.parentNode !== document.body && e(document.body).append(s), this.each(function () {
-            function o() {
-                var t, o;
-                "getComputedStyle" in window ? (t = window.getComputedStyle(u, null), o = u.getBoundingClientRect().width, e.each(["paddingLeft", "paddingRight", "borderLeftWidth", "borderRightWidth"], function (e, i) {
-                    o -= parseInt(t[i], 10)
-                }), s.style.width = o + "px") : s.style.width = Math.max(p.width(), 0) + "px"
-            }
-            
-            function a() {
-                var a = {};
-                if (t = u, s.className = i.className, d = parseInt(p.css("maxHeight"), 10), e.each(n, function (e, t) {
-                        a[t] = p.css(t)
-                    }), e(s).css(a), o(), window.chrome) {
-                    var r = u.style.width;
-                    u.style.width = "0px", u.offsetWidth, u.style.width = r
-                }
-            }
-            
-            function r() {
-                var e, n;
-                t !== u ? a() : o(), s.value = u.value + i.append, s.style.overflowY = u.style.overflowY, n = parseInt(u.style.height, 10), s.scrollTop = 0, s.scrollTop = 9e4, e = s.scrollTop, d && e > d ? (u.style.overflowY = "scroll", e = d) : (u.style.overflowY = "hidden", c > e && (e = c)), e += w, n !== e && (u.style.height = e + "px", f && i.callback.call(u, u))
-            }
-            
-            function l() {
-                clearTimeout(h), h = setTimeout(function () {
-                    var e = p.width();
-                    e !== g && (g = e, r())
-                }, parseInt(i.resizeDelay, 10))
-            }
-            
-            var d, c, h, u = this,
-                p = e(u),
-                w = 0,
-                f = e.isFunction(i.callback),
-                z = {
-                    height: u.style.height,
-                    overflow: u.style.overflow,
-                    overflowY: u.style.overflowY,
-                    wordWrap: u.style.wordWrap,
-                    resize: u.style.resize
-                },
-                g = p.width();
-            p.data("autosize") || (p.data("autosize", !0), ("border-box" === p.css("box-sizing") || "border-box" === p.css("-moz-box-sizing") || "border-box" === p.css("-webkit-box-sizing")) && (w = p.outerHeight() - p.height()), c = Math.max(parseInt(p.css("minHeight"), 10) - w || 0, p.height()), p.css({
-                overflow: "hidden",
-                overflowY: "hidden",
-                wordWrap: "break-word",
-                resize: "none" === p.css("resize") || "vertical" === p.css("resize") ? "none" : "horizontal"
-            }), "onpropertychange" in u ? "oninput" in u ? p.on("input.autosize keyup.autosize", r) : p.on("propertychange.autosize", function () {
-                "value" === event.propertyName && r()
-            }) : p.on("input.autosize", r), i.resizeDelay !== !1 && e(window).on("resize.autosize", l), p.on("autosize.resize", r), p.on("autosize.resizeIncludeStyle", function () {
-                t = null, r()
-            }), p.on("autosize.destroy", function () {
-                t = null, clearTimeout(h), e(window).off("resize", l), p.off("autosize").off(".autosize").css(z).removeData("autosize")
-            }), r())
-        })) : this
-    }
-})(window.jQuery || window.$);
 var __slice = [].slice;
 (function (e, t) {
     var n;
@@ -293,36 +176,11 @@ var __slice = [].slice;
         }
     })
 })(window.jQuery, window);
-$(function () {
-    return $(".starrr").starrr()
-})
 
 $(function () {
-    
-    $('#new-review').autosize({
-        append: "\n"
-    });
-    var reviewBox = $('#post-review-box');
-    var newReview = $('#new-review');
-    var openReviewBtn = $('#open-review-box');
-    var closeReviewBtn = $('#close-review-box');
+    $(".starrr").starrr();
+    $('#new-review').autogrow();
     var ratingsField = $('#ratings-hidden');
-    openReviewBtn.click(function (e) {
-        reviewBox.slideDown(400, function () {
-            $('#new-review').trigger('autosize.resize');
-            newReview.focus();
-        });
-        openReviewBtn.fadeOut(100);
-        closeReviewBtn.show();
-    });
-    closeReviewBtn.click(function (e) {
-        e.preventDefault();
-        reviewBox.slideUp(300, function () {
-            newReview.focus();
-            openReviewBtn.fadeIn(200);
-        });
-        closeReviewBtn.hide();
-    });
     $('.starrr').on('starrr:change', function (e, value) {
         ratingsField.val(value);
     });
