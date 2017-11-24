9b6dce806a3f2cbc26fd8e61f7baef69d51cb4f7
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-11-25 00:07:16 +0700

    https://github.com/Kademi/kademi-dev/issues/3751

--- src/main/marketplace/libs/calendar-lib/APP-INF/app.js
+++ src/main/marketplace/libs/calendar-lib/APP-INF/app.js
@@ -1,1 +1,2 @@
 controllerMappings.addComponent("calendar/components", "multiCalendars", "html", "Display multi-calendars for current and next years in tab format", "Calendar App component");
+controllerMappings.addComponent("calendar/components", "eventManager", "html", "Display panel for managing event. Allows administrator to add, edit events in front-end site", "Calendar App component");
