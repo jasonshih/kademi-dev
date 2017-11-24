49b2ad30463bbda94aac290e06308beecf4fd041
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-11-24 13:40:38 +0700

    https://github.com/Kademi/kademi-dev/issues/4497

---
+++ src/main/marketplace/libs/keditor-lib/common/theme/apps/keditor-lib/dist/js/keditor-component-photo-0.0.0.js
@@ -0,0 +1,231 @@
+/**!
+ * KEditor - Kademi content editor
+ * @copyright: Kademi (http://kademi.co)
+ * @author: Kademi (http://kademi.co)
+ * @version: 0.0.0
+ * @dependencies: $, $.fn.draggable, $.fn.droppable, $.fn.sortable, Bootstrap (optional), FontAwesome (optional)
+ */
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+    
+    KEditor.components['photo'] = {
+        init: function (contentArea, container, component, keditor) {
+            flog('init "photo" component', component);
+            
+            var componentContent = component.children('.keditor-component-content');
+            var img = componentContent.find('img');
+            
+            img.css('display', 'inline-block');
+            if (!img.css('vertical-align')) {
+                img.css('vertical-align', 'middle');
+            }
+            
+            img.attr({
+                width: img.width(),
+                height: img.height()
+            }).css({
+                width: '',
+                height: ''
+            });
+            
+            var options = keditor.options;
+            if (typeof options.onComponentReady === 'function') {
+                options.onComponentReady.call(contentArea, component);
+            }
+        },
+        
+        settingEnabled: true,
+        
+        settingTitle: 'Photo Settings',
+        
+        initSettingForm: function (form, keditor) {
+            flog('init "photo" settings', form, keditor);
+            
+            var self = this;
+            
+            return $.ajax({
+                url: '/theme/apps/keditor-lib/componentPhotoSettings.html',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                    
+                    var txtLink = form.find('#photo-link');
+                    txtLink.on('change', function () {
+                        var link = this.value.trim();
+                        keditor.getSettingComponent().find('a').attr('href', link);
+                    });
+                    
+                    var cbbTarget = form.find('#photo-target');
+                    cbbTarget.on('change', function () {
+                        keditor.getSettingComponent().find('a').attr('target', this.value);
+                    });
+                    
+                    var chkLinkable = form.find('#photo-linkable');
+                    chkLinkable.on('click', function () {
+                        var img = keditor.getSettingComponent().find('img');
+                        
+                        if (chkLinkable.is(':checked')) {
+                            txtLink.prop('disabled', false);
+                            cbbTarget.prop('disabled', false);
+                            img.wrap('<a href=""></a>');
+                        } else {
+                            txtLink.prop('disabled', true);
+                            cbbTarget.prop('disabled', true);
+                            img.unwrap('a');
+                        }
+                    });
+                    
+                    var inputAlign = form.find('#photo-align');
+                    inputAlign.on('change', function () {
+                        var panel = keditor.getSettingComponent().find('.photo-panel');
+                        panel.css('text-align', this.value);
+                    });
+                    
+                    var inputVAlign = form.find('#v-align');
+                    inputVAlign.on('change', function () {
+                        var panel = keditor.getSettingComponent().find('.photo-panel').find('img');
+                        panel.css('vertical-align', this.value);
+                    });
+                    
+                    var inputResponsive = form.find('#photo-responsive');
+                    inputResponsive.on('click', function () {
+                        keditor.getSettingComponent().find('img')[this.checked ? 'addClass' : 'removeClass']('img-responsive');
+                    });
+                    
+                    var cbbStyle = form.find('#photo-style');
+                    cbbStyle.on('change', function () {
+                        var img = keditor.getSettingComponent().find('img');
+                        var val = this.value;
+                        
+                        img.removeClass('img-rounded img-circle img-thumbnail');
+                        if (val) {
+                            img.addClass(val);
+                        }
+                    });
+                    
+                    var inputWidth = form.find('#photo-width');
+                    var inputHeight = form.find('#photo-height');
+                    inputWidth.on('change', function () {
+                        var img = keditor.getSettingComponent().find('img');
+                        var newWidth = +this.value;
+                        var newHeight = Math.round(newWidth / self.ratio);
+                        
+                        if (newWidth <= 0) {
+                            newWidth = self.width;
+                            newHeight = self.height;
+                            this.value = newWidth;
+                        }
+                        
+                        img.attr({
+                            'width': newWidth,
+                            'height': newHeight
+                        });
+                        inputHeight.val(newHeight);
+                    });
+                    inputHeight.on('change', function () {
+                        var img = keditor.getSettingComponent().find('img');
+                        var newHeight = +this.value;
+                        var newWidth = Math.round(newHeight * self.ratio);
+                        
+                        if (newHeight <= 0) {
+                            newWidth = self.width;
+                            newHeight = self.height;
+                            this.value = newHeight;
+                        }
+                        
+                        img.attr({
+                            'height': newHeight,
+                            'width': newWidth
+                        });
+                        inputWidth.val(newWidth);
+                    });
+                    
+                    var photoEdit = form.find('#photo-edit');
+                    photoEdit.mselect({
+                        contentTypes: ['image'],
+                        bs3Modal: true,
+                        pagePath: keditor.options.pagePath,
+                        basePath: keditor.options.basePath,
+                        onSelectFile: function (url, relativeUrl, fileType, hash) {
+                            var img = keditor.getSettingComponent().find('img');
+                            var src = '/_hashes/files/' + hash;
+                            
+                            $('<img />').attr('src', src).load(function () {
+                                img.attr({
+                                    width: this.width,
+                                    height: this.height,
+                                    src: src
+                                });
+                                self.ratio = this.width / this.height;
+                                self.width = this.width;
+                                self.height = this.height;
+                                inputWidth.val(this.width);
+                                inputHeight.val(this.height);
+                            });
+                        }
+                    });
+                }
+            });
+        },
+        
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "photo" component', component);
+            
+            var self = this;
+            var inputAlign = form.find('#photo-align');
+            var inputVAlign = form.find('#v-align');
+            var inputResponsive = form.find('#photo-responsive');
+            var inputWidth = form.find('#photo-width');
+            var inputHeight = form.find('#photo-height');
+            var cbbStyle = form.find('#photo-style');
+            var txtLink = form.find('#photo-link');
+            var cbbTarget = form.find('#photo-target');
+            var chkLinkable = form.find('#photo-linkable');
+            
+            var panel = component.find('.photo-panel');
+            var img = panel.find('img');
+            
+            var a = img.parent('a');
+            if (a.length > 0) {
+                chkLinkable.prop('checked', true);
+                txtLink.prop('disabled', false).val(a.attr('href'));
+                cbbTarget.prop('disabled', false).val(a.attr('target'));
+            } else {
+                chkLinkable.prop('checked', false);
+                txtLink.prop('disabled', true).val('');
+                cbbTarget.prop('disabled', true).val('');
+            }
+            
+            var align = panel.css('text-align');
+            if (align !== 'right' || align !== 'center') {
+                align = 'left';
+            }
+            
+            var valign = img.css('vertical-align');
+            
+            if (img.hasClass('img-rounded')) {
+                cbbStyle.val('img-rounded');
+            } else if (img.hasClass('img-circle')) {
+                cbbStyle.val('img-circle');
+            } else if (img.hasClass('img-thumbnail')) {
+                cbbStyle.val('img-thumbnail');
+            } else {
+                cbbStyle.val('');
+            }
+            inputAlign.val(align);
+            inputVAlign.val(valign);
+            inputResponsive.prop('checked', img.hasClass('img-responsive'));
+            inputWidth.val(img.width());
+            inputHeight.val(img.height());
+            
+            $('<img />').attr('src', img.attr('src')).load(function () {
+                self.ratio = this.width / this.height;
+                self.width = this.width;
+                self.height = this.height;
+            });
+        }
+    };
+    
+})(jQuery);
---
+++ src/main/marketplace/libs/keditor-lib/common/theme/apps/keditor-lib/src/js/keditor-component-media.js
@@ -0,0 +1,215 @@
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
+    
+    KEditor.components['media'] = {
+        init: function (contentArea, container, component, keditor) {
+            flog('init "media" component', component);
+            
+            var options = keditor.options;
+            var componentContent = component.children('.keditor-component-content');
+            var img = componentContent.find('img.media-object');
+            
+            img.css('display', 'inline-block');
+            
+            var mediaBody = componentContent.find('.media-body');
+            mediaBody.addClass('clearfix');
+            
+            var mediaLeft = componentContent.find('.media-left');
+            var mediaLeftInner = componentContent.find('.media-left-inner');
+            if (mediaLeftInner.length === 0) {
+                mediaLeft.html('<div class="media-left-inner">' + mediaLeft.html() + '</div>');
+            }
+            
+            if (!mediaBody.attr('id')) {
+                mediaBody.attr('id', keditor.generateId('component-text-content-inner'));
+            }
+            
+            mediaBody.prop('contenteditable', true);
+            mediaBody.on('input', function (e) {
+                if (typeof options.onComponentChanged === 'function') {
+                    options.onComponentChanged.call(contentArea, e, component);
+                }
+                
+                if (typeof options.onContainerChanged === 'function') {
+                    options.onContainerChanged.call(contentArea, e, container);
+                }
+                
+                if (typeof options.onContentChanged === 'function') {
+                    options.onContentChanged.call(contentArea, e);
+                }
+            });
+            
+            var editor = mediaBody.ckeditor(options.ckeditorOptions).editor;
+            editor.on('instanceReady', function () {
+                flog('CKEditor is ready', component);
+                
+                if (typeof options.onComponentReady === 'function') {
+                    options.onComponentReady.call(contentArea, component, editor);
+                }
+            });
+        },
+        
+        getContent: function (component, keditor) {
+            flog('getContent "media" component', component);
+            
+            var componentContent = component.find('.keditor-component-content');
+            var mediaBody = componentContent.find('.media-body');
+            
+            var id = mediaBody.attr('id');
+            var editor = CKEDITOR.instances[id];
+            if (editor) {
+                mediaBody.replaceWith('<div class="media-body clearfix">' + editor.getData() + '</div>');
+            }
+            
+            return componentContent.html();
+        },
+        
+        destroy: function (component, keditor) {
+            flog('destroy "text" component', component);
+            
+            var id = component.find('.media-body').attr('id');
+            var editor = CKEDITOR.instances[id];
+            if (editor) {
+                editor.destroy();
+            }
+        },
+        
+        settingEnabled: true,
+        
+        settingTitle: 'Media Settings',
+        
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "media" component');
+            
+            return $.ajax({
+                url: '/theme/apps/keditor-lib/componentMediaSettings.html',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+                    var txtLink = form.find('.photo-link');
+                    txtLink.on('change', function () {
+                        var link = this.value.trim();
+                        keditor.getSettingComponent().find('a').attr('href', link);
+                    });
+                    
+                    var cbbTarget = form.find('.photo-target');
+                    cbbTarget.on('change', function () {
+                        keditor.getSettingComponent().find('a').attr('target', this.value);
+                    });
+                    
+                    var chkLinkable = form.find('.photo-linkable');
+                    chkLinkable.on('click', function () {
+                        var img = keditor.getSettingComponent().find('img.media-object');
+                        
+                        if (chkLinkable.is(':checked')) {
+                            txtLink.prop('disabled', false);
+                            cbbTarget.prop('disabled', false);
+                            img.wrap('<a href=""></a>');
+                        } else {
+                            txtLink.prop('disabled', true);
+                            cbbTarget.prop('disabled', true);
+                            img.unwrap('a');
+                        }
+                    });
+                    
+                    form.find('.photo-v-align').on('change', function () {
+                        var mediaLeft = keditor.getSettingComponent().find('.media-left');
+                        mediaLeft.css('vertical-align', this.value);
+                    });
+                    
+                    form.find('.photo-responsive').on('click', function () {
+                        keditor.getSettingComponent().find('img')[this.checked ? 'addClass' : 'removeClass']('img-responsive');
+                    });
+                    
+                    form.find('.photo-style').on('change', function () {
+                        var img = keditor.getSettingComponent().find('img.media-object');
+                        var val = this.value;
+                        
+                        img.removeClass('img-rounded img-circle img-thumbnail');
+                        if (val) {
+                            img.addClass(val);
+                        }
+                    });
+                    
+                    form.find('.photo-width').on('change', function () {
+                        var img = keditor.getSettingComponent().find('img.media-object');
+                        img.attr('width', this.value);
+                    });
+                    
+                    form.find('.photo-height').on('change', function () {
+                        var img = keditor.getSettingComponent().find('img.media-object');
+                        img.attr('height', this.value);
+                    });
+                    
+                    var photoEdit = form.find('.photo-edit');
+                    photoEdit.mselect({
+                        contentTypes: ['image'],
+                        bs3Modal: true,
+                        pagePath: keditor.options.pagePath,
+                        basePath: keditor.options.basePath,
+                        onSelectFile: function (url, relativeUrl, fileType, hash) {
+                            var img = keditor.getSettingComponent().find('img.media-object');
+                            
+                            img.attr('src', '/_hashes/files/' + hash);
+                        }
+                    });
+                    
+                    form.find('.left-width').on('change', function () {
+                        var mediaLeftInner = keditor.getSettingComponent().find('.media-left-inner');
+                        mediaLeftInner.css('width', this.value);
+                    });
+                }
+            });
+        },
+        
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "media" component', component);
+            
+            var inputVAlign = form.find('.photo-v-align');
+            var inputResponsive = form.find('.photo-responsive');
+            var inputWidth = form.find('.photo-width');
+            var inputHeight = form.find('.photo-height');
+            var cbbStyle = form.find('.photo-style');
+            var txtLink = form.find('.photo-link');
+            var cbbTarget = form.find('.photo-target');
+            var chkLinkable = form.find('.photo-linkable');
+            
+            var img = component.find('img.media-object');
+            var mediaLeft = component.find('.media-left');
+            var mediaLeftInner = component.find('.media-left-inner');
+            
+            form.find('.left-width').val(mediaLeftInner.get(0).style.width || '');
+            
+            var a = img.parent('a');
+            if (a.length > 0) {
+                chkLinkable.prop('checked', true);
+                txtLink.prop('disabled', false).val(a.attr('href'));
+                cbbTarget.prop('disabled', false).val(a.attr('target'));
+            } else {
+                chkLinkable.prop('checked', false);
+                txtLink.prop('disabled', true).val('');
+                cbbTarget.prop('disabled', true).val('');
+            }
+            
+            var valign = mediaLeft.css('vertical-align') || 'top';
+            
+            if (img.hasClass('img-rounded')) {
+                cbbStyle.val('img-rounded');
+            } else if (img.hasClass('img-circle')) {
+                cbbStyle.val('img-circle');
+            } else if (img.hasClass('img-thumbnail')) {
+                cbbStyle.val('img-thumbnail');
+            } else {
+                cbbStyle.val('');
+            }
+            
+            inputVAlign.val(valign);
+            inputResponsive.prop('checked', img.hasClass('img-responsive'));
+            inputWidth.val(img.width());
+            inputHeight.val(img.height());
+        }
+    };
+    
+})(jQuery);
