520cb6c018c3829858b6d6b3f69e12353f1c6634
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-11-28 16:30:39 +0700

    https://github.com/Kademi/kademi-dev/issues/4468

--- src/main/marketplace/apps/KFullStory/APP-INF/app.js
+++ src/main/marketplace/apps/KFullStory/APP-INF/app.js
@@ -22,6 +22,7 @@ function initKFullStory(orgRoot, webRoot, enabled) {
             }
         }
         
+        saveMapping(db);
     } else {
         log.info("I'm in an organisation");
     }
@@ -39,11 +40,107 @@ controllerMappings
     .postPriviledge("READ_CONTENT")
     .build();
 
+controllerMappings
+    .setUserTimelineFunction('generateProfileTimeline');
+
+function generateProfileTimeline(page, userResource, list) {
+    var queryJson = {
+        'sort': {
+            'savedDate': 'desc'
+        },
+        'stored_fields': [
+            'session',
+            'userId',
+            'savedDate'
+        ],
+        'size': 999,
+        'query': {
+            'bool': {
+                'must': [{
+                    'type': {
+                        'value': TYPE_RECORD
+                    }
+                }],
+                'filter': [
+                    {
+                        'term': {
+                            'userId': userResource.userId
+                        }
+                    }
+                ]
+            }
+        }
+    };
+    var searchResult = doDBSearch(page, queryJson);
+    
+    if (searchResult && searchResult.hits.totalHits > 0) {
+        for (var i = 0; i < searchResult.hits.totalHits; i++) {
+            var record = searchResult.hits.hits[i];
+            
+            var bean = applications.stream.streamEventBuilder()
+                .profile(userResource)
+                .category('info')
+                .icon('fa fa-play-circle')
+                .desc('Your session url: ' + record.fields.session.value)
+                .title('FullStory session is started')
+                .inbound(false)
+                .date(formatter.parseDate(record.fields.savedDate.value))
+                .build();
+            
+            list.add(bean);
+        }
+    }
+}
+
 function saveSession(page, params) {
     log.info('saveSession > page={}, params={}', page, params);
     
     var db = getDB(page);
+    var currentUser = securityManager.currentUser;
     
+    var queryJson = {
+        'stored_fields': [
+            'session',
+            'userId'
+        ],
+        'size': 1,
+        'query': {
+            'bool': {
+                'must': [{
+                    'type': {
+                        'value': TYPE_RECORD
+                    }
+                }],
+                'filter': [
+                    {
+                        'term': {
+                            'session': params.session
+                        }
+                    }, {
+                        'term': {
+                            'userId': currentUser.userId
+                        }
+                    }
+                ]
+            }
+        }
+    };
+    var searchResult = doDBSearch(page, queryJson);
+    
+    if (searchResult && searchResult.hits.totalHits > 0) {
+        // Do nothing
+    } else {
+        var recordName = 'record-' + generateRandomText(32);
+        
+        securityManager.runAsUser(currentUser, function () {
+            db.createNew(recordName, JSON.stringify({
+                recordId: recordName,
+                session: params.session,
+                userId: currentUser.userId,
+                savedDate: (new Date()).toISOString()
+            }), TYPE_RECORD);
+        });
+    }
 }
 
 // ============================================================================
--- src/main/marketplace/apps/KFullStory/APP-INF/config.js
+++ src/main/marketplace/apps/KFullStory/APP-INF/config.js
@@ -3,6 +3,36 @@ var DB_NAME = 'KFullStory';
 var DB_TITLE = 'KFullStory';
 var dbType = 'json';
 
+var recordMapping = {
+    "properties": {
+        "recordName": {
+            "type": "text",
+            "store": true
+        },
+        "session": {
+            "type": "keyword",
+            "store": true,
+            "fields": {
+                "text": {
+                    "type": "text"
+                }
+            }
+        },
+        "userId": {
+            "type": "integer",
+            "store": true
+        },
+        "savedDate": {
+            "type": "date",
+            "store": true
+        }
+    }
+}
+
+var DB_MAPPINGS = {
+    record: recordMapping
+};
+
 var JSON_DB = '/jsondb';
 
 var TYPE_RECORD = 'record';
