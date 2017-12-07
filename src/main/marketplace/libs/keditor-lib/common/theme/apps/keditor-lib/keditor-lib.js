6a5db8243f7eacf8dbdcf834fb0855e7df2af61a
Duc Doan Hoang Minh <ducdhm@gmail.com>
2017-12-07 16:38:45 +0700

    keditor-lib supports loadKEditor method

---
+++ src/main/marketplace/libs/keditor-lib/common/theme/apps/keditor-lib/keditor-lib.js
@@ -0,0 +1,20 @@
+var KEDITOR_PATH = '/theme/apps/keditor-lib/dist/';
+var KEDITOR_JS_PATH = KEDITOR_PATH + 'js/keditor-0.0.0.min.js';
+var KEDITOR_COMPONENTS_JS_PATH = KEDITOR_PATH + 'js/keditor-components-0.0.0.js';
+var KEDITOR_CSS_PATH = KEDITOR_PATH + 'css/keditor-0.0.0.min.css';
+var KEDITOR_COMPONENTS_CSS_PATH = KEDITOR_PATH + 'css/keditor-components-0.0.0.css';
+
+function loadKEditor(callback) {
+    $.getStyleOnce(KEDITOR_CSS_PATH);
+    $.getStyleOnce(KEDITOR_COMPONENTS_CSS_PATH);
+    
+    $.getScriptOnce('/static/jquery-ui/1.12.1-noui/jquery-ui.min.js', function () {
+        $.getScriptOnce(KEDITOR_JS_PATH, function () {
+            $.getScriptOnce(KEDITOR_COMPONENTS_JS_PATH, function () {
+                if (typeof  callback === 'function') {
+                    callback();
+                }
+            });
+        });
+    });
+}
