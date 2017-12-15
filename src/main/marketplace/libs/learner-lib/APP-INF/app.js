780ae9c5de465325be599ae07c6cd90b7041b4c2
anh <anhnt38@gmail.com>
2017-12-15 14:12:05 +0700

    https://github.com/Kademi/kademi-dev/issues/4627

--- src/main/marketplace/libs/learner-lib/APP-INF/app.js
+++ src/main/marketplace/libs/learner-lib/APP-INF/app.js
@@ -2,3 +2,94 @@ controllerMappings.addComponent("learner/components", "modulesTile", "html", "Di
 controllerMappings.addComponent("learner/components", "modulesList", "html", "Displays modules in list or grid format", "Learning App");
 controllerMappings.addComponent("learner/components", "coursePicker", "html", "Displays course picker", "Learning App");
 controllerMappings.addComponent("learner/components", "courseDescription", "html", "Displays course description", "Learning App");
+controllerMappings.addComponent("learner/components", "moduleStatuses", "html", "Displays table of module statuses", "Learning App");
+
+function buildTableModuleStatuses(programCode, courseCode, moduleCode, complete, profile) {
+    var query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "type": {
+                            "value": "moduleStatus"
+                        }
+                    }
+                ]
+            }
+        },
+        "aggs": {
+            "byProgramCode": {
+                "terms": {
+                    "field": "programCode"
+                }
+            },
+            "byCourseCode": {
+                "terms": {
+                    "field": "courseCode"
+                }
+            },
+            "byModuleCode": {
+                "terms": {
+                    "field": "moduleCode"
+                }
+            }
+        },
+        "size": 1000,
+        "from": 0
+    };
+
+    if (complete){
+        query.query.bool.must.push({
+            "term": {
+                "complete": complete == 'true'
+            }
+        });
+    }
+
+    if (moduleCode){
+        query.query.bool.must.push({
+            "term": {
+                "moduleCode": moduleCode
+            }
+        });
+    }
+
+    if (courseCode){
+        query.query.bool.must.push({
+            "term": {
+                "courseCode": courseCode
+            }
+        });
+    }
+
+    if (programCode){
+        query.query.bool.must.push({
+            "term": {
+                "programCode": programCode
+            }
+        });
+    }
+
+    if (profile){
+        query.query.bool.must.push({
+            "term": {
+                "profile": params.profile
+            }
+        });
+    }
+
+    if (queryManager.selectedOrgIds && queryManager.selectedOrgIds.length){
+        var arr = [];
+        queryManager.selectedOrgIds.forEach(function (item) {
+            arr.push(item);
+        });
+        query.query.bool.must.push({
+            "terms": {
+                "parentOrgs": arr
+            }
+        });
+    }
+    var sm = applications.search.searchManager;
+    var resp = sm.search(JSON.stringify(query), 'profile');
+    return resp;
+}
---
+++ src/main/marketplace/libs/learner-lib/common/theme/apps/learner/components/moduleStatusesComponent.js
@@ -0,0 +1,84 @@
+(function ($) {
+    var KEditor = $.keditor;
+
+    KEditor.components['moduleStatuses'] = {
+        settingEnabled: true,
+
+        settingTitle: 'Module Statuses Table',
+
+        initSettingForm: function (form, keditor) {
+            return $.ajax({
+                url: '_components/moduleStatuses?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
+
+                    form.find('.query-height').on('change', function () {
+                        var number = this.value;
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        if (isNaN(number) || number < 200) {
+                            number = 200;
+                            this.value = number;
+                        }
+
+                        component.attr('data-height', number);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
+                    form.find('.txt-title').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        component.attr('data-title', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
+                    form.find('.complete').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        component.attr('data-complete', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
+                    form.find('.programCode').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        component.attr('data-program-code', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
+                    form.find('.courseCode').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        component.attr('data-course-code', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
+                    form.find('.moduleCode').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        component.attr('data-module-code', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+                }
+            });
+        },
+
+        showSettingForm: function (form, component, keditor) {
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            form.find('.query-height').val(dataAttributes['data-height']);
+            form.find('.txt-title').val(dataAttributes['data-title']);
+            form.find('.complete').val(dataAttributes['data-complete']);
+            form.find('.programCode').val(dataAttributes['data-program-code']);
+            form.find('.courseCode').val(dataAttributes['data-course-code']);
+            form.find('.moduleCode').val(dataAttributes['data-module-code']);
+
+        }
+    };
+})(jQuery);
