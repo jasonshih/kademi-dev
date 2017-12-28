5476e46ffc95e550f935bbe4a25e0a177663d04d
anh <anhnt38@gmail.com>
2017-12-28 11:50:36 +0700

    https://github.com/Kademi/kademi-dev/issues/4699

--- src/main/webapp/templates/apps/products/manageSkus.js
+++ src/main/webapp/templates/apps/products/manageSkus.js
@@ -117,22 +117,16 @@
     }
 
     function initUpdateSku() {
-        $('#productsTableBody').on('change', '.input-sku', function (e) {
-            var target = $(e.target);
-            var newSku = target.val();
+        $('#productsTableBody').on('input', '.input-sku', function (e) {
+            if ((e.keyCode || e.which) == keymap.ENTER){
+                doUpdateSkuCode();
+                return false;
+            }
+            typewatch(function () {
+                doUpdateSkuCode()
+            }, 1000);
 
-            var row = target.closest('tr');
-            var td = row.find('td:first-child');
-            var rowId = row.attr('id');
-            var productId = td.data('productid');
-            var skuId = td.data('skuid');
-            var options = td.data('options');
-
-            updateSku(productId, options, skuId, newSku, rowId);
-        });
-
-        $('#productsTableBody').on('keyup', '.input-sku', function (e) {
-            if ((e.keyCode || e.which) == keymap.ENTER) {
+            function doUpdateSkuCode() {
                 var target = $(e.target);
                 var newSku = target.val();
 
@@ -149,20 +143,9 @@
     }
 
     function initUpdateSkuTitle() {
-        $('#productsTableBody').on('change', '.input-sku-Title', function (e) {
+        $('#productsTableBody').on('input', '.input-sku-Title', function (e) {
             var target = $(this);
-            var newSkuTitle = target.val();
-            var row = target.closest('tr');
-            var td = row.find('td:first-child');
-            var rowId = row.attr('id');
-            var skuId = td.data('skuid');
-
-            updateSkuTitle(skuId, newSkuTitle, rowId);
-        });
-
-        $('#productsTableBody').on('keyup', '.input-sku-Title', function (e) {
-            if ((e.keyCode || e.which) == keymap.ENTER) {
-                var target = $(e.target);
+            typewatch(function () {
                 var newSkuTitle = target.val();
                 var row = target.closest('tr');
                 var td = row.find('td:first-child');
@@ -170,7 +153,7 @@
                 var skuId = td.data('skuid');
 
                 updateSkuTitle(skuId, newSkuTitle, rowId);
-            }
+            }, 1000)
         });
     }
 
@@ -412,6 +395,9 @@
     }
 
     function updateSkuTitle(skuId, newSkuTitle, rowId) {
+        if (!newSkuTitle){
+            return Msg.error('Title must not be blank');
+        }
         $.ajax({
             type: 'POST',
             url: window.href,
