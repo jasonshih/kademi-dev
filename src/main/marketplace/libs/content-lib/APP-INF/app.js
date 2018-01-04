bd7852c2a08ab6089cfacd7fb0cf741329167919
brad <brad@kademi.co>
2018-01-05 10:45:14 +1300

    https://github.com/Kademi/kademi-dev/issues/4735

--- src/main/marketplace/libs/content-lib/APP-INF/app.js
+++ src/main/marketplace/libs/content-lib/APP-INF/app.js
@@ -4,3 +4,6 @@ controllerMappings.addComponent("content/components", "htmlPanel", "html", "Disp
 controllerMappings.addComponent("content/components", "showHideButton", "html", "A button which causes all following content to be initially hidden, and is only shown when clicked", "Content App component");
 controllerMappings.addComponent("content/components", "rating", "html", "Shows a rating stars which is predefined by admin", "Content App component");
 controllerMappings.addComponent("content/components", "tile", "html", "Shows a tile which contains image, text and link", "Content App component");
+
+// Asset stuff
+controllerMappings.addComponent("content/components", "assetQueryList", "html", "Shows a list of assets from an asset query", "Content App component");
