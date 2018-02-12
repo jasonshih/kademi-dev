3426bf52517e8dabd0f068acf1bfd860c137b11b
Duc Doan Hoang Minh <ducdhm@gmail.com>
2018-02-12 12:06:26 +0700

    https://github.com/Kademi/kademi-dev/issues/3751

--- src/main/webapp/static/jquery.mselect/1.1.1/jquery.mselect-1.1.1.js
+++ src/main/webapp/static/jquery.mselect/1.1.1/jquery.mselect-1.1.1.js
@@ -430,14 +430,21 @@
         var self = this;
         var options = self.options;
         var btnEditImage = container.find('.btn-edit-image');
+        var mselectModal;
+        if (options.useModal) {
+            mselectModal = container;
+        }
+        if (!mselectModal) {
+            mselectModal = $('.modal-mselect');
+        }
         
         var photoEditor = btnEditImage.photoEditor({
             modalSize: 'modal-lg',
             modalAuto: false,
             btnCancelText: 'Back to Upload',
             onModalShow: function () {
-                if (options.useModal) {
-                    container.modal('hide');
+                if (mselectModal && mselectModal.length > 0) {
+                    mselectModal.modal('hide');
                 }
             },
             onModalShown: function () {
@@ -446,8 +453,8 @@
                 this.setImage(hash ? '/_hashes/files/' + hash : '/assets/' + uniqueId);
             },
             onCancel: function () {
-                if (options.useModal) {
-                    container.modal('show');
+                if (mselectModal && mselectModal.length > 0) {
+                    mselectModal.modal('show');
                 }
             },
             onSave: function (data) {
