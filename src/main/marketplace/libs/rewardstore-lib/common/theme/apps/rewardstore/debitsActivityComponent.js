40c315eba4ee4e78a36908b233e584419fc7bd19
Duc Doan Hoang Minh <ducdhm@gmail.com>
2018-03-09 14:12:20 +0700

    https://github.com/Kademi/kademi-dev/issues/5097

--- src/main/marketplace/libs/rewardstore-lib/common/theme/apps/rewardstore/debitsActivityComponent.js
+++ src/main/marketplace/libs/rewardstore-lib/common/theme/apps/rewardstore/debitsActivityComponent.js
@@ -1,81 +1,71 @@
-$(function () {
-    // Parse JSON
-    var jsonResp = $('.debits-activity').data("jsonresp");
-    console.info("jsonResp", jsonResp);
+(function ($) {
+    var KEditor = $.keditor;
+    var flog = KEditor.log;
 
-    function initActivityChart(resp) {
-        if ($('.debits-activity').length > 0) {
-            nv.addGraph(function () {
-                var chart = nv.models.multiBarChart()
-                    .margin({top: 0, right: 0, bottom: 0, left: 0})
-                    .showControls(false)
-                    .showLegend(false)
-                    .showYAxis(false)
-                    .showXAxis(false);
+    KEditor.components['debitsActivity'] = {
+        settingEnabled: true,
+        settingTitle: 'Debits Activity Settings',
+        initSettingForm: function (form, keditor) {
+            return $.ajax({
+                url: '_components/debitsActivity?settings',
+                type: 'get',
+                dataType: 'HTML',
+                success: function (resp) {
+                    form.html(resp);
 
-                chart.xAxis     //Chart x-axis settings
-                    .axisLabel('Date')
-                    .tickFormat(function (d) {
-                        return moment(d).format("DD MMM");
+                    form.find('.debitsActivityBucket').on('change', function () {
+                        var selectedBucket = this.value;
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        if (selectedBucket) {
+                            component.attr('data-bucket', selectedBucket);
+                            keditor.initDynamicContent(dynamicElement);
+                        } else {
+                            dynamicElement.html('<p>Please select a points bucket</p>');
+                        }
                     });
 
-                chart.yAxis     //Chart y-axis settings
-                    .axisLabel('Count')
-                    .tickFormat(d3.format('.02f'));
+                    form.find('.debitsActivityDays').on('change', function () {
+                        var number = this.value;
 
-                /* Generate data*/
-                var debits = [];
-                var myData = [
-                    {
-                        values: debits,
-                        key: 'Debits',
-                        color: '#2ca02c'
-                    }
-                ];
+                        if (isNaN(number) || +number <= 1) {
+                            number = 1;
+                            this.value = number;
+                        }
 
-                if (resp && resp.aggregations) {
-                    var d = resp.aggregations;
-                    var sBuckets = d.debits.dates.buckets;
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
 
+                        component.attr('data-days', number);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
 
-                    for (var i = 0; i < sBuckets.length; i++) {
-                        var b = sBuckets[i];
-                        debits.push({
-                            x: b.key,
-                            y: b.sum.value || 0
-                        });
-                    }
+                    form.find('.debitsActivityHeight').on('change', function () {
+                        var number = this.value;
 
+                        if (isNaN(number) || +number <= 99) {
+                            number = 100;
+                            this.value = number;
+                        }
 
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+
+                        component.attr('data-height', number);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
                 }
-
-                debits.sort(dynamicSort('x'));
-
-                d3.select('#rewardDebitsActivityChart-' + $('.debits-activity').data("bucketId") + ' svg')    //Select the <svg> element you want to render the chart in.
-                    .datum(myData)         //Populate the <svg> element with chart data...
-                    .call(chart);          //Finally, render the chart!
-
-                //Update the chart when window resizes.
-                nv.utils.windowResize(function () {
-                    chart.update()
-                });
-                return chart;
             });
+        },
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "debitsActivity" component');
+
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+            form.find('.debitsActivityBucket').val(dataAttributes['data-bucket']);
+            form.find('.debitsActivityDays').val(dataAttributes['data-days']);
+            form.find('.debitsActivityHeight').val(dataAttributes['data-height']);
         }
+    };
 
-    }
-
-    function dynamicSort(property) {
-        var sortOrder = 1;
-        if (property[0] === "-") {
-            sortOrder = -1;
-            property = property.substr(1);
-        }
-        return function (a, b) {
-            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-            return result * sortOrder;
-        };
-    }
-
-    initActivityChart(jsonResp);
-});
+})(jQuery);
