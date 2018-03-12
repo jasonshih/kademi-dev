a0675724a84396d512821528043893b1933ef04b
Duc Doan Hoang Minh <ducdhm@gmail.com>
2018-03-12 13:45:57 +0700

    https://github.com/Kademi/kademi-dev/issues/5056

--- src/main/marketplace/libs/orgsLocator-lib/website/theme/apps/orgsLocator/orgsLocator.js
+++ src/main/marketplace/libs/orgsLocator-lib/website/theme/apps/orgsLocator/orgsLocator.js
@@ -1,18 +1,18 @@
 (function ($, window) {
     var win = $(window);
-    
+
     window.initOrgsLocator = function (orgsLocator) {
         flog('initOrgsLocator', orgsLocator);
-        
+
         var uri = new URI(window.location.pathname + window.location.search);
         var params = uri.search(true);
-        
+
         var id = orgsLocator.attr('id');
         var initLat = orgsLocator.attr('data-lat');
         initLat = +initLat || -33.867;
         var initLng = orgsLocator.attr('data-lng');
         initLng = +initLng || 151.195;
-        
+
         var options = {
             searchUrl: '/orgsLocator/',
             initLatLng: [initLat, initLng],
@@ -23,11 +23,11 @@
                 win.on('resize', function () {
                     var mapWrapper = mapDiv.parent();
                     var winWidth = win.width();
-                    
+
                     itemsWrapper.css('height', '');
                     itemsWrapper.css('max-height', '');
                     mapWrapper.css('padding-bottom', '');
-                    
+
                     if (winWidth < 768) {
                         mapWrapper.css('padding-bottom', '120%');
                         itemsWrapper.css('max-height', 200);
@@ -39,29 +39,29 @@
                 }).trigger('resize');
             }
         };
-        
+
         var component = orgsLocator.closest('[data-type="component-orgsLocator"]');
         var searchWhenInit = component.attr('data-search-when-init') === 'true';
-        
+
         if (searchWhenInit) {
             options.searchWhenInit = searchWhenInit;
             options.initSearchOptions = {
                 query: params.query || '',
-                lat:  params.lat ? +params.lat : null,
-                lng: params.lng ? +params.lng : null
+                lat: +params.lat,
+                lng: +params.lng
             };
         }
-        
+
         flog('Init orgsLocator', options);
         orgsLocator.orgFinder(options);
     };
-    
+
     $(function () {
         $('.orgs-locator-component').each(function () {
             var orgsLocator = $(this);
-            
+
             initOrgsLocator(orgsLocator);
         });
     });
-    
+
 })(jQuery, window);
