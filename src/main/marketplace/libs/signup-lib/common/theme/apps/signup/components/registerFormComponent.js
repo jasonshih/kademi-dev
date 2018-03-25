afa6a5a62d901fcd9628cf71dc6e5025a617b722
brad <brad@kademi.co>
2018-03-26 12:20:41 +1300

    https://github.com/Kademi/kademi-dev/issues/5193 - signup-lib 1.1.6

--- src/main/marketplace/libs/signup-lib/common/theme/apps/signup/components/registerFormComponent.js
+++ src/main/marketplace/libs/signup-lib/common/theme/apps/signup/components/registerFormComponent.js
@@ -19,28 +19,28 @@
                 dataType: 'html',
                 success: function (resp) {
                     form.html(resp);
-                    
+
                     form.find('[name=displayLabels]').on('click', function(e){
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
                         component.attr('data-display-labels', this.value === 'true');
                         keditor.initDynamicContent(dynamicElement);
                     });
-                    
+
                     form.find('[name=displayName]').on('click', function(e){
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
                         component.attr('data-display-name', this.value === 'true');
                         keditor.initDynamicContent(dynamicElement);
                     });
-                    
+
                     form.find('[name=displayNickname]').on('click', function(e){
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
                         component.attr('data-display-nickname', this.value === 'true');
                         keditor.initDynamicContent(dynamicElement);
                     });
-                    
+
                     form.find('[name=displayPhone]').on('click', function(e){
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
@@ -54,7 +54,7 @@
                         component.attr('data-display-pwd', this.value === 'true');
                         keditor.initDynamicContent(dynamicElement);
                     });
-                    
+
                     form.find('[name=displayCancel]').on('click', function(e){
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
@@ -104,6 +104,13 @@
                         keditor.initDynamicContent(dynamicElement);
                     });
 
+                    form.find('[name=extra-group]').on('change', function(e){
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        component.attr('data-extra-group', this.value);
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
                     form.find('[name=labelAlign]').on('change', function(e){
                         var component = keditor.getSettingComponent();
                         var dynamicElement = component.find('[data-dynamic-href]');
@@ -117,13 +124,13 @@
 
         showSettingForm: function (form, component, keditor) {
             flog('showSettingForm "registerForm" component', form, component, keditor);
-            
+
             var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
             var showDisplayLabels = dataAttributes['data-display-labels'] !== undefined ? dataAttributes['data-display-labels'] : "true";
             var showDisplayName = dataAttributes['data-display-name'] !== undefined ? dataAttributes['data-display-name'] : "false";
             var showDisplayNickname = dataAttributes['data-display-nickname'] !== undefined ? dataAttributes['data-display-nickname'] : "false";
             var showDisplayPhone = dataAttributes['data-display-phone'] !== undefined ? dataAttributes['data-display-phone'] : "false";
-            
+
             form.find('[name=displayLabels][value='+showDisplayLabels+']').prop('checked', true);
             form.find('[name=displayName][value='+ showDisplayName +']').prop('checked', true);
             form.find('[name=displayNickname][value='+showDisplayNickname+']').prop('checked', true);
@@ -136,6 +143,7 @@
             form.find('[name=successTitle]').val(dataAttributes['data-success-title']);
             form.find('[name=successBody]').val(dataAttributes['data-success-body']);
             form.find('[name=group]').val(dataAttributes['data-group']);
+            form.find('[name=extra-group]').val(dataAttributes['data-extra-group']);
             form.find('[name=labelAlign]').val(dataAttributes['data-label-align']);
         }
     };
