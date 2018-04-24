b80d2e483fdf1373fc14f08ebe99b81b7d78494e
brad <brad@kademi.co>
2018-04-24 17:03:46 +1200

    https://github.com/Kademi/kademi-dev/issues/5355

--- src/main/marketplace/apps/KTimesheets/common/theme/apps/KTimesheets/ktimesheets.js
+++ src/main/marketplace/apps/KTimesheets/common/theme/apps/KTimesheets/ktimesheets.js
@@ -1,8 +1,13 @@
-$(function() {
-    $(".timesheet-table").each(function(i, n){
+$(function () {
+    $(".timesheet-table").each(function (i, n) {
         var table = $(n);
 
-        table.on("change", ".timesheet-item", function(e) {
+        $(document.body).on('pageDateChanged', function (e, startDate, endDate) {
+            flog("reloading", table.attr("id"));
+            table.reloadFragment();
+        });
+
+        table.on("change", ".timesheet-item", function (e) {
             try {
                 flog("item changed");
                 var node = $(e.target);
@@ -10,12 +15,12 @@ $(function() {
                 var tr = node.closest("tr");
                 flog("item changed3");
                 tr.find("input").prop("disabled", false);
-            } catch(e) {
+            } catch (e) {
                 flog("Error: ", e);
             }
         });
 
-        table.on("change", ".timesheet-hours", function(e) {
+        table.on("change", ".timesheet-hours", function (e) {
             var node = $(e.target);
 
             flog("Changed node2", node, node.data("day"));
@@ -26,20 +31,20 @@ $(function() {
                 url: '/timesheets/',
                 type: 'post',
                 data: {
-                    item : item,
+                    item: item,
                     hours: hours,
-                    date : date
+                    date: date
                 },
                 dataType: 'json',
                 success: function (resp) {
                     flog("success");
-                    if (resp && resp.status ) {
+                    if (resp && resp.status) {
                         Msg.info("Saved");
                     } else {
                         Msg.error(resp.messages.join('\n'));
                     }
                 },
-                error : function(resp) {
+                error: function (resp) {
                     Msg.error("Sorry, couldnt save the times");
                 }
             });
