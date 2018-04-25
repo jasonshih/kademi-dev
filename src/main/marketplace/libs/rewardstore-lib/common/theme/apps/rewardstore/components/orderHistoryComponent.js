171fd7afe2724318941903be90fad83d5e2ede32
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2018-04-25 15:07:03 +0300

    Fixed and enhanced tabular components - https://github.com/Kademi/kademi-dev/issues/5368

--- src/main/marketplace/libs/rewardstore-lib/common/theme/apps/rewardstore/components/orderHistoryComponent.js
+++ src/main/marketplace/libs/rewardstore-lib/common/theme/apps/rewardstore/components/orderHistoryComponent.js
@@ -5,8 +5,53 @@
     KEditor.components['orderHistory'] = {
         settingEnabled: true,
         settingTitle: 'Order History Settings',
-        showSettingForm: function (form, component, keditor) {
+        initSettingForm: function (form, keditor) {
+            flog('initSettingForm "pointsEarned" component', form, keditor);
 
+            return $.ajax({
+                url: '_components/pointsEarned?settings',
+                type: 'get',
+                dataType: 'html',
+                success: function (resp) {
+                    form.html(resp);
+                    
+                    form.find('.value-label').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        component.attr('data-label', this.value);
+                        
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+                    
+                    $.getStyleOnce('/static/bootstrap-iconpicker/1.7.0/css/bootstrap-iconpicker.min.css');
+                    $.getScriptOnce('/static/bootstrap-iconpicker/1.7.0/js/iconset/iconset-fontawesome-4.2.0.min.js', function () {
+                        $.getScriptOnce('/static/bootstrap-iconpicker/1.7.0/js/bootstrap-iconpicker.min.js', function () {
+                            form.find('.value-icon').iconpicker({
+                                rows: 5,
+                                cols: 5,
+                                iconset: 'fontawesome',
+                                search: true,
+                                placement: 'left'
+                            }).on('change', function (e) {
+                                var component = keditor.getSettingComponent();
+                                var dynamicElement = component.find('[data-dynamic-href]');
+                                component.attr('data-icon', 'fa ' + e.icon);
+                                
+                                keditor.initDynamicContent(dynamicElement);
+                            });
+                        });
+                    });
+                }
+            });
+        },
+
+        showSettingForm: function (form, component, keditor) {
+            flog('showSettingForm "orderHistory" component', form, component, keditor);
+
+            var dataAttributes = keditor.getDataAttributes(component, ['data-type'], false);
+
+            form.find('.value-label').val(dataAttributes['data-label']);
+            form.find('.value-icon').find('i').attr('class', 'fa ' + dataAttributes['data-icon']);
         }
     };
 
--- src/main/marketplace/libs/salesdata-lib/common/theme/apps/salesdata/components/salesTableComponent.js
+++ src/main/marketplace/libs/salesdata-lib/common/theme/apps/salesdata/components/salesTableComponent.js
@@ -5,7 +5,7 @@
     KEditor.components['salesTable'] = {
         init: function (contentArea, container, component, keditor) {
             flog('init "salesTable" component', contentArea, container, component, keditor);
-            
+
             var self = this;
         },
 
@@ -68,6 +68,33 @@
                         component.attr('data-filter-by-user', checked);
                         keditor.initDynamicContent(dynamicElement);
                     });
+
+                    form.find('.value-label').on('change', function () {
+                        var component = keditor.getSettingComponent();
+                        var dynamicElement = component.find('[data-dynamic-href]');
+                        component.attr('data-label', this.value);
+
+                        keditor.initDynamicContent(dynamicElement);
+                    });
+
+                    $.getStyleOnce('/static/bootstrap-iconpicker/1.7.0/css/bootstrap-iconpicker.min.css');
+                    $.getScriptOnce('/static/bootstrap-iconpicker/1.7.0/js/iconset/iconset-fontawesome-4.2.0.min.js', function () {
+                        $.getScriptOnce('/static/bootstrap-iconpicker/1.7.0/js/bootstrap-iconpicker.min.js', function () {
+                            form.find('.value-icon').iconpicker({
+                                rows: 5,
+                                cols: 5,
+                                iconset: 'fontawesome',
+                                search: true,
+                                placement: 'left'
+                            }).on('change', function (e) {
+                                var component = keditor.getSettingComponent();
+                                var dynamicElement = component.find('[data-dynamic-href]');
+                                component.attr('data-icon', 'fa ' + e.icon);
+
+                                keditor.initDynamicContent(dynamicElement);
+                            });
+                        });
+                    });
                 }
             });
         },
@@ -77,6 +104,8 @@
 
             var dataAttributes = keditor.getDataAttributes(component, [], false);
             form.find('.select-data').val(dataAttributes['data-name']);
+            form.find('.value-label').val(dataAttributes['data-label']);
+            form.find('.value-icon').find('i').attr('class', 'fa ' + dataAttributes['data-icon']);
             form.find('.filter-by-user').prop("checked", dataAttributes['data-filter-by-user'] == "true");
         }
     };
