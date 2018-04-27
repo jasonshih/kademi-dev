dd41a50399cebac698ae8ac1b5f19f59681f0df1
anh <anhnt38@gmail.com>
2018-04-27 12:34:44 +0700

    https://github.com/Kademi/kademi-dev/issues/5334

--- src/main/marketplace/libs/user-lib/APP-INF/app.js
+++ src/main/marketplace/libs/user-lib/APP-INF/app.js
@@ -1,11 +1,10 @@
 controllerMappings.addQuery("/APP-INF/queries/registrationsOverTime.query.json", ["signuplog"], ["ReportsViewer"]);
-controllerMappings.addQuery("/APP-INF/queries/membersTable.query.json", ["profile"], ["ReportsViewer"]);
-controllerMappings.addQuery("/APP-INF/queries/membersTableAll.query.json", ["profile"], ["ReportsViewer"]);
 controllerMappings.addQuery("/APP-INF/queries/membersSpentPoint.query.json", ["rewardpointsdebit"], ["ReportsViewer"]);
 
 controllerMappings.addQuery("/APP-INF/queries/orgsSpentPoint.query.json", ["rewardpointsdebit"], ["ReportsViewer"]);
 controllerMappings.addQuery("/APP-INF/queries/orgsEarnedPoint.query.json", ["rewardPoints"], ["ReportsViewer"]);
 controllerMappings.addQuery("/APP-INF/queries/membersOrgs.query.json", ["profile"], ["ReportsViewer"]);
+controllerMappings.addQuery("/APP-INF/queries/membersOrgsByGroup.query.json", ["profile"], ["ReportsViewer"]);
 controllerMappings.addQuery("/APP-INF/queries/memberTableOrgPoints.query.json", ["org"], ["ReportsViewer"]);
 
 controllerMappings.addComponent("user/components", "recentUserRegistrations", "html", "Display recent registrations", "User App component");
