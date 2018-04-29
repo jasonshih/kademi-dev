93b67eebed18e56a8979fb94a6c9e8bd084287b3
Mohammed El Khoudary <m.elkhoudary@gmail.com>
2018-04-29 15:32:59 +0300

    Claims form enhancements - https://github.com/Kademi/projects/issues/107

--- src/main/marketplace/apps/salesDataClaimer/website/theme/apps/salesDataClaimer/claimsRegisterProduct.js
+++ src/main/marketplace/apps/salesDataClaimer/website/theme/apps/salesDataClaimer/claimsRegisterProduct.js
@@ -1,7 +1,9 @@
 (function ($) {
     $(function () {
         initWizard();
-
+        initDateTimePickers();
+        initImagePicker();
+        
         $(".claimRegisterProductForm").forms({
             onSuccess: function (resp, form) {
                 flog('Claims :: ', resp);
@@ -14,6 +16,40 @@
         });
     });
 
+    function initDateTimePickers() {
+        flog('initDateTimePickers');
+    
+        var purchaseDate = $('#purchase-date');
+        purchaseDate.datetimepicker({
+            format: 'DD/MM/YYYY',
+            startDate: '01/05/2018',
+            endDate: '31/08/2018'
+        });
+        
+        var installationDate = $('#installation-date');
+        installationDate.datetimepicker({
+            format: 'DD/MM/YYYY'
+        });
+    }
+    
+    function initImagePicker(){
+        $('.success-invoice-image').hide();
+        $('#invoice-image').change(function(){
+            $('.success-invoice-image').show();    
+        });
+        
+        $('.success-installation-image').hide();
+        $('#installation-invoice-image').change(function(){
+            $('.success-installation-image').show();    
+        });
+    }
+    
+    function parseDate(input) {
+      var parts = input.match(/(\d+)/g);
+      // note parts[1]-1
+      return new Date(parts[2], parts[1]-1, parts[0]);
+    }
+    
     function initWizard() {
         flog('initWizard');
 
@@ -28,29 +64,38 @@
 
             if (data.step === 2) {
                 var stepPane2 = stepPane.eq(1);
-
             }
 
             if (data.step === 5) {
                // $(".last-step").html(""); $("#ClaimRegisterProductForm .step-pane:not(.last-step)").clone().css("display", "block").find(":input").prop("disabled", true).end().appendTo(".last-step")
-                $(".last-step").html(""); $("#ClaimRegisterProductForm .step-pane:not(.last-step)")
-                        .find("select :selected").each(function() { $(this).attr("selected", "selected"); })
-                        .end().find("button").each(function(){ $(this).replaceWith("")})
-                        .end().clone().css("display", "block").find(":input")
-                        .each(function() { $(this).replaceWith('<span class="summary-field" data-parent-step="' + $(this).parents(".step-pane").data("step") + '">' + ($(this).find(":selected").html() === undefined ? $(this).val() : $(this).find(":selected").html()) + '</span>'); }).end().appendTo(".last-step")
+                $(".last-step").html(""); 
+                $("#ClaimRegisterProductForm .step-pane:not(.last-step)")
+                        .find("select :selected").each(function() { 
+                            $(this).attr("selected", "selected"); 
+                        }).end()
+                        .find("button").each(function(){ 
+                            $(this).replaceWith("");
+                        }).end().clone().css("display", "block")
+                        .find(":input").each(function() {
+                            $(this).replaceWith('<span class="summary-field" data-parent-step="' + $(this).parents(".step-pane").data("step") + '">' + ($(this).find(":selected").html() === undefined ? $(this).val().replace("C:\\fakepath\\", "") : $(this).find(":selected").html()) + '</span>'); 
+                        }).end().appendTo(".last-step");
                 $("body").on("click", ".summary-field", function() {
                     $("li[data-step='" + $(this).data("parent-step") + "']").click();
                 });
-
             }
         });
 
         myWizard.on('actionclicked.fu.wizard', function (evt, data) {
-            flog("here");
             if (data.step === 1) {
                 var stepPane1 = stepPane.eq(0);
                 resetValidation(stepPane1);
                 if (!validateFormFields(stepPane1)) {
+                    evt.preventDefault();
+                }
+                
+                if( $('#confirm-email').val() != $('#email').val()){
+                    showErrorField($('#confirm-email'));
+                    showErrorMessage(stepPane2, null, 'Email address and confirm email address must be similar');
                     evt.preventDefault();
                 }
             }
@@ -61,6 +106,30 @@
                 if (!validateFormFields(stepPane2)) {
                     evt.preventDefault();
                 }
+                
+                if($('#purchase-date').length > 0 && validateFormFields(stepPane2) ){
+                    var purchaseDate = parseDate($('#purchase-date').val());
+                    var month = purchaseDate.getMonth() + 1;
+                    var year = purchaseDate.getFullYear();
+                    if( month < 5 || month > 8 || year != 2018){
+                        showErrorField($('#purchase-date'));
+                        showErrorMessage(stepPane2, null, 'Your purchase must be between 1st May and 31st August to be eligible');
+                        evt.preventDefault();
+                    }
+                }
+                
+                if($('#contact-email').is(":visible") && $('#contact-email').val() != $('#confirm-contact-email').val()){
+                    showErrorField($('#confirm-contact-email'));
+                    showErrorMessage(stepPane2, null, 'Supplier email address and confirm email address must be similar');
+                    evt.preventDefault();
+                }
+                
+                if($('#installer-email').is(":visible") && $('#installer-email').val() != $('#confirm-installer-email').val()){
+                    showErrorField($('#confirm-installer-email'));
+                    showErrorMessage(stepPane2, null, 'Installer email address and confirm email address must be similar');
+                    evt.preventDefault();
+                }
+                
             }
 
             if (data.step === 3) {
@@ -75,6 +144,20 @@
                 var stepPane4 = stepPane.eq(3);
                 resetValidation(stepPane4);
                 if (!validateFormFields(stepPane4)) {
+                    evt.preventDefault();
+                }
+                
+                var bsbReg = new RegExp("^[0-9]{6}$");
+                if(!bsbReg.test($('#bsb-number').val())){
+                    showErrorField($('#bsb-number'));
+                    showErrorMessage(stepPane4, null, 'Your BSB number must be between exaclty 6 digits and cointains digits only to be eligible');
+                    evt.preventDefault();
+                }
+                
+                var accountReg = new RegExp("^[0-9]{1,10}$");
+                if(!accountReg.test($('#account-no').val())){
+                    showErrorField($('#account-no'));
+                    showErrorMessage(stepPane4, null, 'Your account number must be less than 10 digits and cointains digits only to be eligible');
                     evt.preventDefault();
                 }
             }
