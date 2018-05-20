c287d3e857bf25ed1c612ac16725179c135551b1
brad <brad@brad-zen>
2018-05-20 16:46:52 +1200

    https://github.com/Kademi/kademi-dev/issues/5450

---
+++ src/main/marketplace/apps/KCommerce2/APP-INF/app.js
@@ -0,0 +1,6 @@
+controllerMappings.addComponent("KCommerce2/components", "ecomStoreSidebar", "html", "Display ecom product details", "E-commerce App component");
+
+
+function initKCommerce2App(orgRoot, webRoot, enabled) {
+    
+}
