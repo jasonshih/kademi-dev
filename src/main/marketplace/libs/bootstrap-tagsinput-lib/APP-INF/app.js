123f7df3bd3f763af6c551018ac95e7ff35a3668
brad <brad@kademi.co>
2018-06-22 12:13:52 +1200

    https://github.com/Kademi/kademi-dev/issues/5355

--- src/main/marketplace/libs/bootstrap-tagsinput-lib/APP-INF/app.js
+++
@@ -1,7 +0,0 @@
-controllerMappings
-        .adminController()
-        .path("/helloWorld/")
-        .enabled(true)
-        .defaultView(views.templateView("helloWorld/manageHelloWorld.html"))
-        .addMethod("POST", "handleCoffee")
-        .build();
